b4f33520bac2c62cde3d9bd19e6859a98d1e6ea4
Stacy Vlasits <stacyvlasits@gmail.com>
2021-10-28 14:48:14 -0500

    prepared app for plugins stored in Containers/Tools

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { createTheme, ThemeProvider } from '@material-ui/core/styles';
 import { grey } from '@material-ui/core/colors';
 import CadView from './Containers/CadView';
+import XYZExtract from './Containers/Tools/XYZExtract/XYZExtract.js';
 import ReactGA from 'react-ga';
 
 const outerTheme = createTheme({
@@ -30,8 +31,11 @@ function App() {
     <ThemeProvider theme={outerTheme}>
       <Router>
         <Switch>
-          <Route path='/'>
+          <Route exact path='/'>
             <CadView />
+          </Route>
+          <Route path='/extract'>
+            <XYZExtract />
           </Route>
         </Switch>
       </Router>
