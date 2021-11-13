80a439ab8c178c7e3fe01a01361fd59ff4bdefb7
Oleg <omoshkovich@gmail.com>
2021-11-13 14:27:43 +0300

    add files

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
   output: {
     path: path.join(__dirname, '/build'),
     filename: '[name].[contenthash].js',
-    publicPath: '/',
+    publicPath: '',
   },
   devtool: 'source-map',
   module: {
