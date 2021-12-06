158af75824ccaf9e48e21c73ddf0ce1031111090
Oleg <omoshkovich@gmail.com>
2021-12-06 19:07:14 +0300

    experiment with icon layout

--- src/Containers/CadView.jsx
+++ src/Containers/CadView.jsx
@@ -268,7 +268,8 @@ const CadView = () => {
     // Expanded version of viewer.loadIfcUrl('/index.ifc').  Using
     // this to get access to progress and error.
     const parts = window.location.pathname.split(/[-\w\d]+.ifc/);
-    const filePath = './haus.ifc';
+    // const filePath = './haus.ifc';
+    const filePath = './';
     if (debug) {
       console.log('CadView#useEffect: load from server and hash: ', filePath);
     }
