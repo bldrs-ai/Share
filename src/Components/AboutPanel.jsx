cc703d3b5bbdfbe861117408bb425befed17301e
Pablo Mayrgundter <pablo.mayrgundter@gmail.com>
2021-12-10 17:12:37 +0300

    Slow (#74)
    * Refactor AboutPanel to be self-contained and fast.
    
    * New build.

--- src/Components/AboutPanel.jsx
+++ src/Components/AboutPanel.jsx
@@ -6,7 +6,12 @@ import Close from '../assets/Close.svg'
 
 
 const useStyles = makeStyles((theme) => ({
-  paper: {
+  panel: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    width: '500px',
+    margin: '0 auto',
     fontFamily: 'Helvetica',
     padding: '1em',
     "& h1, & h2": {
@@ -16,7 +21,7 @@ const useStyles = makeStyles((theme) => ({
       marginTop: 0
     }
   },
-  close:{
+  closeButton: {
     float:'right',
     cursor:'pointer',
     "& svg": {
@@ -26,25 +31,37 @@ const useStyles = makeStyles((theme) => ({
   }
 }));
 
-const AboutPanel = ({close}) => {
-  const classes = useStyles();
+
+const AboutIcon = () => {
+  const [open, setOpen] = React.useState(true);
   return (
-      <Paper elevation={3} className={classes.paper}>
-        <div className = {classes.close}onClick = {close}><Close/></div>
-        <h1>About</h1>
-        <p><strong>BLDRS</strong> is a collaborative integration environment for IFC files.
-            We are just getting started!  Stay tuned for the upcoming MVP release.</p>
-        <p>BLDRS is an open source project. Please visit our repository:&nbsp;
-          <a href = {'https://github.com/buildrs/Share'} target="_new">github.com/buildrs/Share</a></p>
-        <h2>Features</h2>
-        <ul>
-          <li>Upload IFC file</li>
-          <li>Select IFC element</li>
-          <li>Get IFC element properties </li>
-          <li>Share IFC element with the URL address</li>
-        </ul>
-      </Paper>
-  );
+    <div onClick={() => { setOpen(!open) }}>
+      About {open && <AboutPanel openToggle={()=>{setOpen(!open)}}/>}
+    </div>);
 };
 
-export default AboutPanel;
+
+const AboutPanel = ({openToggle}) => {
+  const classes = useStyles();
+
+  return (
+    <Paper elevation={3} className={classes.panel}>
+      <div className = {classes.closeButton} onClick={openToggle}><Close/></div>
+      <h1>About</h1>
+      <p><strong>BLDRS</strong> is a collaborative integration environment for IFC files.
+        We are just getting started!  Stay tuned for the upcoming MVP release.</p>
+      <p>BLDRS is an open source project. Please visit our repository:&nbsp;
+      <a href = {'https://github.com/buildrs/Share'} target="_new">github.com/buildrs/Share</a></p>
+      <h2>Features</h2>
+      <ul>
+        <li>Upload IFC file</li>
+        <li>Select IFC element</li>
+        <li>Get IFC element properties </li>
+        <li>Share IFC element with the URL address</li>
+      </ul>
+    </Paper>);
+};
+
+export {
+  AboutIcon
+}
