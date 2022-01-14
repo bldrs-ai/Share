495c9b2a393b653e60f11b3ac0d639f0c310aa86
Oleg <omoshkovich@gmail.com>
2022-01-14 16:44:42 +0100

    properties drawer

--- src/Components/ItemPropertiesDrawer.jsx
+++ src/Components/ItemPropertiesDrawer.jsx
@@ -6,42 +6,66 @@ import { makeStyles } from '@mui/styles';
 const useStyles = makeStyles({
   drawerPaper:{
     marginTop:'65px',
-    height:'200px',
     width:'350px',
     borderRadius:'0px',
     marginLeft:'20px',
     zIndex: 10,
-    overflow:'hidden',
     '@media (max-width: 900px)': {
-    width:'auto',
-    borderRadius:'8px',
-    marginLeft:'0px',
+      width:'auto',
+      height:'200px',
+      borderRadius:'8px',
+      marginLeft:'0px',
     },
+  },
+  drawerContainer:{
+    height:'100%',
+    width:'auto',
+    overflow:'hidden'
   },
   headerWrapper: {
     display: 'flex',
     justifyContent: 'space-between',
     alignItems: 'center',
-    margin: '10px 10px',
+    margin: '16px 10px 8px 10px',
+    paddingBottom:'12px',
+    borderBottom:'1px solid #494747',
     '@media (max-width: 900px)': {
       paddingBottom: '10px',
-      borderBottom:'1px solid lightgrey',
+      borderBottom:'none',
     }
   },
   title: {
     fontFamily: 'Helvetica',
     fontSize: '20px',
-    fontWeight: 200,
+    fontWeight: '200px',
     color: '#696969',
+    marginLeft:'13px'
+  },
+  closeContainer: {
+    position:'relative',
+    right:'20px',
+    '@media (max-width: 900px)': {
+      maxHeight:'200px',
+      right:'20px',
+      top:'2px'
+    },
   },
   close: {
     height: '20px',
     width: '20px',
     zIndex: 1000,
     cursor: 'pointer',
+    '@media (max-width: 900px)': {
+      height: '30px',
+      width: '30px',
+    },
   },
   contentContainer:{
+    display:'flex',
+    flexDirection:'row',
     justifyContent:'center',
+    overflow:'auto',
+    height:'90%',
     '@media (max-width: 900px)': {
       maxHeight:'200px',
       overflow:'auto'
@@ -64,20 +88,21 @@ const ItemPropertiesDrawer = ({
       <React.Fragment key={'right'}>
         <Drawer
           elevation={3}
-          BackdropProps={{ invisible: true }}
           anchor={anchor}
           open={true || open}
           variant='persistent'
-          docked='true'
           classes={{paper: classes.drawerPaper}}
         >
+        <div className ={classes.drawerContainer}>
           <div className={classes.headerWrapper} onClick={onClose}>
             <div className={classes.title}>{title}</div>
-            <Close className={classes.close} />
+           <div className = {classes.closeContainer}><Close className={classes.close} /></div>
           </div>
+
           <div className = {classes.contentContainer}>
             {content}
           </div>
+        </div>
         </Drawer>
       </React.Fragment>
     </>
