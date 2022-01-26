f50628b5fecbbb07756abb11975f283e59e11ef3
Pablo Mayrgundter <pablo.mayrgundter@gmail.com>
2022-01-26 20:02:54 +0300

    Integrated Oleg changes from main and added unit tests for ItemProperties and updated others.

--- src/utils/Ifc.test.js
+++ src/utils/Ifc.test.js
@@ -1,6 +1,7 @@
 import {
   decodeIFCString,
-} from './Ifc.js';
+  getType
+} from './Ifc.js'
 
 
 test('Test decode ifc string', () => {
@@ -8,3 +9,41 @@ test('Test decode ifc string', () => {
   expect(someAscii).toEqual(decodeIFCString(someAscii));
   expect('Küche').toEqual(decodeIFCString('K\\X2\\00FC\\X0\\che'));
 });
+
+
+test('ifc.js IfcManager.getType', () => {
+  const elt = {
+    children: [],
+    expressID: 1,
+    Name: 'Building'
+  };
+  expect(getType(elt, mockViewer)).toEqual('IFCELEMENT');
+});
+
+
+export const mockViewer = {
+  IFC: {
+    loader: {
+      ifcManager: {
+        getPropertySets: (modelId, expressId) => {
+          return new Promise((resolve, reject) => {
+            resolve([]);
+          });
+        },
+        getIfcType: (elt, viewer) => 'IFCELEMENT'
+      }
+    }
+  }
+};
+
+
+export function newMockStringValueElt(label, id = 1) {
+  return {
+    children: [],
+    expressID: id,
+    Name: {
+      type: 1,
+      value: label
+    }
+  }
+}
