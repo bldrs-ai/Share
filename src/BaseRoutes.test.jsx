1005fbd8b328ad47a2f6a79cb39ed3077c103922
Oleg <omoshkovich@gmail.com>
2022-02-11 14:13:51 +0100

    pass tests

--- src/BaseRoutes.test.jsx
+++ src/BaseRoutes.test.jsx
@@ -3,6 +3,9 @@ import {render} from '@testing-library/react'
 import {mockRoutes} from './BaseRoutesMock.test'
 import BaseRoutes from './BaseRoutes'
 
+//TO do flacky test
+//  RangeError: /Users/olegmoshkovich/Desktop/builders/Share/node_modules/web-ifc/web-ifc-api.js:
+//Maximum call stack size exceeded
 
 test('BaseRoutes', () => {
   const testLabel = 'Test node label'
