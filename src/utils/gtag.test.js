42337ccdd45432cc321c75e9def121766e48e880
OlegMoshkovich <oleg.mosh@gmail.com>
2022-03-15 10:58:08 -0400

    format

--- src/utils/gtag.test.js
+++ src/utils/gtag.test.js
@@ -5,8 +5,8 @@ test('gtag', () => {
   window.dataLayer = {
     push: (a) => {
       args = a
-    }
+    },
   }
   gtag('a', 'b')
-  expect(args).toMatchObject({ '0': 'a', '1': 'b' })
+  expect(args).toMatchObject({0: 'a', 1: 'b'})
 })
