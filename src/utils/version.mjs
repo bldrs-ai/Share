f50784b64228190689ec47609d41a26b0d1cacb0
Omachonu Ogali <232224+oogali@users.noreply.github.com>
2022-05-07 04:56:01 -0400

    Specify type assertion for JSON import (#194)
    * Specify type assertion for JSON import
    
    * bldrs/Share#194: Ensure all project JS files are checked by linter
    
    * bldrs/Share#194: Add ESLint plugin (and configuration) for import assertions
    
    * bldrs/Share#194: Fix lint errors in version generation logic

--- src/utils/version.mjs
+++ src/utils/version.mjs
@@ -1,9 +1,11 @@
-import { execSync} from 'child_process';
-import PkgInfo from '../../package.json';
+import {execSync} from 'child_process'
+import PkgInfo from '../../package.json' assert {type: "json"}
 
 // https://stackoverflow.com/questions/24663175/how-can-i-inject-a-build-number-with-webpack
-const __versionString__ = execSync('git rev-list HEAD --count').toString().trim();
+const __versionString__ = execSync('git rev-list HEAD --count').toString().trim()
 const version = PkgInfo.version.toString().replace(/-.*/, '')
-PkgInfo.version = `${version}-r${__versionString__}`;
-const PkgInfoStr = JSON.stringify(PkgInfo, null, '  ');
-console.log(PkgInfoStr);
+
+PkgInfo.version = `${version}-r${__versionString__}`
+const PkgInfoStr = JSON.stringify(PkgInfo, null, '  ')
+
+console.log(PkgInfoStr)
