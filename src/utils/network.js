17ef92403feee1bfd821ee4524b0cd756e7cd4fc
Omachonu Ogali <232224+oogali@users.noreply.github.com>
2022-05-30 16:04:29 -0400

    Tighten up local development environment identification (#210)

--- src/utils/network.js
+++ src/utils/network.js
@@ -5,12 +5,19 @@ import debug from './debug'
  * @return {boolean} True if running locally
  */
 export function isRunningLocally() {
-  const local = window.location.hostname == 'localhost' ||
-    window.location.hostname.startsWith('127.') ||
-        window.location.hostname.startsWith('192.')
+  const local = window.location.hostname === 'localhost' ||
+      window.location.hostname === '[::1]' ||
+      window.location.hostname.startsWith('10.') ||
+      window.location.hostname.startsWith('127.') ||
+      window.location.hostname.startsWith('169.254.') ||
+      window.location.hostname.match(/^172\.(1[6-9]|2[0-9]|3[0-1])/) ||
+      window.location.hostname.startsWith('192.168.') ||
+      window.location.hostname.endsWith('.local')
+
   if (local) {
     debug().warn('Network: site is being served locally')
     return true
   }
+
   return false
 }
