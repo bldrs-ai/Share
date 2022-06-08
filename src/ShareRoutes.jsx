6d1b3242047415e2dbf1347aeec941cd99c7a348
Omachonu Ogali <oogali@gmail.com>
2022-06-08 12:00:35 -0400

    Refactor function to use native URL parsing

--- src/ShareRoutes.jsx
+++ src/ShareRoutes.jsx
@@ -126,30 +126,38 @@ export function githubUrlOrPathToSharePath(urlWithPath) {
 // Functions below exported only for testing.
 /**
  * Look for any obvious problems with the given url.
- * @param {Object} urlStr
- * @return {boolean} return true if url is found
+ * @param {string} urlStr URL to process
+ * @return {string} URL path without any BLDRS-specific items
  * @throws Error if the argument have path slash '/' characters after
  * trimming host and appinstal prefix.
  * @private
  */
 export function trimToPath(urlStr) {
   assertDefined(urlStr)
-  let s = urlStr.trim()
-  if (s.startsWith('http://')) {
-    s = s.substring('http://'.length)
-  } else if (s.startsWith('https://')) {
-    s = s.substring('https://'.length)
+  urlStr = urlStr.trim()
+
+  let u
+  if (urlStr.indexOf('/') === 0) {
+    // If we are passed a relative URL, then assume the base is Github
+    u = new URL(urlStr, 'https://github.com')
+  } else {
+    u = new URL(urlStr)
   }
+
+  let path = u.pathname
+
   // Allow use of links like bldrs.ai/share/v/gh and localhost:8080/share/v/gh
-  const sharePathNdx = s.indexOf('share/v/gh')
-  if (sharePathNdx > 0) {
-    s = s.substring(sharePathNdx + 'share/v/gh'.length)
+  const pathIndex = path.indexOf('share/v/gh')
+  if (pathIndex > 0) {
+    path = path.substring(pathIndex + 'share/v/gh'.length)
   }
-  const firstSlashNdx = s.indexOf('/')
-  if (firstSlashNdx == -1) {
+
+  const firstSlashNdx = path.indexOf('/')
+  if (firstSlashNdx === -1) {
     throw new Error('Expected at least one slash for file path: ' + urlStr)
   }
-  return s.substring(firstSlashNdx)
+
+  return path.substring(firstSlashNdx)
 }
 
 
--- src/ShareRoutes.test.jsx
+++ src/ShareRoutes.test.jsx
@@ -15,10 +15,10 @@ const tests = [
   {s: 'http://github.com/org/repo/blob/branch/a/b/c/file.ifc', out: pathBlobAbc},
   {s: 'https://www.github.com/org/repo/blob/branch/file.ifc', out: pathBlob},
   {s: 'https://github.com/org/repo/blob/branch/file.ifc', out: pathBlob},
-  {s: 'github.com/org/repo/blob/branch/file.ifc', out: pathBlob},
-  {s: 'githubcom/org/repo/blob/branch/file.ifc', out: pathBlob},
-  {s: 'localhost:8080/share/v/gh/org/repo/branch/file.ifc', out: path},
-  {s: 'bldrs.ai/share/v/gh/org/repo/branch/file.ifc', out: path},
+  {s: 'http://github.com/org/repo/blob/branch/file.ifc', out: pathBlob},
+  {s: 'https://githubcom/org/repo/blob/branch/file.ifc', out: pathBlob},
+  {s: 'http://localhost:8080/share/v/gh/org/repo/branch/file.ifc', out: path},
+  {s: 'https://bldrs.ai/share/v/gh/org/repo/branch/file.ifc', out: path},
   {s: 'http://localhost:8080/share/v/gh/org/repo/branch/file.ifc', out: path},
   {s: 'http://bldrs.ai/share/v/gh/org/repo/branch/file.ifc', out: path},
   {s: 'https://localhost:8080/share/v/gh/org/repo/branch/file.ifc', out: path},
@@ -40,8 +40,8 @@ describe('ShareRoutes', () => {
 
   test('trimToPath', () => {
     [
-      {s: '', err: 'Expected at least one slash for file path: '},
-      {s: 'window', err: 'Expected at least one slash for file path: window'},
+      {s: '', err: 'Invalid URL: '},
+      {s: 'window', err: 'Invalid URL: window'},
     ].concat(tests).forEach((pair) => {
       if (pair.out !== undefined) {
         expect(trimToPath(pair.s), `With input ${pair.s}`).toBe(pair.out)
@@ -58,13 +58,13 @@ describe('ShareRoutes', () => {
   })
 
   test('githubUrlOrPathToSharePath', () => {
-    const errPrefix = 'Expected a multi-part file path: '
-    const errPrefix2 = 'Expected at least one slash for file path: ';
+    const errPrefix = 'Expected a multi-part file path: ';
+
     [
-      {s: 'a/b/c', err: errPrefix + '/b/c'},
-      {s: 'www.google.com', err: errPrefix2 + 'www.google.com'},
-      {s: 'http://www.google.com', err: errPrefix2 + 'http://www.google.com'},
-      {s: 'http://www.google.com/', err: errPrefix + '/'},
+      {s: 'a/b/c', err: 'Invalid URL: a/b/c'},
+      {s: 'www.google.com', err: 'Invalid URL: www.google.com'},
+      {s: 'http://www.google.com', err: errPrefix + '/'},
+      {s: 'http://www.google.com/search', err: errPrefix + '/search'},
     ].concat(tests).forEach((pair) => {
       if (pair.out !== undefined) {
         const out = pair.out.replace(/blob\//, '')
