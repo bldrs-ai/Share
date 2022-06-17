ac8e22098ef26f13f7b9fa8b1dbd023f17fe817b
OlegMoshkovich <oleg.mosh@gmail.com>
2022-06-17 17:06:12 +0300

    simplified storage

--- src/utils/store.js
+++ src/utils/store.js
@@ -2,28 +2,11 @@ import create from 'zustand'
 
 
 const useStore = create((set) => ({
-  viewerStore: {},
-  modelStore: null,
-  selectedElementStore: {},
   isDrawerOpen: false,
   isCommentsOn: false,
-  isPropertiesOn: false,
-  selectedIssueId: null,
-  selectedCommentIndex: null,
   snackMessage: null,
-  issues: [],
-  replies: [],
-  setViewerStore: (viewer) => set(() => ({viewerStore: viewer})),
-  setModelStore: (model) => set(() => ({modelStore: model})),
-  setSelectedElementStore: (element) => set(() => ({selectedElementStore: element})),
   setSnackMessage: (message) => set(() => ({snackMessage: message})),
   openDrawer: () => set(() => ({isDrawerOpen: true})),
   closeDrawer: () => set(() => ({isDrawerOpen: false})),
-  toggleIsCommentsOn: () => set((state) => ({isCommentsOn: !state.isCommentsOn})),
-  toggleIsPropertiesOn: () => set((state) => ({isPropertiesOn: !state.isPropertiesOn})),
-  setIssues: (issues) => set(() => ({issues: issues})),
-  setComments: (comments) => set(() => ({comments: comments})),
-  setSelectedIssueId: (issueId) => set(() => ({selectedIssueId: issueId})),
-  setSelectedIssueIndex: (issueIndex) => set(() => ({setSelectedIssueIndex: issueIndex})),
 }))
 export default useStore
