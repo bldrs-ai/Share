807693fd994255d964b8ed82f97cec8fd56b9438
OlegMoshkovich <oleg.mosh@gmail.com>
2022-06-17 12:41:19 +0300

    add zustand test

--- src/utils/store.test.js
+++ src/utils/store.test.js
@@ -1,21 +1,13 @@
-import { act, renderHook } from "@testing-library/react-hooks";
-import { cleanup } from "@testing-library/react";
-import useStore from "./store";
+import {act, renderHook} from '@testing-library/react-hooks'
+import useStore from './store'
 
-describe("useStore", () => {
-    afterEach(() => {
-        // You can chose to set the store's state to a default value here.
-        jest.resetAllMocks();
-        cleanup();
-    });
 
-    it("Set snack message", () => {
-        const { result } = renderHook(() => useStore((state) => state));
-
-        act(() => {
-            result.current.setSnackMessage(["loading"]);
-        });
-
-        expect(result.current.snackMessage).toEqual(["loading"]);
-    });
-});
+describe('useStore', () => {
+  it('Set snack message', () => {
+    const {result} = renderHook(() => useStore((state) => state))
+    act(() => {
+      result.current.setSnackMessage(['loading'])
+    })
+    expect(result.current.snackMessage).toEqual(['loading'])
+  })
+})
