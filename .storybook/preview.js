96d455c7f10a7bfa8e47aaf145915572e3e0c2e1
Omachonu Ogali <oogali@gmail.com>
2022-06-22 08:57:21 -0400

    Set default theme

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,8 @@
+import {createContext} from 'react'
+import {addDecorator} from '@storybook/react'
+import {ThemeProvider} from '@mui/material/styles'
+import useTheme from '../src/Theme'
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -7,3 +12,16 @@ export const parameters = {
     },
   },
 }
+
+addDecorator((Story) => {
+  const {theme, colorMode} = useTheme()
+  const ColorModeContext = createContext({toggleColorMode: () => {}})
+
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        {Story()}
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  )
+})
