f38263564c95fa5f358dd5dc0ae20e8a7ee81d3f
Pablo Mayrgundter <pablo.mayrgundter@gmail.com>
2022-07-28 20:29:35 +0200

    This includes new build rules and slight change in how we build.

--- .storybook/main.js
+++ .storybook/main.js
@@ -7,7 +7,8 @@ module.exports = {
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
-    'storybook-addon-material-ui'
+    'storybook-addon-material-ui',
+    'storybook-addon-turbo-build'
   ],
   'framework': '@storybook/react',
   'features': {
