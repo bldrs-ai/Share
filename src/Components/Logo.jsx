238bee15698c4f73426ef9b955411c7b17fd0084
OlegMoshkovich <oleg.mosh@gmail.com>
2022-08-09 21:08:59 +0200

    put open into search bar

--- src/Components/Logo.jsx
+++ src/Components/Logo.jsx
@@ -1,11 +1,10 @@
-import React, {useContext} from 'react'
+import React from 'react'
 import {makeStyles, useTheme} from '@mui/styles'
 import IconButton from '@mui/material/IconButton'
 import Tooltip from '@mui/material/Tooltip'
 import PkgJson from '../../package.json'
-import LogoIcon from '../assets/2D_Icons/Logo_DS1.svg'
-import {ColorModeContext} from '../Context/ColorMode'
-import LogoIconDark from '../assets/2D_Icons/Logo_DS1_DarkMode.svg'
+import LogoIcon from '../assets/2D_Icons/Logo.svg'
+
 
 
 /**
@@ -14,12 +13,11 @@ import LogoIconDark from '../assets/2D_Icons/Logo_DS1_DarkMode.svg'
  */
 export default function Logo({onClick}) {
   const classes = useStyles(useTheme())
-  const theme = useContext(ColorModeContext)
   return (
     <div className={classes.logoGroup}>
       <Tooltip title={`Bldrs: ${PkgJson.version}`} describeChild placement="right">
         <IconButton disableRipple onClick={onClick}>
-          {theme.isDay() ? <LogoIcon/> : <LogoIconDark/>}
+          <LogoIcon/>
         </IconButton>
       </Tooltip>
     </div>
@@ -31,7 +29,7 @@ const useStyles = makeStyles((theme) => ({
   logoGroup: {
     'position': 'fixed',
     'bottom': '26px',
-    'left': '0px',
+    'left': '20px',
     'width': '140px',
     'height': '50px',
     '& svg': {
