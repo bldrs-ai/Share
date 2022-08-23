cbe10c3451ab7f5038fa89962016fa53edf5bfcf
OlegMoshkovich <oleg.mosh@gmail.com>
2022-08-23 13:42:01 +0200

    add test scaffold for properties view

---
+++ src/Containers/CadView.test.jsx
@@ -0,0 +1,13 @@
+// import React from 'react'
+// import {act, render, renderHook, screen, waitFor} from '@testing-library/react'
+// import ShareMock from '../ShareMock'
+// import {MockModel} from '../utils/IfcMock.test'
+// import useStore from '../store/useStore'
+// import ItemProperties from './ItemProperties'
+
+
+test('test the right parameters show up in the side drawer properites', () => {
+  //  elect the element in the model
+  //  check if the right information appears in the properties panel
+})
+
