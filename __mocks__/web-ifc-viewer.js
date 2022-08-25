b206c5595ea56a441b31884f63c840b843c2c85c
Pablo Mayrgundter <pablo.mayrgundter@gmail.com>
2022-08-25 13:28:10 -0500

    Tests: Introduce CadView.test.jsx. (#348)
    * Introduce CadView.test.jsx.
    
    * Add waitFor on content to CadView.test to clean up act warning.  Move web-ifc-viewer to node_modules mocks.
    
    * CadView.test: add todo for why three is being mocked.

---
+++ __mocks__/web-ifc-viewer.js
@@ -0,0 +1,25 @@
+const ifcjsMock = jest.createMockFromModule('web-ifc-viewer')
+export default ifcjsMock
+
+const constructorMock = ifcjsMock.IfcViewerAPI
+// Not sure why this is required, but otherwise these internal fields
+// are not present in the instantiated IfcViewerAPI.
+const impl = {
+  IFC: {
+    context: {
+      ifcCamera: {
+        cameraControls: {},
+      },
+    },
+    setWasmPath: jest.fn(),
+    loadIfcUrl: jest.fn(),
+  },
+  clipper: {
+    active: false,
+  },
+  context: {
+    resize: jest.fn(),
+  },
+}
+constructorMock.mockImplementation(() => impl)
+export {constructorMock as IfcViewerAPI}
---
+++ src/Containers/CadView.test.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import {render, screen, waitFor} from '@testing-library/react'
+import CadView from './CadView'
+import ShareMock from '../ShareMock'
+
+
+// TODO(pablo): This mock suppresses "WARNING: Multiple instances of
+// Three.js being imported", but why is it being included if
+// web-ifc-viewer is being mocked?
+jest.mock('three')
+
+jest.mock('web-ifc-viewer')
+
+describe('CadView', () => {
+  it('renders with mock IfcViewerAPI', async () => {
+    // Mock IfcViewerApi is in $repo/__mocks__/web-ifc-viewer.js
+    const modelPath = {
+      gitPath: '',
+    }
+    render(
+        <ShareMock>
+          <CadView
+            installPrefix={''}
+            appPrefix={''}
+            pathPrefix={''}
+            modelPath={modelPath}
+          />
+        </ShareMock>)
+    // Necessary to wait for some of the component to render to avoid
+    // act() warningings from testing-library.
+    await waitFor(() => screen.getByTitle(/Bldrs: 1.0.0/i))
+  })
+})
