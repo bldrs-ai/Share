574b42b9e0b780002119b40708f79ba99882e6ad
Omachonu Ogali <oo@bldrs.ai>
2022-09-09 22:15:01 -0400

    Configure Cypress to look at the build output directory

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    fileServerFolder: 'docs/',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
