bb28d0a1988283cfab931baa13dede80dd369eb3
Omachonu Ogali <oo@bldrs.ai>
2022-09-12 19:16:43 -0400

    Use Github icon for login button

--- src/Components/LoginButton.jsx
+++ src/Components/LoginButton.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import {useAuth0} from '@auth0/auth0-react'
 import {TooltipIconButton} from './Buttons'
-import LoginIcon from '@mui/icons-material/Login'
+import GitHubIcon from '@mui/icons-material/GitHub'
 
 
 const LoginButton = ({
@@ -12,8 +12,8 @@ const LoginButton = ({
   const {loginWithRedirect} = useAuth0()
   return (
     <TooltipIconButton
-      title={'Log in to Github'}
-      icon={<LoginIcon/>}
+      title={'Log in with Github'}
+      icon={<GitHubIcon />}
       onClick={loginWithRedirect}
     />
   )
