c4c7d01a8b4b2aa6937b717a9e0a221455a06fb8
Pablo Mayrgundter <pablo.mayrgundter@gmail.com>
2022-09-16 20:22:38 +0200

    AppTray using direct window.PostMessage matrix API

--- src/Components/AppTray.jsx
+++ src/Components/AppTray.jsx
@@ -1,11 +1,18 @@
-import React, {useState} from 'react'
+import React, {useCallback, useState} from 'react'
 
 
 /**
  * @return {object} React component
  */
 export default function AppTray() {
-  const [frameSource, setFrameSource] = useState('https://www.vyzn.tech/')
+  const [frameSource, setFrameSource] = useState('https://localhost:8081/stickerpicker.html')
+  const appFrameRef = useCallback((elt) => {
+    if (elt) {
+      initMatrix(elt)
+    }
+  }, [])
+
+
   return (
     <>
       <div
@@ -13,7 +20,7 @@ export default function AppTray() {
           position: 'absolute',
           top: '0.5em',
           right: '5em',
-          width: '20em',
+          width: '60em',
         }}
       >
         <form name='appFrame' onSubmit={(e) => {
@@ -24,15 +31,76 @@ export default function AppTray() {
           IFrame source: <input name='frameSource' defaultValue={frameSource} width='20'/>
         </form>
         <iframe
-          title='Vyzn'
+          ref={appFrameRef}
+          title='App IFrame'
           src={frameSource}
           style={{
             border: 'solid 3px red',
-            width: '20em',
-            height: '20em',
+            width: '60em',
+            height: '40em',
           }}
         ></iframe>
       </div>
     </>
   )
+}
+
+
+/**
+ * @param {boolean} invalid
+ * @param {msg} msg
+ */
+function check(invalid, msg) {
+  if (invalid) {
+    throw new Error(msg)
+  }
+}
+
+
+/** */
+function initMatrix(iframe) {
+  // First we need to set up a listener to ensure we're able to hear the client's requests
+  window.onmessage = function(event) {
+    // First make sure we are roughly in shape to be a widget: we need a parent window to
+    // make sure it's not another tab trying to contact us.
+    check(!window.parent, 'request not from parent; ignoring')
+
+    // Next we validate to make sure the request is a valid shape
+    const request = event.data
+    check(!request, 'Event missing request')
+    check(request['api'] !== 'fromWidget', 'Request not from widget')
+    check(!request['requestId'], 'Request missing requestId')
+    check(!request['action'], 'Request missing action')
+
+    console.log('AppTray.jsx, legit event', event)
+
+    const widgetId = 1
+    if (request['widgetId'] === null) {
+      console.log('AppTray: assigning widget ID')
+      window.parent.postMessage({
+        ...request, // include the original request
+        response: {
+          action: 'capabilities',
+          widgetId,
+        },
+      }, event.origin)
+    } else if (request['capabilities'] === 'capabilities') {
+      console.log('GOT A CAPABLE APP:', request)
+    } else {
+      // We'll send an error response for this. Ideally we'd do a full implementation
+      // of the widget API, but that is out of scope for this tutorial.
+      window.parent.postMessage({
+        ...request, // include the original request
+        response: {
+          error: {
+            message: 'Action not supported',
+          },
+        },
+      }, event.origin)
+    }
+  }
+
+
+  // console.log('AppTray.jsx, sending initial message')
+  // window.parent.postMessage({action: 'capabilities'})
 }
