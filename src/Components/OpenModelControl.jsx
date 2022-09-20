15c124d485bbf8d6be34e4091751cfe05ec8f585
OlegMoshkovich <oleg.mosh@gmail.com>
2022-09-20 13:10:57 +0200

    add camera positions to the sample projects

--- src/Components/OpenModelControl.jsx
+++ src/Components/OpenModelControl.jsx
@@ -69,13 +69,13 @@ function OpenModelDialog({isDialogDisplayed, setIsDialogDisplayed, fileOpen}) {
     setSelected(e.target.value)
     const modelPath = {
       1: '/share/v/gh/OlegMoshkovich/BLDRS_models/main/haus.ifc',
-      2: '/share/v/gh/Swiss-Property-AG/Portfolio/main/ASTRA.ifc',
-      3: '/share/v/gh/Swiss-Property-AG/Portfolio/main/EISVOGEL.ifc',
-      4: '/share/v/gh/Swiss-Property-AG/Portfolio/main/KNIK.ifc',
+      2: '/share/v/gh/Swiss-Property-AG/Portfolio/main/ASTRA.ifc#c:-81.42,13,62.13,26.05,15.66,-12.89',
+      3: '/share/v/gh/Swiss-Property-AG/Portfolio/main/EISVOGEL.ifc#c:96.08,3.46,130.78,2.76,2.45,17.22',
+      4: '/share/v/gh/Swiss-Property-AG/Portfolio/main/KNIK.ifc#c:-28.69,7.17,40.11,-5.02,3.35,0.61',
       5: '/share/v/gh/Swiss-Property-AG/Portfolio/main/MOMENTUM%20TINYHOUSE.ifc',
       6: '/share/v/gh/Swiss-Property-AG/Portfolio/main/NIEDERSCHERLI.ifc',
       // eslint-disable-next-line max-len
-      7: '/share/v/gh/sujal23ks/BCF/main/packages/fileimport-service/ifc/ifcs/171210AISC_Sculpture_brep.ifc/120010/120020/120023/5007/2907#c:-115.5,-36.4,109.55,0,-41.93,13.88',
+      7: '/share/v/gh/sujal23ks/BCF/main/packages/fileimport-service/ifc/ifcs/171210AISC_Sculpture_brep.ifc/120010/120020/120023/4998/2867#c:-163.46,16.12,223.99,12.03,-28.04,-15.28',
       // eslint-disable-next-line max-len
       8: '/share/v/gh/Alhakam/BIMsage/master/BIMsage-Source/BIMsage/src/test/resources/ontology/20200121_Promnitz_Stones.ifc/70/91/116/131/80830#c:-7.58,-3.45,0,0.11,-1.73,-0.32',
 
