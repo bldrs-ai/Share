61a8e776ad76a28ab2fa5e26b19b7ea09c1799b3
Omachonu Ogali <oo@bldrs.ai>
2022-10-06 14:33:39 -0400

    Disable capture of test screenshots and videos

--- cypress.config.js
+++ cypress.config.js
@@ -7,5 +7,7 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    screenshotOnRunFailure: false,
+    video: false,
   },
 })
