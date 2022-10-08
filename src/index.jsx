78e8b8fe1b2ecee2b68756ca8b183ca0aaacba4f
Omachonu Ogali <oo@bldrs.ai>
2022-10-08 10:42:55 -0400

    Add handler to filter unhandled mock requests
    We only care if an unexpected call is made to api.github.com, as opposed
    to the default of raising a console warning about everything.
    
    The presence of a MSW console error/warning means that we are trying to
    exercise GitHub API functionality that we haven't yet mocked.
    
    Currently, we're raising a console error but as our confidence
    increases, we can change this to throw an exception that will halt
    execution.

--- src/index.jsx
+++ src/index.jsx
@@ -6,7 +6,13 @@ import BaseRoutes from './BaseRoutes'
 
 if (process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test') {
   const {worker} = require('./__mocks__/browser')
-  worker.start()
+  worker.start({
+    onUnhandledRequest(req) {
+      if (req.url.host === 'api.github.com') {
+        console.error(`Found an unhandled ${req.method} request to ${req.url}`)
+      }
+    },
+  })
 }
 
 const root = createRoot(document.getElementById('root'))
