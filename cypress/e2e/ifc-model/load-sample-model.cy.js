28151d92c7b82920907b36ff263a00312698f0b5
Omachonu Ogali <oo@bldrs.ai>
2022-10-17 23:42:24 -0400

    Set a timeout for finding our async components

--- cypress/e2e/ifc-model/load-sample-model.cy.js
+++ cypress/e2e/ifc-model/load-sample-model.cy.js
@@ -3,27 +3,28 @@ describe('sample models', () => {
     it('should display tooltip when hovering', () => {
       cy.setCookie('isFirstTime', 'false')
       cy.visit('/')
-      cy.get('form').findByRole('button', {name: /Open IFC/}).realHover()
+      cy.get('form').findByRole('button', {name: /Open IFC/, timeout: 5000}).realHover()
       cy.findByRole('tooltip').contains('Open IFC')
     })
 
     it('should display the sample models dialog', () => {
       cy.setCookie('isFirstTime', 'false')
       cy.visit('/')
-      cy.get('form').findByRole('button', {name: /Open IFC/}).realClick()
       cy.findByRole('dialog')
+      cy.get('form').findByRole('button', {name: /Open IFC/, timeout: 5000}).realClick()
     })
 
     it('should load the Momentum model when selected', () => {
       cy.setCookie('isFirstTime', 'false')
       cy.visit('/')
-      cy.get('form').findByRole('button', {name: /Open IFC/}).realClick()
+      cy.get('form').findByRole('button', {name: /Open IFC/, timeout: 5000}).realClick()
       cy.findByRole('button', {name: /Sample Projects/}).realClick()
       cy.findByRole('listbox').within(() => {
         cy.findByRole('option', {name: /Momentum/}).realClick()
       })
+      cy.findByRole('listbox').should('not.exist')
       cy.findByRole('tree', {label: /IFC Navigator/})
-      cy.findByText('Momentum / KNIK v3', {timeout: 10000})
+      cy.findByText('Momentum / KNIK v3', {timeout: 5000})
     })
   })
 })
