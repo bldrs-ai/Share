873be508cab1efce02768b917e44dd6e4b925c02
RyugaRyuzaki <ryuzaki2005@gmail.com>
2022-10-18 16:17:25 +0700

    fix 2 option type camera navCube

--- src/Components/NavCube/NavCubeMaterial.js
+++ src/Components/NavCube/NavCubeMaterial.js
@@ -1,56 +1,59 @@
-import {MeshLambertMaterial, MeshBasicMaterial, DoubleSide, Color} from 'three'
-
+import { MeshLambertMaterial, MeshBasicMaterial, DoubleSide, Color, LineBasicMaterial } from "three";
 
 export const LightColor = {
-  light: 0x404040,
-  normalCube: new Color('#e0e0e0'),
-  hoverCube: new Color('#e0e0e0'),
-  textCube: new Color('#459A47'),
-  refPlan: new Color('rgb(245, 245, 245)'),
-  ring: new Color('rgb(123, 130, 28)'),
-}
+	light: 0x404040,
+	normalCube: new Color("rgb(255, 255, 255)"),
+	hoverCube: new Color("rgb(14, 237, 51)"),
+	textCube: new Color("rgb(255, 0, 0)"),
+	refPlan: new Color("rgb(245, 245, 245)"),
+	ring: new Color("rgb(123, 130, 28)"),
+};
 export const NavCubeMaterial = {
-  mainCube: (textTex) => {
-    return new MeshBasicMaterial({
-      color: 'skyblue',
-      map: textTex,
-      wireframe: true,
-    })
-  },
-  normalCube: new MeshBasicMaterial({
-    transparent: true,
-    opacity: 1,
-    color: LightColor.normalCube,
-    depthTest: true,
-    // wireframe: true,
-  }),
-  hoverCube: new MeshLambertMaterial({
-    transparent: true,
-    opacity: 1,
-    color: LightColor.hoverCube,
-    depthTest: true,
-  }),
-  textCube: new MeshLambertMaterial({
-    transparent: true,
-    opacity: 1,
-    color: LightColor.textCube,
-    depthTest: true,
-  }),
+	mainCube: (textTex) => {
+		return new MeshBasicMaterial({
+			color: "skyblue",
+			map: textTex,
+			wireframe: true,
+		});
+	},
+	normalCube: new MeshBasicMaterial({
+		transparent: true,
+		opacity: 1,
+		color: LightColor.normalCube,
+		depthTest: true,
+		// wireframe: true,
+	}),
+	hoverCube: new MeshLambertMaterial({
+		transparent: true,
+		opacity: 1,
+		color: LightColor.hoverCube,
+		depthTest: true,
+	}),
+	textCube: new MeshLambertMaterial({
+		transparent: true,
+		opacity: 1,
+		color: LightColor.textCube,
+		depthTest: true,
+	}),
 
-  ring: new MeshLambertMaterial({
-    transparent: true,
-    opacity: 0.5,
-    color: LightColor.hoverCube,
-    side: DoubleSide,
-    depthWrite: true,
-    depthTest: true,
-  }),
-  textRing: new MeshLambertMaterial({
-    transparent: true,
-    opacity: 1,
-    color: LightColor.ring,
-    side: DoubleSide,
-    depthWrite: true,
-    depthTest: true,
-  }),
-}
+	ring: new MeshLambertMaterial({
+		transparent: true,
+		opacity: 0.5,
+		color: LightColor.hoverCube,
+		side: DoubleSide,
+		depthWrite: true,
+		depthTest: true,
+	}),
+	textRing: new MeshLambertMaterial({
+		transparent: true,
+		opacity: 1,
+		color: LightColor.ring,
+		side: DoubleSide,
+		depthWrite: true,
+		depthTest: true,
+	}),
+	outLine: new LineBasicMaterial({
+		color: "black",
+		linewidth: 10,
+	}),
+};
