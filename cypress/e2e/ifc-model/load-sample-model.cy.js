513f3ae4e5d5adb7c4fa74cbf517c7f58f085013
Omachonu Ogali <oo@bldrs.ai>
2022-10-17 23:43:10 -0400

    Create before wrapper for removing first time visit flag

--- cypress/e2e/ifc-model/load-sample-model.cy.js
+++ cypress/e2e/ifc-model/load-sample-model.cy.js
@@ -1,22 +1,22 @@
 describe('sample models', () => {
   context('when no model is loaded', () => {
-    it('should display tooltip when hovering', () => {
+    beforeEach(() => {
       cy.setCookie('isFirstTime', 'false')
       cy.visit('/')
+      cy.get('#viewer-container').get('canvas').should('be.visible')
+    })
+
+    it('should display tooltip when hovering', () => {
       cy.get('form').findByRole('button', {name: /Open IFC/, timeout: 5000}).realHover()
       cy.findByRole('tooltip').contains('Open IFC')
     })
 
     it('should display the sample models dialog', () => {
-      cy.setCookie('isFirstTime', 'false')
-      cy.visit('/')
-      cy.findByRole('dialog')
       cy.get('form').findByRole('button', {name: /Open IFC/, timeout: 5000}).realClick()
+      cy.findByRole('dialog').contains('Sample Projects')
     })
 
     it('should load the Momentum model when selected', () => {
-      cy.setCookie('isFirstTime', 'false')
-      cy.visit('/')
       cy.get('form').findByRole('button', {name: /Open IFC/, timeout: 5000}).realClick()
       cy.findByRole('button', {name: /Sample Projects/}).realClick()
       cy.findByRole('listbox').within(() => {
