2d4c53007445504e943483b3cf8e92802e02c263
Omachonu Ogali <oo@bldrs.ai>
2022-10-25 07:48:44 -0400

    Correct capitalization of GitHub

--- src/Components/LoginButton.jsx
+++ src/Components/LoginButton.jsx
@@ -5,11 +5,12 @@ import GitHubIcon from '@mui/icons-material/GitHub'
 
 
 const LoginButton = ({
-  title = 'Log in to Github',
+  title = 'Log in to GitHub',
   placement = 'right',
   ...props
 }) => {
   const {loginWithRedirect} = useAuth0()
+
   return (
     <TooltipIconButton
       title={'Log in with Github'}
