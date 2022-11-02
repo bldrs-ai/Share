8bc5e85a7f03a5be202ecdd6656d5499a3de8c4b
Pablo Mayrgundter <pablo.mayrgundter@gmail.com>
2022-11-02 08:10:09 -0400

    Upgrade storybook (#449)
    * Move issues components into sub-directory and move major components to their own files.
    
    * Upgrade storybook.

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,17 +1,11 @@
 module.exports = {
-  'stories': [
-    '../src/**/*.stories.mdx',
-    '../src/**/*.stories.@(js|jsx|ts|tsx)'
-  ],
-  'addons': [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
-    'storybook-addon-material-ui',
-    'storybook-addon-turbo-build'
-  ],
-  'framework': '@storybook/react',
+  'stories': ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  'addons': ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions', 'storybook-addon-material-ui', 'storybook-addon-turbo-build'],
+  'framework': {
+    name: '@storybook/react-webpack5',
+    options: {}
+  },
   'features': {
     'modernInlineRender': true
   }
-}
+};
