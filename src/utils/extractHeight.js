87416e6e70b33148996ea08dda968d9af6367fa0
Mohammad <mkeshavarzi@berkeley.edu>
2022-11-03 06:42:24 -0700

    Integrate Unit Calculations

--- src/utils/extractHeight.js
+++ src/utils/extractHeight.js
@@ -9,12 +9,32 @@ export async function extractHeight(ifcModel) {
     const ifcBuildingStoreyID = 3124254112
     const ifcBuildingStorey = ifcModel.getAllItemsOfType(ifcBuildingStoreyID, true)
 
+    const ifcSIUnitID = 448429030
+    const ifcSIUnit = ifcModel.getAllItemsOfType(ifcSIUnitID, true)
+
+    let unitScale = 1
+
     const printStorey = async () => {
+      const allUnits = await ifcSIUnit
+      for (let i = 0; i < allUnits.length; i++) {
+        if (allUnits[i].UnitType.value === 'LENGTHUNIT') {
+          if (allUnits[i].Prefix.value === 'MILLI') {
+            const milliValue = 0.001
+            unitScale = milliValue
+          }
+          if (allUnits[i].Prefix.value === 'CENTI') {
+            const centiValue = 0.01
+            unitScale = centiValue
+          }
+        }
+      }
+
       const allStor = await ifcBuildingStorey
       const elevValues = []
       for (let i = 0; i < allStor.length; i++) {
-        elevValues[i] = allStor[i].Elevation.value
+        elevValues[i] = allStor[i].Elevation.value * unitScale
       }
+
       return elevValues
     }
     const elevValues = []
