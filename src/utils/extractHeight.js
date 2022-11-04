a74f86c536c0ceb15b37dbb8a26974b8b445e587
Mohammad <mkeshavarzi@berkeley.edu>
2022-11-04 11:40:40 -0700

    Remove using ifcType maps + add no level detection exception

--- src/utils/extractHeight.js
+++ src/utils/extractHeight.js
@@ -1,6 +1,5 @@
-import * as IfcTypesMap from './IfcTypesMap.js'
-
-
+const IFC_TYPE_ID_BUILDING_STOREY = 3124254112
+const IFC_TYPE_ID_SI_UNIT = 448429030
 const SI_PREFIX_VALUE_MILLI = 0.001
 const SI_PREFIX_VALUE_CENTI = 0.01
 
@@ -11,41 +10,43 @@ const SI_PREFIX_VALUE_CENTI = 0.01
  * @return {Array} elevation values
  */
 export async function extractHeight(ifcModel) {
-  const elevValues = []
-  const storeys = await ifcModel.getAllItemsOfType(IfcTypesMap.getId('IFCBUILDINGSTOREY', false), true)
-  const siUnits = await ifcModel.getAllItemsOfType(IfcTypesMap.getId('IFCSIUNIT', false), true)
+  try {
+    const elevValues = []
 
-  let unitScale = 1
+    const storeys = await ifcModel.getAllItemsOfType(IFC_TYPE_ID_BUILDING_STOREY, true)
+    const siUnits = await ifcModel.getAllItemsOfType(IFC_TYPE_ID_SI_UNIT, true)
 
-  console.log(siUnits)
+    let unitScale = 1
 
-  for (let i = 0; i < siUnits.length; i++) {
-    if (siUnits[i].UnitType.value === 'LENGTHUNIT') {
-      try {
-        const prefix = siUnits[i].Prefix.value
-        switch (siUnits[i].Prefix.value) {
-          case 'MILLI': unitScale = SI_PREFIX_VALUE_MILLI; break
-          case 'CENTI': unitScale = SI_PREFIX_VALUE_CENTI; break
-          default: {
-            console.warn('Unhandled length unit: ', prefix)
-            unitScale = 1
+    for (let i = 0; i < siUnits.length; i++) {
+      if (siUnits[i].UnitType.value === 'LENGTHUNIT') {
+        try {
+          const prefix = siUnits[i].Prefix.value
+          switch (prefix) {
+            case 'MILLI': unitScale = SI_PREFIX_VALUE_MILLI; break
+            case 'CENTI': unitScale = SI_PREFIX_VALUE_CENTI; break
+            default: {
+              console.warn('Unhandled length unit: ', prefix)
+              unitScale = 1
+            }
           }
+        } catch {
+          console.warn('No length unit found')
         }
       }
-      catch {
-        console.warn('No length unit found')
+    }
+
+    for (let i = 0; i < storeys.length; i++) {
+      const elevation = storeys[i].Elevation.value
+      if (!isFinite(elevation)) {
+        console.warn('Found invalid elevation value: ', elevation)
+        continue
       }
+      elevValues[i] = elevation * unitScale
     }
-  }
 
-  for (let i = 0; i < storeys.length; i++) {
-    const elevation = storeys[i].Elevation.value
-    if (!isFinite(elevation)) {
-      console.warn('Found invalid elevation value: ', elevation)
-      continue
-    }
-    elevValues[i] = elevation * unitScale
+    return elevValues
+  } catch {
+    console.warn('No levels found')
   }
-
-  return elevValues
 }
