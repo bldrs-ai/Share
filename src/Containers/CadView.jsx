72aeed21b9fe1274b0f86ebbe7dc3f2255c3c609
Pablo Mayrgundter <pablo.mayrgundter@gmail.com>
2022-11-17 18:27:37 +0100

    Fix comments not scrolling, like in #490. (#493)
    * Fix comments not scrolling, like in #490.
    
    * Fix frozen model rotation bug just introduced by last fix.

--- src/Containers/CadView.jsx
+++ src/Containers/CadView.jsx
@@ -568,7 +568,7 @@ const useStyles = makeStyles({
     flexDirection: 'column',
     justifyContent: 'flex-start',
     alignItems: 'flex-start',
-    height: '70%',
+    maxHeight: '95%',
   },
   view: {
     position: 'absolute',
