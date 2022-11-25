bldrs-ai/Share

commit 02e2fc31c9516818aa92ec0b525aab2617bd1666

Cleanup: console logging, debug and testing output, lint. (#486)
* Cleanup console logging in prod: turn off debugging, fix some undefined refs, etc..

* Cleanup lint.

* Cleanup debug and testing output.

* Use highlight colors from theme for pick selections.

* Revert some of the color changes.

src/Components/SideDrawer.jsx
@@ -1,7 +1,7 @@
 import React, {useEffect} from 'react'
 import {useLocation} from 'react-router-dom'
 import Drawer from '@mui/material/Drawer'
-import {makeStyles} from '@mui/styles'
+import {makeStyles, useTheme} from '@mui/styles'
 import useStore from '../store/useStore'
 import {getHashParams} from '../utils/location'
 import {preprocessMediaQuery} from '../utils/mediaQuery'
@@ -29,6 +29,7 @@ export function SideDrawer({
     isPropertiesOn: isPropertiesOn,
   })
   const isMobile = useIsMobile()
+  const theme = useTheme()
 
 
   useEffect(() => {
@@ -61,7 +62,15 @@ export function SideDrawer({
           className={classes.drawer}
         >
           <div className={classes.content}>
-            <div className={classes.containerNotes}>
+            <div
+              sx={{
+                overflow: 'hidden',
+                height: isPropertiesOn ? '50%' : '1200px',
+                display: isCommentsOn ? '' : 'none',
+                borderRadius: '0px',
+                borderBottom: `1px solid ${theme.palette.highlight.heaviest}`,
+              }}
+            >
               {isCommentsOn ? <NotesPanel/> : null}
             </div>
             <div className={classes.divider}/>
@@ -175,13 +184,6 @@ const useStyles = makeStyles((theme, props) => (preprocessMediaQuery(MOBILE_WIDT
   container: {
     borderRadius: '5px',
     overflow: 'hidden',
-  },
-  containerNotes: {
-    overflow: 'hidden ',
-    height: (p) => p.isPropertiesOn ? '50%' : '1200px',
-    display: (p) => p.isCommentsOn ? '' : 'none',
-    borderRadius: '0px',
-    borderBottom: `1px solid ${theme.palette.highlight.heaviest}`,
   },
   containerProperties: {
     borderRadius: '5px',

src/BaseRoutes.test.jsx
@@ -2,10 +2,8 @@ import React from 'react'
 import {render} from '@testing-library/react'
 import MockRoutes from './BaseRoutesMock.test'
 import BaseRoutes from './BaseRoutes'
-import {setDebugLevel} from './utils/debug'
 
 
-setDebugLevel(0)
 /**
  * TODO(pablo): fix flaky test
  * RangeError: /Users/olegmoshkovich/Desktop/builders/Share/node_modules/web-ifc/web-ifc-api.js:

src/Components/BranchesControl.jsx
@@ -56,7 +56,7 @@ export default function Branches() {
     if (branches.length === 0 && modelPath.repo !== undefined) {
       fetchBranches()
     }
-  }, [repository])
+  }, [repository, branches.length, modelPath.branch, modelPath.filepath, modelPath.org, modelPath.repo])
 
 
   const handleSelect = (event) => {

src/Components/Buttons.test.jsx
@@ -7,12 +7,6 @@ import {MockComponent} from '../__mocks__/MockComponent'
 // When this test is ran an error is thrown by the assert as expected therefore the test is passing,
 // but the error is printed on the screen making it look like something is wrong.
 describe('<TooltipIconButton />', () => {
-  test('should throw error if missing required props', () => {
-    expect(() => render(<MockComponent>
-      <TooltipIconButton/>
-    </MockComponent>)).toThrowError('Arg 0 is not defined')
-  })
-
   test('should render successfully', async () => {
     /* eslint-disable no-empty-function */
     const rendered = render(<MockComponent>

src/Components/ExtractLevelsMenu.jsx
@@ -1,4 +1,5 @@
-import React, {useState, useEffect} from 'react'
+import React, {useEffect, useState} from 'react'
+import {useLocation} from 'react-router-dom'
 import Menu from '@mui/material/Menu'
 import MenuItem from '@mui/material/MenuItem'
 import {TooltipIconButton} from './Buttons'
@@ -24,6 +25,7 @@ export default function ExtractLevelsMenu({listOfOptions, icon, title}) {
   const [anchorEl, setAnchorEl] = useState(null)
   const [allLevelsState, setAllLevelsState] = useState([])
   const model = useStore((state) => state.modelStore)
+  const location = useLocation()
   const levelInstance = useStore((state) => state.levelInstance)
   const setLevelInstance = useStore((state) => state.setLevelInstance)
   const setCutPlaneDirection = useStore((state) => state.setCutPlaneDirection)
@@ -36,22 +38,25 @@ export default function ExtractLevelsMenu({listOfOptions, icon, title}) {
   const ceilingOffset = 0.4
 
   useEffect(() => {
-    const planeHash = getHashParams(location, 'p')
-    const fetchFloors = async () => {
-      const allLevels = await extractHeight(model)
-      setAllLevelsState(allLevels)
-      if (planeHash && model && viewer ) {
-        const levelHash = planeHash.split(':')[1]
-        if (isNumeric(levelHash)) {
-          const level = parseInt(levelHash)
-          createFloorplanPlane(allLevels[level] + floorOffset, allLevels[level + 1] - ceilingOffset, level)
+    // TODO(pablo): need to test getAllItemsOfType since it's null in
+    // our mock.  Don't know how to mock the async function correctly.
+    if (model && model.getAllItemsOfType) {
+      const planeHash = getHashParams(location, 'p')
+      const fetchFloors = async () => {
+        const allLevels = await extractHeight(model)
+        setAllLevelsState(allLevels)
+        if (planeHash && model && viewer) {
+          const levelHash = planeHash.split(':')[1]
+          if (isNumeric(levelHash)) {
+            const level = parseInt(levelHash)
+            createFloorplanPlane(allLevels[level] + floorOffset, allLevels[level + 1] - ceilingOffset, level)
+          }
         }
       }
+      fetchFloors()
     }
-    fetchFloors()
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [model])
-
 
   const createFloorplanPlane = (planeHeightBottom, planeHeightTop, level) => {
     removePlanes(viewer)

src/Containers/CadView.jsx
@@ -151,7 +151,7 @@ export default function CadView({
     const preselectMat = new MeshLambertMaterial({
       transparent: true,
       opacity: 0.5,
-      color: theme.palette.highlight.light,
+      color: theme.palette.highlight.secondary,
       depthTest: true,
     })
     const selectMat = new MeshLambertMaterial({
@@ -393,7 +393,7 @@ export default function CadView({
   async function onElementSelect(expressId) {
     const lookupElt = elementsById[parseInt(expressId)]
     if (!lookupElt) {
-      console.error(`CadView#onElementSelect(${expressId}) missing in table:`, elementsById)
+      debug().error(`CadView#onElementSelect(${expressId}) missing in table:`, elementsById)
       return
     }
     await selectItemsInScene([expressId])

src/Share.jsx
@@ -59,7 +59,6 @@ export default function Share({installPrefix, appPrefix, pathPrefix}) {
     // TODO(pablo): currently expect these to both be defined.
     const {org, repo} = urlParams
     if (org && repo) {
-      console.log(`Setting GH repo ${org}/${repo}`)
       setRepository(org, repo)
     } else if (pathPrefix.startsWith('/share/v/p')) {
       debug().log('Setting default repo pablo-mayrgundter/Share')
@@ -67,7 +66,7 @@ export default function Share({installPrefix, appPrefix, pathPrefix}) {
     } else {
       console.warn('No repository set for project!', pathPrefix)
     }
-  }, [appPrefix, installPrefix, modelPath, pathPrefix, setRepository, urlParams])
+  }, [appPrefix, installPrefix, modelPath, pathPrefix, setRepository, urlParams, setModelPath])
 
 
   const {theme, colorMode} = useTheme()

src/Theme.jsx
@@ -95,7 +95,7 @@ function loadTheme(mode) {
     },
     highlight: {
       main: darkGreen,
-      secondary: lightGreen,
+      secondary: darkGreen,
       heavy: grey[700],
       heavier: grey[600],
       heaviest: grey[500],

commit aa29d8cdd4623db240a9f6b15443bd95a305b858

Fix some regressions with SideDrawer from #486.

src/Components/SideDrawer.jsx
@@ -1,6 +1,7 @@
 import React, {useEffect} from 'react'
 import {useLocation} from 'react-router-dom'
 import Drawer from '@mui/material/Drawer'
+import {Box} from '@mui/system'
 import {makeStyles, useTheme} from '@mui/styles'
 import useStore from '../store/useStore'
 import {getHashParams} from '../utils/location'
@@ -62,17 +63,22 @@ export function SideDrawer({
           className={classes.drawer}
         >
           <div className={classes.content}>
-            <div
-              sx={{
-                height: isPropertiesOn ? '50%' : '100%',
-                display: isCommentsOn ? '' : 'none',
-                borderRadius: '0px',
-                borderBottom: `1px solid ${theme.palette.highlight.heaviest}`,
-              }}
-            >
-              {isCommentsOn ? <NotesPanel/> : null}
-            </div>
-            <div className={classes.divider}/>
+            {isCommentsOn ?
+             (
+               <Box
+                 sx={{
+                   height: isPropertiesOn ? '50%' : '100%',
+                   display: isCommentsOn ? 'block' : 'none',
+                   borderRadius: '0px',
+                   borderBottom: `1px solid ${theme.palette.highlight.heaviest}`,
+                   overflowY: 'scroll',
+                 }}
+               >
+                 <NotesPanel/>
+               </Box>
+             ) : null
+            }
+            {(isCommentsOn && isPropertiesOn) ? <div className={classes.divider}/> : null}
             <div className={classes.containerProperties}>
               {isPropertiesOn ? <PropertiesPanel/> : null}
             </div>
@@ -170,6 +176,7 @@ const useStyles = makeStyles((theme, props) => (preprocessMediaQuery(MOBILE_WIDT
     },
   },
   content: {
+    'height': '100%',
     'marginTop': '20px',
     'display': 'flex',
     'flexDirection': 'column',