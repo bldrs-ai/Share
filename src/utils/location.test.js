3ecd9f5154c7eb850b60341420e36ce3ad8f0a65
Omachonu Ogali <oo@bldrs.ai>
2022-12-08 10:52:04 -0500

    Refactor location tests to use Location type

--- src/utils/location.test.js
+++ src/utils/location.test.js
@@ -24,82 +24,99 @@ const newTestLocation = () => ({
 })
 
 test('addHashParams', () => {
-  let loc
+  const loc = newTestLocation()
 
-  loc = {hash: ''}
+  loc.hash = ''
   addHashParams(loc, 'test', {a: 1})
   expect(loc.hash).toBe('test:1')
 
-  loc = {hash: '#'}
+  loc.hash = '#'
   addHashParams(loc, 'test', {a: 1})
   expect(loc.hash).toBe('test:1')
 
-  loc = {hash: '#'}
+  loc.hash = '#'
   addHashParams(loc, 'test', {a: 1}, true) // true: includeNames
   expect(loc.hash).toBe('test:a=1')
 
-  loc = {hash: '#test:a=0'}
+  loc.hash = '#test:a=0'
   addHashParams(loc, 'test', {a: 1}, true)
   expect(loc.hash).toBe('test:a=1')
 
-  loc = {hash: '#test:b=0'}
+  loc.hash = '#test:b=0'
   addHashParams(loc, 'test', {a: 1}, true)
   expect(loc.hash).toBe('test:a=1')
 })
 
 
 test('addHashParamsMultiple', () => {
-  let loc
-  loc = {hash: '#other:a=0::otter:b=3'}
+  const loc = newTestLocation()
+
+  loc.hash = '#other:a=0::otter:b=3'
   addHashParams(loc, 'test', {a: 1}, true)
   expect(loc.hash).toBe('other:a=0::otter:b=3::test:a=1')
 
-  loc = {hash: '#other:a=0::test:a=0::otter:b=3'}
+  loc.hash = '#other:a=0::test:a=0::otter:b=3'
   addHashParams(loc, 'test', {a: 1}, true)
   expect(loc.hash).toBe('other:a=0::test:a=1::otter:b=3')
 })
 
 
 test('addHashParams with tilde', () => {
-  const loc = {hash: '#other:a=0::otter:b=3'}
+  const loc = newTestLocation()
+  loc.hash = '#other:a=0::otter:b=3'
+
   addHashParams(loc, 'test', {a: 1}, true)
   expect(loc.hash).toBe('other:a=0::otter:b=3::test:a=1')
 })
 
 
-test('getHashParams', () => {
-  let loc
+describe('getHashParams', () => {
+  const loc = newTestLocation()
 
-  loc = {hash: '#a:1'}
-  expect(getHashParams(loc, 'a')).toBe('a:1')
+  it('', () => {
+    loc.hash = '#a:1'
+    expect(getHashParams(loc, 'a')).toBe('a:1')
+  })
 
-  loc = {hash: '#a:1::b:2'}
-  expect(getHashParams(loc, 'a')).toBe('a:1')
-  expect(getHashParams(loc, 'b')).toBe('b:2')
-  expect(getHashParams(loc, 'c')).toBe(undefined)
+  it('', () => {
+    loc.hash = '#a:1::b:2'
+    expect(getHashParams(loc, 'a')).toBe('a:1')
+    expect(getHashParams(loc, 'b')).toBe('b:2')
+    expect(getHashParams(loc, 'c')).toBe(undefined)
+  })
 })
 
 
-test('removeHashParams', () => {
-  let loc
+describe('removeHashParams', () => {
+  const loc = newTestLocation()
 
-  loc = {hash: '#'}
-  removeHashParams(loc, 'a')
-  expect(loc).toStrictEqual({hash: ''})
+  it('operates on an empty hash', () => {
+    loc.hash = ''
+    removeHashParams(loc, 'a')
+    expect(loc.hash).toStrictEqual('')
+  })
 
-  loc = {hash: '#a:1'}
-  removeHashParams(loc, 'a')
-  expect(loc).toStrictEqual({hash: ''})
+  it('removes the only hash parameter', () => {
+    loc.hash = '#a:1'
+    removeHashParams(loc, 'a')
+    expect(loc.hash).toStrictEqual('')
+  })
 
-  loc = {hash: '#a:1::b:2'}
-  removeHashParams(loc, 'a')
-  expect(loc).toStrictEqual({hash: 'b:2'})
+  it('removes the first of two hash parameters', () => {
+    loc.hash = '#a:1::b:2'
+    removeHashParams(loc, 'a')
+    expect(loc.hash).toStrictEqual('b:2')
+  })
 
-  loc = {hash: '#a:1::b:2'}
-  removeHashParams(loc, 'b')
-  expect(loc).toStrictEqual({hash: 'a:1'})
+  it('removes the second of two hash parameters', () => {
+    loc.hash = '#a:1::b:2'
+    removeHashParams(loc, 'b')
+    expect(loc.hash).toStrictEqual('a:1')
+  })
 
-  loc = {hash: '#a:1::b:2::c:3'}
-  removeHashParams(loc, 'b')
-  expect(loc).toStrictEqual({hash: 'a:1::c:3'})
+  it('removes the second of three hash parameters', () => {
+    loc.hash = '#a:1::b:2::c:3'
+    removeHashParams(loc, 'b')
+    expect(loc.hash).toStrictEqual('a:1::c:3')
+  })
 })
