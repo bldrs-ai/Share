854c3a86e686b95575acdcb78f804ca02424394c
OlegMoshkovich <oleg.mosh@gmail.com>
2022-12-13 04:03:31 +0100

    WIP: Add accept privacy to the about dialog (#437)

--- src/Components/Buttons.jsx
+++ src/Components/Buttons.jsx
@@ -60,16 +60,21 @@ export function RectangularButton({
   title,
   icon,
   onClick,
+  noBorder = true,
+  noBackground = false,
 }) {
   assertDefined(title, icon, onClick)
+  const theme = useTheme()
   return (
     <Button
       onClick={onClick}
       variant='rectangular'
       startIcon={icon}
       sx={{
+        'border': `1px solid ${noBorder ? 'none' : theme.palette.highlight.heavy }`,
+        'backgroundColor': noBackground ? 'none' : theme.palette.highlight.main,
         '& .MuiButton-startIcon': {position: 'absolute', left: '20px'},
-        '&.MuiButtonBase-root:hover': {bgcolor: 'none'},
+        '&.MuiButtonBase-root:hover': {bgcolor: theme.palette.highlight.secondary},
       }}
     >
       {title}
@@ -124,7 +129,7 @@ const useStyles = makeStyles((theme) => ({
     '& button': {
       'width': '40px',
       'height': '40px',
-      'border': 'none ',
+      'border': 'none',
       'margin': '4px 0px 4px 0px',
       '&.Mui-selected, &.Mui-selected:hover': {
         backgroundColor: '#97979720',
---
+++ src/Components/Toggle.jsx
@@ -0,0 +1,31 @@
+import React, {useContext} from 'react'
+import Switch from '@mui/material/Switch'
+import {ColorModeContext} from '../Context/ColorMode'
+
+
+/**
+ * Toggle Switch
+ *
+ * @param {string} installPrefix node
+ * @return {object} React component
+ */
+export default function Toggle({onChange, checked}) {
+  const theme = useContext(ColorModeContext).getTheme()
+  return (
+    <Switch
+      checked={checked}
+      onChange={onChange}
+      sx={{
+        '& .MuiSwitch-thumb': {
+          backgroundColor: theme.palette.highlight.main,
+        },
+        '& .MuiSwitch-track': {
+          backgroundColor: theme.palette.highlight.heavier,
+        },
+        '& .MuiSwitch-switchBase.Mui-checked+.MuiSwitch-track': {
+          backgroundColor: theme.palette.highlight.secondary,
+        },
+      }}
+    />
+  )
+}
