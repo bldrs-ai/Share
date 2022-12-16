9a8fda40fb84fec852d4b95b27303cc37673b18f
ronoyama2023 <gigalots2023@gmail.com>
2022-12-16 13:42:32 -0300

    update Hooks

--- src/Components/Hooks.jsx
+++ src/Components/Hooks.jsx
@@ -1,44 +1,42 @@
-import {useState, useEffect} from 'react'
+import { useState, useEffect } from "react";
 
-
-export const MOBILE_WIDTH = 500
-
+export const MOBILE_WIDTH = 500;
 
 /**
  * @return {boolean} True iff window width <= MOBILE_WIDTH.
  */
 export function useIsMobile() {
-  return useWindowDimensions().width <= MOBILE_WIDTH
+  return useWindowDimensions().width <= MOBILE_WIDTH;
 }
-
 
 /**
  * @return {object} {width, height}
  */
 export function useWindowDimensions() {
-  const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions())
+  const [windowDimensions, setWindowDimensions] = useState(
+    getWindowDimensions()
+  );
 
   useEffect(() => {
     /** Handle resize. */
     function handleResize() {
-      setWindowDimensions(getWindowDimensions())
+      setWindowDimensions(getWindowDimensions());
     }
 
-    window.addEventListener('resize', handleResize)
-    return () => window.removeEventListener('resize', handleResize)
-  }, [])
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
-  return windowDimensions
+  return windowDimensions;
 }
-
 
 /**
  * @return {object} {width, height}
  */
 function getWindowDimensions() {
-  const {innerWidth: width, innerHeight: height} = window
+  const { innerWidth: width, innerHeight: height } = window;
   return {
     width,
     height,
-  }
+  };
 }
