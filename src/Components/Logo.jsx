ce92abd294e3a561d344441c507a6962da298409
ronoyama2023 <gigalots2023@gmail.com>
2022-12-16 13:58:47 -0300

    update Logo styles

--- src/Components/Logo.jsx
+++ src/Components/Logo.jsx
@@ -1,48 +1,45 @@
-import React from 'react'
-import IconButton from '@mui/material/IconButton'
-import Tooltip from '@mui/material/Tooltip'
-import {makeStyles, useTheme} from '@mui/styles'
-import LogoIcon from '../assets/LogoB_4.svg'
-import PkgJson from '../../package.json'
-
+import React from "react";
+import LogoIcon from "../assets/LogoB_4.svg";
+import PkgJson from "../../package.json";
+import { Box, Tooltip, IconButton } from "@mui/material";
 
 /**
- * @param {Function} onClick function triggered when logo is cliked
+ * @param {Function} onClick function triggered when logo is clicked
  * @return {object} React component
  */
-export default function Logo({onClick}) {
-  const classes = useStyles(useTheme())
+export default function Logo({ onClick }) {
   return (
-    <div className={classes.logoGroup}>
-      <Tooltip title={`Bldrs: ${PkgJson.version}`} describeChild placement="right">
+    <Box
+      sx={(theme) => ({
+        position: "fixed",
+        bottom: "-60px",
+        left: "12px",
+        "& svg": {
+          width: "50px",
+          "@media (max-width: 900px)": {
+            width: "50px",
+          },
+          "& .left-face": {
+            fill: theme.palette.primary.light,
+          },
+          "& .right-face": {
+            fill: theme.palette.primary.dark,
+          },
+          "& .edges": {
+            stroke: theme.palette.primary.contrastText,
+          },
+        },
+      })}
+    >
+      <Tooltip
+        title={`Bldrs: ${PkgJson.version}`}
+        describeChild
+        placement="right"
+      >
         <IconButton disableRipple onClick={onClick}>
-          <LogoIcon/>
+          <LogoIcon />
         </IconButton>
       </Tooltip>
-    </div>
-  )
+    </Box>
+  );
 }
-
-
-const useStyles = makeStyles((theme) => ({
-  logoGroup: {
-    'position': 'fixed',
-    'bottom': '-60px',
-    'left': '12px',
-    '& svg': {
-      'width': '50px',
-      '@media (max-width: 900px)': {
-        width: '50px',
-      },
-      '& .left-face': {
-        fill: theme.palette.primary.light,
-      },
-      '& .right-face': {
-        fill: theme.palette.primary.dark,
-      },
-      '& .edges': {
-        stroke: theme.palette.primary.contrastText,
-      },
-    },
-  },
-}))
