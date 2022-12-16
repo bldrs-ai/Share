c04c4f663345203f525a2a260297115ac58097cc
ronoyama2023 <gigalots2023@gmail.com>
2022-12-16 13:33:02 -0300

    update Dialog Redesign styles

--- src/Components/Dialog_redesign.jsx
+++ src/Components/Dialog_redesign.jsx
@@ -1,15 +1,21 @@
-import React from 'react'
-import MuiDialog from '@mui/material/Dialog'
-import DialogContent from '@mui/material/DialogContent'
-import DialogTitle from '@mui/material/DialogTitle'
-import Divider from '@mui/material/Divider'
-import {grey} from '@mui/material/colors'
-import {makeStyles, useTheme} from '@mui/styles'
-import {UilBuilding, UilUpload, UilMultiply, UilGraduationCap, UilGithub} from '@iconscout/react-unicons'
-import {assertDefined} from '../utils/assert'
-import {RectangularButton} from './Buttons'
-import InputBar from './InputBar'
-
+import React from "react";
+import {
+  MuiDialog,
+  DialogContent,
+  DialogTitle,
+  Divider,
+} from "@mui/material/Dialog";
+import { grey } from "@mui/material/colors";
+import {
+  UilBuilding,
+  UilUpload,
+  UilMultiply,
+  UilGraduationCap,
+  UilGithub,
+} from "@iconscout/react-unicons";
+import { assertDefined } from "../utils/assert";
+import { RectangularButton } from "./Buttons";
+import InputBar from "./InputBar";
 
 /**
  * A generic base dialog component.
@@ -26,24 +32,31 @@ export default function Dialog({
   isDialogDisplayed,
   setIsDialogDisplayed,
 }) {
-  assertDefined(headerContent, bodyContent, isDialogDisplayed, setIsDialogDisplayed)
-  const classes = useStyles(useTheme())
-  const close = () => setIsDialogDisplayed(false)
+  assertDefined(
+    headerContent,
+    bodyContent,
+    isDialogDisplayed,
+    setIsDialogDisplayed
+  );
+  const close = () => setIsDialogDisplayed(false);
   return (
-    <MuiDialog
-      open={isDialogDisplayed}
-      onClose={close}
-      maxWidth={'sm'}
-    >
-      <DialogTitle>
-        {headerContent}
-      </DialogTitle>
-      <DialogContent className={classes.contentBody}>
+    <MuiDialog open={isDialogDisplayed} onClose={close} maxWidth={"sm"}>
+      <DialogTitle>{headerContent}</DialogTitle>
+      <DialogContent
+        sx={(theme) => ({
+          height: "400px",
+          maxWidth: "300px",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "space-around",
+          alignContent: "center",
+        })}
+      >
         {bodyContent}
       </DialogContent>
-    </MuiDialog>)
+    </MuiDialog>
+  );
 }
-
 
 /**
  * Content for the open Dialog
@@ -51,33 +64,126 @@ export default function Dialog({
  * @return {object} React component
  */
 export function OpenDialogBodyContent() {
-  const classes = useStyles(useTheme())
   return (
-    <div className={classes.contentBody}>
-      <div className={classes.recommendedContainer}>
-        <div className={classes.recommendedText}>Recommended Method</div>
-        <InputBar startAdorment={<UilGithub/>}/>
-        <div className={classes.fileDescriptionContainer}>
-          <UilGraduationCap className={classes.fileDescriptionIcon}/>
-          <div className={classes.fileDescriptionText}>
+    <Box
+      sx={{
+        height: "400px",
+        maxWidth: "300px",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-around",
+        alignContent: "center",
+      }}
+    >
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignContent: "center",
+        }}
+      >
+        <Box
+          sx={{
+            fontFamily: "Helvetica",
+            marginBottom: "12px",
+            fontWeight: 600,
+            fontSize: "10px",
+            lineHeight: "12px",
+            letterSpacing: "0.14em",
+            textTransform: "uppercase",
+            color: "#0085FF",
+            textAlign: "center",
+          }}
+        >
+          Recommended Method
+        </Box>
+        <InputBar startAdorment={<UilGithub />} />
+        <Box
+          sx={{
+            marginTop: "10px",
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "flex-start",
+            alignContent: "center",
+            cursor: "pointer",
+          }}
+        >
+          <UilGraduationCap
+            sx={{ color: "#979797", width: "13px", height: "13px" }}
+          />
+          <Box
+            sx={{
+              fontFamily: "Helvetica",
+              marginLeft: "5px",
+              width: "200px",
+              color: "#979797",
+              fontSize: "12px",
+            }}
+          >
             How do I host .ifc files on GitHub?
-          </div>
-        </div>
-      </div>
-      <div className={classes.divider}>
-        <Divider/>
-        <div className={classes.dividerText}>or</div>
-      </div>
-      <RectangularButton title='Upload from device' onClick={() => console.log('clicked')} icon={<UilUpload/>}/>
-      <div className={classes.divider}>
-        <Divider/>
-        <div className={classes.dividerText}>or</div>
-      </div>
-      <RectangularButton title='Load Sample Model' onClick={() => console.log('clicked')} icon={<UilBuilding/>}/>
-    </div>
-  )
+          </Box>
+        </Box>
+      </Box>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignContent: "center",
+        }}
+      >
+        <Divider />
+        <Box
+          sx={{
+            fontFamily: "Helvetica",
+            position: "absolute",
+            alignSelf: "center",
+            textAlign: "center",
+            width: "40px",
+            color: "#777777",
+            backgroundColor: grey[100],
+          }}
+        >
+          or
+        </Box>
+      </Box>
+      <RectangularButton
+        title="Upload from device"
+        onClick={() => console.log("clicked")}
+        icon={<UilUpload />}
+      />
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignContent: "center",
+        }}
+      >
+        <Divider />
+        <Box
+          sx={{
+            fontFamily: "Helvetica",
+            position: "absolute",
+            alignSelf: "center",
+            textAlign: "center",
+            width: "40px",
+            color: "#777777",
+            backgroundColor: grey[100],
+          }}
+        >
+          or
+        </Box>
+      </Box>
+      <RectangularButton
+        title="Load Sample Model"
+        onClick={() => console.log("clicked")}
+        icon={<UilBuilding />}
+      />
+    </Box>
+  );
 }
-
 
 /**
  * Title for the open Dialog
@@ -85,107 +191,40 @@ export function OpenDialogBodyContent() {
  * @return {object} React component
  */
 export function OpenDialogHeaderContent() {
-  const classes = useStyles(useTheme())
   return (
-    <div className={classes.titleContainer}>
-      <div className={classes.titleTextContainer}>
-        <div className={classes.titleText}>
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignContent: "center",
+        maxWidth: "500px",
+      }}
+    >
+      <Box
+        sx={{
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignContent: "center",
+        }}
+      >
+        <Box sx={{ fontWeight: "bold", fontSize: "30px" }}>
           Open file
-          <div className={classes.secondarytext}>
+          <Box
+            sx={{
+              fontWeight: 500,
+              fontSize: "14px",
+              lineHeight: "17px",
+              color: "#777777",
+            }}
+          >
             We support .ifc file types
-          </div>
-        </div>
-      </div>
-      <div>
-        <UilMultiply style={{color: '#505050'}}/>
-      </div>
-    </div>
-  )
+          </Box>
+        </Box>
+      </Box>
+      <Box>
+        <UilMultiply style={{ color: "#505050" }} />
+      </Box>
+    </Box>
+  );
 }
-
-
-const useStyles = makeStyles((theme) => ({
-  titleContainer: {
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignContent: 'center',
-    maxWidth: '500px',
-  },
-  titleTextContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignContent: 'center',
-  },
-  titleText: {
-    fontWeight: 'bold',
-    fontSize: '30px',
-  },
-  secondarytext: {
-    fontWeight: 500,
-    fontSize: '14px',
-    lineHeight: '17px',
-    color: '#777777',
-  },
-  contentBody: {
-    height: '400px',
-    maxWidth: '300px',
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'space-around',
-    alignContent: 'center',
-  },
-  divider: {
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignContent: 'center',
-  },
-  dividerText: {
-    fontFamily: 'Helvetica',
-    position: 'absolute',
-    alignSelf: 'center',
-    textAlign: 'center',
-    width: '40px',
-    color: '#777777',
-    backgroundColor: grey[100],
-  },
-  recommendedContainer: {
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignContent: 'center',
-  },
-  recommendedText: {
-    fontFamily: 'Helvetica',
-    marginBottom: '12px',
-    fontWeight: 600,
-    fontSize: '10px',
-    lineHeight: '12px',
-    letterSpacing: '0.14em',
-    textTransform: 'uppercase',
-    color: '#0085FF',
-    textAlign: 'center',
-  },
-  fileDescriptionContainer: {
-    marginTop: '10px',
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'flex-start',
-    alignContent: 'center',
-    cursor: 'pointer',
-  },
-  fileDescriptionText: {
-    fontFamily: 'Helvetica',
-    marginLeft: '5px',
-    width: '200px',
-    color: '#979797',
-    fontSize: '12px',
-  },
-  fileDescriptionIcon: {
-    color: '#979797',
-    width: '13px',
-    height: '13px',
-  },
-}))
-
