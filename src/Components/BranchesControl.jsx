cbb20a3f6af98e693a64fcb0ac597dfa09c0b34d
ronoyama2023 <gigalots2023@gmail.com>
2022-12-16 13:15:58 -0300

    update Branches Control styles

--- src/Components/BranchesControl.jsx
+++ src/Components/BranchesControl.jsx
@@ -1,16 +1,11 @@
-import React, {useContext, useEffect, useState} from 'react'
-import {useNavigate} from 'react-router-dom'
-import TextField from '@mui/material/TextField'
-import MenuItem from '@mui/material/MenuItem'
-import Paper from '@mui/material/Paper'
-import Typography from '@mui/material/Typography'
-import {makeStyles} from '@mui/styles'
-import {ColorModeContext} from '../Context/ColorMode'
-import debug from '../utils/debug'
-import {getBranches} from '../utils/GitHub'
-import useStore from '../store/useStore'
-import {navigateBaseOnModelPath} from '../utils/location'
-
+import React, { useContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { TextField, MenuItem, Paper, Typography } from "@mui/material";
+import { ColorModeContext } from "../Context/ColorMode";
+import debug from "../utils/debug";
+import { getBranches } from "../utils/GitHub";
+import useStore from "../store/useStore";
+import { navigateBaseOnModelPath } from "../utils/location";
 
 /**
  * @param {boolean} isDialogDisplayed
@@ -18,127 +13,129 @@ import {navigateBaseOnModelPath} from '../utils/location'
  * @return {object} React component
  */
 export default function Branches() {
-  const classes = useStyles()
-  const navigate = useNavigate()
-  const colorMode = useContext(ColorModeContext)
-  const repository = useStore((state) => state.repository)
-  const [branches, setBranches] = useState([])
-  const [versionPaths, setVersionPaths] = useState([])
-  const [selected, setSelected] = useState(0)
-  const modelPath = useStore((state) => state.modelPath)
+  const navigate = useNavigate();
+  const colorMode = useContext(ColorModeContext);
+  const repository = useStore((state) => state.repository);
+  const [branches, setBranches] = useState([]);
+  const [versionPaths, setVersionPaths] = useState([]);
+  const [selected, setSelected] = useState(0);
+  const modelPath = useStore((state) => state.modelPath);
 
   useEffect(() => {
     if (!repository) {
-      debug().warn('IssuesControl#Issues: 1, no repo defined')
-      return
+      debug().warn("IssuesControl#Issues: 1, no repo defined");
+      return;
     }
     const fetchBranches = async () => {
       try {
-        const branchesData = await getBranches(repository)
-        const versionPathsTemp = []
+        const branchesData = await getBranches(repository);
+        const versionPathsTemp = [];
         if (branchesData.data.length > 0) {
-          setBranches(branchesData.data)
+          setBranches(branchesData.data);
         }
         branchesData.data.map((branch, i) => {
           if (branch.name === modelPath.branch) {
             // select the current branch
-            setSelected(i)
+            setSelected(i);
           }
-          const versionPath = navigateBaseOnModelPath(modelPath.org, modelPath.repo, branch.name, modelPath.filepath)
-          versionPathsTemp.push(versionPath)
-        })
-        setVersionPaths(versionPathsTemp)
+          const versionPath = navigateBaseOnModelPath(
+            modelPath.org,
+            modelPath.repo,
+            branch.name,
+            modelPath.filepath
+          );
+          versionPathsTemp.push(versionPath);
+        });
+        setVersionPaths(versionPathsTemp);
       } catch (e) {
-        debug().warn('failed to fetch branches', e)
+        debug().warn("failed to fetch branches", e);
       }
-    }
+    };
 
     if (branches.length === 0 && modelPath.repo !== undefined) {
-      fetchBranches()
+      fetchBranches();
     }
-  }, [repository, branches.length, modelPath.branch, modelPath.filepath, modelPath.org, modelPath.repo])
-
+  }, [
+    repository,
+    branches.length,
+    modelPath.branch,
+    modelPath.filepath,
+    modelPath.org,
+    modelPath.repo,
+  ]);
 
   const handleSelect = (event) => {
-    const versionNumber = event.target.value
-    setSelected(versionNumber)
+    const versionNumber = event.target.value;
+    setSelected(versionNumber);
     navigate({
       pathname: versionPaths[versionNumber],
-    })
-  }
+    });
+  };
 
   return (
     <>
-      {branches.length > 1 && modelPath.repo !== undefined &&
-        <Paper elevation={0}
+      {branches.length > 1 && modelPath.repo !== undefined && (
+        <Paper
+          elevation={0}
           sx={{
-            backgroundColor: colorMode.isDay() ? '#E8E8E8' : '#4C4C4C',
-            marginTop: '34px',
-            opacity: .8,
+            backgroundColor: colorMode.isDay() ? "#E8E8E8" : "#4C4C4C",
+            marginTop: "34px",
+            opacity: 0.8,
           }}
         >
           <TextField
-            className={classes.dropDown}
+            sx={(theme) => ({
+              width: "300px",
+              "& .MuiOutlinedInput-input": {
+                color: theme.palette.highlight.grey,
+              },
+              "& .MuiInputLabel-root": {
+                color: theme.palette.highlight.heaviest,
+              },
+              "& .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline": {
+                borderColor: theme.palette.primary.main,
+              },
+              "&:hover .MuiOutlinedInput-input": {
+                color: theme.palette.highlight.heaviest,
+              },
+              // TODO(oleg): connect to props
+              "&:hover .MuiInputLabel-root": {
+                color: theme.palette.highlight.heaviest,
+              },
+              "&:hover .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline":
+                {
+                  borderColor: theme.palette.primary.main,
+                },
+              // TODO(oleg): connect to props
+              "& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-input": {
+                color: theme.palette.highlight.maximum,
+              },
+              // TODO(oleg): connect to props
+              "& .MuiInputLabel-root.Mui-focused": {
+                color: theme.palette.highlight.heaviest,
+              },
+              "& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline":
+                {
+                  borderColor: theme.palette.primary.main,
+                },
+            })}
             onChange={(e) => handleSelect(e)}
-            variant='outlined'
-            label='Git Branches / Project Versions'
+            variant="outlined"
+            label="Git Branches / Project Versions"
             value={selected}
             select
             role="button"
           >
             {branches.map((branch, i) => {
               return (
-                <MenuItem
-                  key={i}
-                  value={i}
-                >
-                  <Typography variant='p'>{branch.name}</Typography>
+                <MenuItem key={i} value={i}>
+                  <Typography variant="p">{branch.name}</Typography>
                 </MenuItem>
-              )
-            })
-            }
+              );
+            })}
           </TextField>
         </Paper>
-      }
+      )}
     </>
-
-  )
+  );
 }
-
-
-const useStyles = makeStyles((theme) => ({
-  dropDown: {
-    'width': '300px',
-    '& .MuiOutlinedInput-input': {
-      color: theme.palette.highlight.grey,
-    },
-    '& .MuiInputLabel-root': {
-      color: theme.palette.highlight.heaviest,
-    },
-    '& .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline': {
-      borderColor: theme.palette.primary.main,
-    },
-    '&:hover .MuiOutlinedInput-input': {
-      color: theme.palette.highlight.heaviest,
-    },
-    // TODO(oleg): connect to props
-    '&:hover .MuiInputLabel-root': {
-      color: theme.palette.highlight.heaviest,
-    },
-    '&:hover .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline': {
-      borderColor: theme.palette.primary.main,
-    },
-    // TODO(oleg): connect to props
-    '& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-input': {
-      color: theme.palette.highlight.maximum,
-    },
-    // TODO(oleg): connect to props
-    '& .MuiInputLabel-root.Mui-focused': {
-      color: theme.palette.highlight.heaviest,
-    },
-    '& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline': {
-      borderColor: theme.palette.primary.main,
-    },
-  },
-}),
-)
