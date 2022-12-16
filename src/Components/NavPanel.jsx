5e31e064b5d68294dfabf51c96a8bbfaa6b5c822
ronoyama2023 <gigalots2023@gmail.com>
2022-12-16 14:14:50 -0300

    update Node Panel styles

--- src/Components/NavPanel.jsx
+++ src/Components/NavPanel.jsx
@@ -1,14 +1,12 @@
-import React, {useContext} from 'react'
-import Paper from '@mui/material/Paper'
-import TreeView from '@mui/lab/TreeView'
-import {makeStyles} from '@mui/styles'
-import NavTree from './NavTree'
-import NodeClosed from '../assets/2D_Icons/NodeClosed.svg'
-import NodeOpen from '../assets/2D_Icons/NodeOpened.svg'
-import useStore from '../store/useStore'
-import {assertDefined} from '../utils/assert'
-import {ColorModeContext} from '../Context/ColorMode'
-
+import React, { useContext } from "react";
+import { Box, Paper } from "@mui/material";
+import TreeView from "@mui/lab/TreeView";
+import NavTree from "./NavTree";
+import NodeClosed from "../assets/2D_Icons/NodeClosed.svg";
+import NodeOpen from "../assets/2D_Icons/NodeOpened.svg";
+import useStore from "../store/useStore";
+import { assertDefined } from "../utils/assert";
+import { ColorModeContext } from "../Context/ColorMode";
 
 /**
  * @param {object} model
@@ -28,88 +26,73 @@ export default function NavPanel({
   setExpandedElements,
   pathPrefix,
 }) {
-  assertDefined(...arguments)
-  const theme = useContext(ColorModeContext)
-  const classes = useStyles({isDay: theme.isDay()})
-  const selectedElements = useStore((state) => state.selectedElements)
+  assertDefined(...arguments);
+  const theme = useContext(ColorModeContext);
+  const selectedElements = useStore((state) => state.selectedElements);
+
   // TODO(pablo): the defaultExpanded array can contain bogus IDs with
   // no error.  Not sure of a better way to pre-open the first few
   // nodes besides hardcoding.
   return (
     <Paper
+      sx={{
+        backgroundColor: theme.isDay() ? "#E8E8E8" : "#4C4C4C",
+        marginTop: "14px",
+        overflow: "auto",
+        width: "300px",
+        opacity: 0.8,
+        justifyContent: "space-around",
+        alignItems: "center",
+        maxHeight: "100%",
+        "@media (max-width: 900px)": {
+          maxHeight: "150px",
+          width: "300px",
+          top: "86px",
+        },
+      }}
       elevation={0}
-      className={classes.root}
-      sx={{backgroundColor: theme.isDay() ? '#E8E8E8' : '#4C4C4C'}}
     >
-      <div className={classes.treeContainer}>
+      <Box
+        sx={{
+          paddingTop: "14px",
+          paddingBottom: "14px",
+          overflow: "auto",
+        }}
+      >
         <TreeView
-          aria-label='IFC Navigator'
-          defaultCollapseIcon={<NodeOpen className={classes.icon} />}
-          defaultExpandIcon={<NodeClosed className={classes.icon} />}
-          sx={{flexGrow: 1, maxWidth: 400, overflowY: 'auto', overflowX: 'hidden'}}
+          aria-label="IFC Navigator"
+          defaultCollapseIcon={
+            <Box
+              sx={{ width: "0.8em", height: "0.8em" }}
+              component={NodeOpen}
+            />
+          }
+          defaultExpandIcon={
+            <Box
+              sx={{
+                width: "0.8em",
+                height: "0.8em",
+              }}
+              component={NodeClosed}
+            />
+          }
+          sx={{
+            flexGrow: 1,
+            maxWidth: 400,
+            overflowY: "auto",
+            overflowX: "hidden",
+          }}
           defaultExpanded={defaultExpandedElements}
           expanded={expandedElements}
           selected={selectedElements}
           onNodeToggle={(event, nodeIds) => {
-            setExpandedElements(nodeIds)
+            setExpandedElements(nodeIds);
           }}
-          key='tree'
+          key="tree"
         >
-          {
-            <NavTree
-              model={model}
-              element={element}
-              pathPrefix={pathPrefix}
-            />
-          }
+          {<NavTree model={model} element={element} pathPrefix={pathPrefix} />}
         </TreeView>
-      </div>
+      </Box>
     </Paper>
-  )
+  );
 }
-
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    'marginTop': '14px',
-    'overflow': 'auto',
-    'width': '300px',
-    'opacity': .8,
-    'justifyContent': 'space-around',
-    'alignItems': 'center',
-    'maxHeight': '100%',
-    '@media (max-width: 900px)': {
-      maxHeight: '150px',
-      width: '300px',
-      top: '86px',
-    },
-  },
-  treeContainer: {
-    paddingTop: '14px',
-    paddingBottom: '14px',
-    overflow: 'auto',
-  },
-  paper: {
-    display: 'flex',
-    overflow: 'auto',
-    flexDirection: 'column',
-    width: '220px',
-  },
-  treeIcon: {
-    width: '30px',
-    height: '30px',
-  },
-  icon: {
-    width: '0.8em',
-    height: '0.8em',
-  },
-  toggleButton: {
-    'position': 'absolute',
-    'top': (props) => `${props.topOffset}px`,
-    'left': '30px',
-    '@media (max-width: 900px)': {
-      left: '20px',
-    },
-  },
-}),
-)
