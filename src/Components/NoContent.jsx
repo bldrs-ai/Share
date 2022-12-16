d4030b7ee200499dbeb166bdab4253e24a5e2c2d
ronoyama2023 <gigalots2023@gmail.com>
2022-12-16 14:16:50 -0300

    update No Content

--- src/Components/NoContent.jsx
+++ src/Components/NoContent.jsx
@@ -1,9 +1,7 @@
-import React, {useContext} from 'react'
-import Box from '@mui/material/Box'
-import Typography from '@mui/material/Typography'
-import {ColorModeContext} from '../Context/ColorMode'
-import AttentionIcon from '../assets/2D_Icons/Attention.svg'
-
+import React, { useContext } from "react";
+import { Box, Typography } from "@mui/material";
+import { ColorModeContext } from "../Context/ColorMode";
+import AttentionIcon from "../assets/2D_Icons/Attention.svg";
 
 /**
  * Loader contains the ItemPanel and allows for
@@ -12,26 +10,27 @@ import AttentionIcon from '../assets/2D_Icons/Attention.svg'
  * @return {object} React component.
  */
 export default function NoContent() {
-  const theme = useContext(ColorModeContext).getTheme()
+  const theme = useContext(ColorModeContext).getTheme();
   return (
-    <Box sx={{width: '100%'}}>
-      <Box sx={{
-        'display': 'flex',
-        'flexDirection': 'column',
-        'justifyContent': 'center',
-        'alignItems': 'center',
-        '& svg': {
-          width: '30px',
-          height: '30px',
-          fill: theme.palette.highlight.secondary,
-        },
-      }}
+    <Box sx={{ width: "100%" }}>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          "& svg": {
+            width: "30px",
+            height: "30px",
+            fill: theme.palette.highlight.secondary,
+          },
+        }}
       >
         <Box>
-          <AttentionIcon/>
+          <AttentionIcon />
         </Box>
-        <Typography variant={'h4'}>no content</Typography>
+        <Typography variant={"h4"}>no content</Typography>
       </Box>
     </Box>
-  )
+  );
 }
