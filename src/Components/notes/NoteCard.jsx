78e470477b988f9e632d35f3172c05ae8f3b793d
ronoyama2023 <gigalots2023@gmail.com>
2022-12-16 12:14:57 -0300

    update Note Card styles

--- src/Components/notes/NoteCard.jsx
+++ src/Components/notes/NoteCard.jsx
@@ -1,25 +1,25 @@
-import React, {useState, useEffect, useContext} from 'react'
-import ReactMarkdown from 'react-markdown'
-import {makeStyles} from '@mui/styles'
-import {ColorModeContext} from '../../Context/ColorMode'
-import useStore from '../../store/useStore'
-import {assertDefined} from '../../utils/assert'
-import {addHashParams, getHashParamsFromHashStr} from '../../utils/location'
-import {isRunningLocally} from '../../utils/network'
-import {findUrls} from '../../utils/strings'
-import {TooltipIconButton} from '../Buttons'
+import React, { useState, useEffect, useContext } from "react";
+import ReactMarkdown from "react-markdown";
+import { makeStyles } from "@mui/styles";
+import { ColorModeContext } from "../../Context/ColorMode";
+import useStore from "../../store/useStore";
+import { assertDefined } from "../../utils/assert";
+import { addHashParams, getHashParamsFromHashStr } from "../../utils/location";
+import { isRunningLocally } from "../../utils/network";
+import { findUrls } from "../../utils/strings";
+import { TooltipIconButton } from "../Buttons";
 import {
   CAMERA_PREFIX,
   addCameraUrlParams,
   setCameraFromParams,
   parseHashParams,
   removeCameraUrlParams,
-} from '../CameraControl'
-import {useIsMobile} from '../Hooks'
-import {NOTE_PREFIX} from './Notes'
-import CameraIcon from '../../assets/2D_Icons/Camera.svg'
-import ShareIcon from '../../assets/2D_Icons/Share.svg'
-
+} from "../CameraControl";
+import { useIsMobile } from "../Hooks";
+import { NOTE_PREFIX } from "./Notes";
+import CameraIcon from "../../assets/2D_Icons/Camera.svg";
+import ShareIcon from "../../assets/2D_Icons/Share.svg";
+import { Box } from "@mui/material";
 
 /**
  * Note card
@@ -39,108 +39,105 @@ import ShareIcon from '../../assets/2D_Icons/Share.svg'
 export default function NoteCard({
   id = null,
   index = null,
-  username = '',
-  title = 'Title',
-  avatarUrl = '',
-  body = '',
-  date = '',
+  username = "",
+  title = "Title",
+  avatarUrl = "",
+  body = "",
+  date = "",
   numberOfComments = null,
   expandedImage = true,
   isComment = false,
 }) {
-  assertDefined(body, id, index)
-  const [expandText, setExpandText] = useState(false)
-  const [expandImage, setExpandImage] = useState(expandedImage)
-  const selectedNoteId = useStore((state) => state.selectedNoteId)
-  const cameraControls = useStore((state) => state.cameraControls)
-  const setSelectedNoteIndex = useStore((state) => state.setSelectedNoteIndex)
-  const setSelectedNoteId = useStore((state) => state.setSelectedNoteId)
-  const setSnackMessage = useStore((state) => state.setSnackMessage)
-  const selected = selectedNoteId === id
-  const bodyWidthChars = 80
-  const textOverflow = body.length > bodyWidthChars
-  const theme = useContext(ColorModeContext)
-  const embeddedCameraParams = findUrls(body)
-      .filter((url) => {
-        if (url.indexOf('#') === -1) {
-          return false
-        }
-        const encoded = getHashParamsFromHashStr(
-            url.substring(url.indexOf('#') + 1),
-            CAMERA_PREFIX)
-        return encoded && parseHashParams(encoded)
-      })
-  const firstCamera = embeddedCameraParams[0] // intentionally undefined if empty
-  const isMobile = useIsMobile()
-
+  assertDefined(body, id, index);
+  const [expandText, setExpandText] = useState(false);
+  const [expandImage, setExpandImage] = useState(expandedImage);
+  const selectedNoteId = useStore((state) => state.selectedNoteId);
+  const cameraControls = useStore((state) => state.cameraControls);
+  const setSelectedNoteIndex = useStore((state) => state.setSelectedNoteIndex);
+  const setSelectedNoteId = useStore((state) => state.setSelectedNoteId);
+  const setSnackMessage = useStore((state) => state.setSnackMessage);
+  const selected = selectedNoteId === id;
+  const bodyWidthChars = 80;
+  const textOverflow = body.length > bodyWidthChars;
+  const theme = useContext(ColorModeContext);
+  const embeddedCameraParams = findUrls(body).filter((url) => {
+    if (url.indexOf("#") === -1) {
+      return false;
+    }
+    const encoded = getHashParamsFromHashStr(
+      url.substring(url.indexOf("#") + 1),
+      CAMERA_PREFIX
+    );
+    return encoded && parseHashParams(encoded);
+  });
+  const firstCamera = embeddedCameraParams[0]; // intentionally undefined if empty
+  const isMobile = useIsMobile();
 
   useEffect(() => {
     if (isMobile) {
-      setExpandImage(false)
+      setExpandImage(false);
     }
-  }, [isMobile])
-
+  }, [isMobile]);
 
   useEffect(() => {
     if (selected && firstCamera) {
-      setCameraFromParams(firstCamera, cameraControls)
+      setCameraFromParams(firstCamera, cameraControls);
     }
-  }, [selected, firstCamera, cameraControls])
-
+  }, [selected, firstCamera, cameraControls]);
 
   /** Selecting a card move the notes to the replies/comments thread. */
   function selectCard() {
-    setSelectedNoteIndex(index)
-    setSelectedNoteId(id)
+    setSelectedNoteIndex(index);
+    setSelectedNoteId(id);
     if (embeddedCameraParams) {
-      setCameraFromParams(firstCamera)
+      setCameraFromParams(firstCamera);
     }
-    addHashParams(window.location, NOTE_PREFIX, {id: id})
+    addHashParams(window.location, NOTE_PREFIX, { id: id });
   }
-
 
   /**
    * Moves the camera to the position specified in the url attached to
    * the issue/comment.
    */
   function showCameraView() {
-    setCameraFromParams(firstCamera, cameraControls)
-    addCameraUrlParams(cameraControls)
+    setCameraFromParams(firstCamera, cameraControls);
+    addCameraUrlParams(cameraControls);
     if (!embeddedCameraParams) {
-      removeCameraUrlParams()
+      removeCameraUrlParams();
     }
   }
-
 
   /**
    * Copies the issue url which contains the issue id, camera position
    * and selected element path.
    */
   function shareIssue() {
-    navigator.clipboard.writeText(window.location)
-    setSnackMessage('The url path is copied to the clipboard')
-    const pauseTimeMs = 5000
-    setTimeout(() => setSnackMessage(null), pauseTimeMs)
+    navigator.clipboard.writeText(window.location);
+    setSnackMessage("The url path is copied to the clipboard");
+    const pauseTimeMs = 5000;
+    setTimeout(() => setSnackMessage(null), pauseTimeMs);
   }
 
-
-  const classes = useStyles({
-    isDay: theme.isDay(),
-    expandText: expandText,
-    select: selected,
-    expandImage: expandImage,
-    isComment: isComment,
-  })
-
-
   return (
-    <div className={classes.container}>
-      <div
-        className={classes.selectionContainer}
-        role='button'
+    <Box
+      sx={(theme) => ({
+        marginBottom: "1em",
+        backgroundColor: theme.isDay() ? "white" : "#383838",
+        borderRadius: "5px",
+        width: "100%",
+        "@media (max-width: 900px)": {
+          width: "350px",
+        },
+      })}
+    >
+      <Box
+        sx={(theme) => ({
+          cursor: isComment ? null : "pointer",
+        })}
+        role="button"
         tabIndex={0}
-        onClick={() => isComment ? null : selectCard()}
-        onKeyPress={() => isComment ? null : selectCard()}
+        onClick={() => (isComment ? null : selectCard())}
+        onKeyPress={() => (isComment ? null : selectCard())}
         data-test-id="selectionContainer"
       >
         <CardTitle
@@ -152,20 +149,36 @@ export default function NoteCard({
           selected={selected}
           onClickSelect={selectCard}
         />
-      </div>
-      <div className={classes.body}>
+      </Box>
+      <Box
+        sx={(theme) => ({
+          height: "auto",
+          margin: "5px",
+          paddingLeft: "5px",
+          fontSize: "1em",
+          lineHeight: "1.3em",
+          // Restore link styling for notes and comments
+          "& a": {
+            color: theme.isDay() ? "black" : "lightGrey",
+            textDecoration: "underline",
+          },
+          "& img": {
+            width: "100%",
+          },
+        })}
+      >
         <ReactMarkdown>{body}</ReactMarkdown>
-      </div>
-      {textOverflow &&
-         <ShowMore
-           expandText={expandText}
-           onClick={(event) => {
-             event.preventDefault()
-             setExpandText(!expandText)
-           }}
-         />
-      }
-      {embeddedCameraParams || numberOfComments > 0 ?
+      </Box>
+      {textOverflow && (
+        <ShowMore
+          expandText={expandText}
+          onClick={(event) => {
+            event.preventDefault();
+            setExpandText(!expandText);
+          }}
+        />
+      )}
+      {embeddedCameraParams || numberOfComments > 0 ? (
         <CardActions
           selectCard={selectCard}
           numberOfComments={numberOfComments}
@@ -173,57 +186,108 @@ export default function NoteCard({
           selected={selected}
           onClickCamera={showCameraView}
           onClickShare={shareIssue}
-        /> : null
-      }
-    </div>
-  )
+        />
+      ) : null}
+    </Box>
+  );
 }
 
-
-const CardTitle = ({avatarUrl, title, username, selected, isComment, date, onClickSelect}) => {
-  const classes = useStyles({isComment: isComment})
-  const dateParts = date.split('T')
+const CardTitle = ({ avatarUrl, title, username, isComment, date }) => {
+  const classes = useStyles({ isComment: isComment });
+  const dateParts = date.split("T");
   return (
-    <div className={classes.titleContainer}>
-      <div className={classes.title}>
-        {
-          isComment ? null : <div >{title}</div>
-        }
-      </div>
-      <div className={classes.titleRightContainer}>
-        <div className={classes.metaDataContainer} style={{marginRight: '10px'}}>
-          <div className={classes.username}>{username}</div>
-          <div className={classes.username}>{dateParts[0]} {dateParts[1]}</div>
-        </div>
-        {!isRunningLocally() ?
-          <img alt={'avatarImage'}
-            className={classes.avatarIcon}
+    <Box
+      sx={(theme) => ({
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        padding: "0.5em",
+        background: isComment ? "#F0F0F0" : "#C8E8C7",
+      })}
+    >
+      <Box sx={(theme) => ({ color: "black", width: "230px" })}>
+        {isComment ? null : <Box>{title}</Box>}
+      </Box>
+      <Box
+        sx={(theme) => ({
+          width: "200px",
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "flex-end",
+          alignItems: "center",
+          marginRight: "6px",
+        })}
+      >
+        <Box
+          sx={(theme) => ({
+            marginRight: "12px",
+            paddingRight: "10px",
+            paddingLeft: "10px",
+            borderRadius: "5px",
+            opacity: 0.5,
+          })}
+          style={{ marginRight: "10px" }}
+        >
+          <Box sx={(theme) => ({ fontSize: "10px", color: "black" })}>
+            {username}
+          </Box>
+          <Box sx={(theme) => ({})} className={classes.username}>
+            {dateParts[0]} {dateParts[1]}
+          </Box>
+        </Box>
+        {!isRunningLocally() ? (
+          <img
+            alt={"avatarImage"}
+            sx={(theme) => ({
+              width: 24,
+              height: 24,
+              borderRadius: "50%",
+              backgroundColor: "lightGrey",
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "center",
+              alignItems: "center",
+              fontWeight: "bold",
+              border: "1px solid lightGrey",
+            })}
             src={avatarUrl}
-          /> :
-          <div
-            className={classes.avatarPlaceholder}
           />
-        }
-      </div>
-    </div>
-  )
-}
+        ) : (
+          <Box
+            sx={(theme) => ({
+              width: 24,
+              height: 24,
+              background: "green",
+              borderRadius: "50%",
+            })}
+          />
+        )}
+      </Box>
+    </Box>
+  );
+};
 
-
-const ShowMore = ({onClick, expandText}) => {
-  const classes = useStyles()
+const ShowMore = ({ onClick, expandText }) => {
+  const classes = useStyles();
   return (
-    <div className={classes.showMore}
+    <Box
+      sx={(theme) => ({
+        display: "none",
+        cursor: "pointer",
+        margin: "5px 5px 15px 10px",
+        fontSize: "10px",
+        color: theme.palette.highlight.main,
+      })}
       onClick={onClick}
-      role='button'
+      role="button"
       tabIndex={0}
       onKeyPress={onClick}
     >
-      {expandText ? 'show less' : 'show more'}
-    </div>
-  )
-}
-
+      {expandText ? "show less" : "show more"}
+    </Box>
+  );
+};
 
 const CardActions = ({
   onClickCamera,
@@ -231,179 +295,102 @@ const CardActions = ({
   numberOfComments,
   selectCard,
   embeddedCameras,
-  selected}) => {
-  const [shareIssue, setShareIssue] = useState(false)
-  const hasCameras = embeddedCameras.length > 0
-  const classes = useStyles({embeddedCameras: hasCameras})
+  selected,
+}) => {
+  const [shareIssue, setShareIssue] = useState(false);
+  const hasCameras = embeddedCameras.length > 0;
+  const classes = useStyles({ embeddedCameras: hasCameras });
   return (
-    <div className={classes.actions}>
-      <div className={classes.actionsLeftGroup}>
-        {hasCameras &&
-         <TooltipIconButton
-           disabled={hasCameras}
-           title='Show the camera view'
-           size='small'
-           placement='bottom'
-           onClick={onClickCamera}
-           icon={<div className={classes.iconContainer}><CameraIcon/></div>}
-         />}
-        {selected &&
-         <TooltipIconButton
-           title='Share'
-           size='small'
-           placement='bottom'
-           onClick={() => {
-             onClickShare()
-             setShareIssue(!shareIssue)
-           }}
-           icon={<div className={classes.iconContainer}><ShareIcon/></div>}
-         />
-        }
-      </div>
-      <div className={classes.commentsIconContainer}
-        role='button'
+    <Box
+      sx={(theme) => ({
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        padding: "0px 5px 10px 5px",
+        fontSize: "10px",
+      })}
+    >
+      <Box
+        sx={(theme) => ({
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "flex-start",
+          alignItems: "center",
+          fontSize: "10px",
+        })}
+      >
+        {hasCameras && (
+          <TooltipIconButton
+            disabled={hasCameras}
+            title="Show the camera view"
+            size="small"
+            placement="bottom"
+            onClick={onClickCamera}
+            icon={
+              <Box
+                sx={(theme) => ({
+                  width: "20px",
+                  height: "20px",
+                  marginBottom: "2px",
+                })}
+              >
+                <CameraIcon />
+              </Box>
+            }
+          />
+        )}
+        {selected && (
+          <TooltipIconButton
+            title="Share"
+            size="small"
+            placement="bottom"
+            onClick={() => {
+              onClickShare();
+              setShareIssue(!shareIssue);
+            }}
+            icon={
+              <Box sx={(theme) => ({})} className={classes.iconContainer}>
+                <ShareIcon />
+              </Box>
+            }
+          />
+        )}
+      </Box>
+      <Box
+        sx={(theme) => ({
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "center",
+          alignItems: "center",
+          marginRight: "4px",
+        })}
+        role="button"
         tabIndex={0}
         onClick={selectCard}
         onKeyPress={selectCard}
       >
-        {numberOfComments > 0 &&
-          <div className={classes.commentsQuantity}>{numberOfComments}</div>
-        }
-      </div>
-    </div>
-  )
-}
-
-
-const useStyles = makeStyles((theme) => ({
-  container: {
-    'marginBottom': '1em',
-    'backgroundColor': (props) => props.isDay ? 'white' : '#383838',
-    'borderRadius': '5px',
-    'width': '100%',
-    '@media (max-width: 900px)': {
-      width: '350px',
-    },
-  },
-  titleContainer: {
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    padding: '0.5em',
-    background: (props) => props.isComment ? '#F0F0F0' : '#C8E8C7',
-  },
-  titleRightContainer: {
-    width: '200px',
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-    marginRight: '6px',
-  },
-  title: {
-    color: 'black',
-    width: '230px',
-  },
-  metaDataContainer: {
-    marginRight: '12px',
-    paddingRight: '10px',
-    paddingLeft: '10px',
-    borderRadius: '5px',
-    opacity: .5,
-  },
-  selectionContainer: {
-    cursor: (props) => props.isComment ? null : 'pointer',
-  },
-  body: {
-    'height': 'auto',
-    'margin': '5px',
-    'paddingLeft': '5px',
-    'fontSize': '1em',
-    'lineHeight': '1.3em',
-    // Restore link styling for notes and comments
-    '& a': {
-      color: (props) => props.isDay ? 'black' : 'lightGrey',
-      textDecoration: 'underline',
-    },
-    '& img': {
-      width: '100%',
-    },
-  },
-  showMore: {
-    display: 'none',
-    cursor: 'pointer',
-    margin: '5px 5px 15px 10px',
-    fontSize: '10px',
-    color: theme.palette.highlight.main,
-  },
-  actions: {
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    padding: '0px 5px 10px 5px',
-    fontSize: '10px',
-  },
-  actionsLeftGroup: {
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'flex-start',
-    alignItems: 'center',
-    fontSize: '10px',
-  },
-  commentsIconContainer: {
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginRight: '4px',
-  },
-  avatarIcon: {
-    width: 24,
-    height: 24,
-    borderRadius: '50%',
-    backgroundColor: 'lightGrey',
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-    fontWeight: 'bold',
-    border: '1px solid lightGrey',
-  },
-  avatarPlaceholder: {
-    width: 24,
-    height: 24,
-    background: 'green',
-    borderRadius: '50%',
-  },
-  commentsQuantity: {
-    width: 16,
-    height: 16,
-    borderRadius: '50%',
-    backgroundColor: 'white',
-    border: '1px solid lightGrey',
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-    fontWeight: 'bold',
-    color: 'black',
-    cursor: 'pointer',
-  },
-  select: {
-    borderRadius: '6px',
-    cursor: 'pointer',
-    marginRight: '2px',
-  },
-  username: {
-    fontSize: '10px',
-    color: 'black',
-  },
-  iconContainer: {
-    width: '20px',
-    height: '20px',
-    marginBottom: '2px',
-  },
-}),
-)
+        {numberOfComments > 0 && (
+          <Box
+            sx={(theme) => ({
+              width: 16,
+              height: 16,
+              borderRadius: "50%",
+              backgroundColor: "white",
+              border: "1px solid lightGrey",
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "center",
+              alignItems: "center",
+              fontWeight: "bold",
+              color: "black",
+              cursor: "pointer",
+            })}
+          >
+            {numberOfComments}
+          </Box>
+        )}
+      </Box>
+    </Box>
+  );
+};
