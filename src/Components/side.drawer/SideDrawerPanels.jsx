10524394078a2a38e0aa71f58cf19d08c8fe1b88
ronoyama2023 <gigalots2023@gmail.com>
2022-12-16 13:01:53 -0300

    update Side Drawer Panels styles

--- src/Components/side.drawer/SideDrawerPanels.jsx
+++ src/Components/side.drawer/SideDrawerPanels.jsx
@@ -1,14 +1,11 @@
-import React from 'react'
-import Box from '@mui/material/Box'
-import Typography from '@mui/material/Typography'
-import {makeStyles, useTheme} from '@mui/styles'
-import useStore from '../../store/useStore'
-import ItemProperties from '../item.properties/ItemProperties'
-import {TooltipIconButton} from '../Buttons'
-import Notes from '../notes/Notes'
-import NotesNavBar from '../notes/NotesNavBar'
-import CloseIcon from '../../assets/2D_Icons/Close.svg'
-
+import React from "react";
+import { Box, Typography } from "@mui/material";
+import useStore from "../../store/useStore";
+import ItemProperties from "../item.properties/ItemProperties";
+import { TooltipIconButton } from "../Buttons";
+import Notes from "../notes/Notes";
+import NotesNavBar from "../notes/NotesNavBar";
+import CloseIcon from "../../assets/2D_Icons/Close.svg";
 
 /**
  * Panel Title
@@ -17,18 +14,22 @@ import CloseIcon from '../../assets/2D_Icons/Close.svg'
  * @param {object} controlsGroup Controls Group is placed on the right of the title
  * @return {object} Properties Panel react component
  */
-function PanelTitle({title, controlsGroup}) {
-  const classes = useStyles(useTheme())
+function PanelTitle({ title, controlsGroup }) {
   return (
-    <div className={classes.titleContainer}>
-      <Typography variant='h2'>
-        {title}
-      </Typography>
+    <Box
+      sx={(theme) => ({
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
+        borderRadius: "5px",
+      })}
+    >
+      <Typography variant="h2">{title}</Typography>
       {controlsGroup}
-    </div>
-  )
+    </Box>
+  );
 }
-
 
 /**
  * PropertiesPanel is a wrapper for the item properties component.
@@ -37,115 +38,77 @@ function PanelTitle({title, controlsGroup}) {
  * @return {object} Properties Panel react component
  */
 export function PropertiesPanel() {
-  const toggleIsPropertiesOn = useStore((state) => state.toggleIsPropertiesOn)
-  const selectedElement = useStore((state) => state.selectedElement)
-  const classes = useStyles(useTheme())
+  const toggleIsPropertiesOn = useStore((state) => state.toggleIsPropertiesOn);
+  const selectedElement = useStore((state) => state.selectedElement);
   // TODO(pablo): this render was sometimes coming up with a react
   // error where createElement is undefined.  I've refactored a little
   // and now can't reproduce.
   return (
     <>
       <PanelTitle
-        title='Properties'
+        title="Properties"
         controlsGroup={
-          <div>
+          <Box>
             <TooltipIconButton
-              title='toggle drawer'
+              title="toggle drawer"
               onClick={toggleIsPropertiesOn}
-              icon={<div className={classes.iconContainerClose}><CloseIcon/></div>}
+              icon={
+                <Box
+                  sx={(theme) => ({
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    width: "14px",
+                    height: "14px",
+                  })}
+                >
+                  <CloseIcon />
+                </Box>
+              }
             />
-          </div>
+          </Box>
         }
       />
-      <div className={classes.contentContainerProperties}>
-        {selectedElement ?
-          <ItemProperties/> :
-          <Box sx={{width: '100%'}}>
-            <Typography
-              variant='h4'
-              sx={{textAlign: 'left'}}
-            >
+      <Box
+        sx={(theme) => ({
+          marginTop: "4px",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          height: "100%",
+          overflow: "scroll",
+        })}
+      >
+        {selectedElement ? (
+          <ItemProperties />
+        ) : (
+          <Box sx={{ width: "100%" }}>
+            <Typography variant="h4" sx={{ textAlign: "left" }}>
               Please select an element
             </Typography>
           </Box>
-        }
-      </div>
+        )}
+      </Box>
     </>
-  )
+  );
 }
-
 
 export const NotesPanel = () => {
-  const classes = useStyles(useTheme())
   return (
     <>
-      <NotesNavBar/>
-      <div className={classes.contentContainerNotes}>
-        <Notes/>
-      </div>
+      <NotesNavBar />
+      <Box
+        sx={(theme) => ({
+          marginTop: "4px",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          height: "100%",
+          overflow: "auto",
+        })}
+      >
+        <Notes />
+      </Box>
     </>
-  )
-}
-
-
-const useStyles = makeStyles((theme) => ({
-  titleContainer: {
-    display: 'flex',
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    borderRadius: '5px',
-  },
-  title: {
-    height: '30px',
-    display: 'flex',
-    fontSize: '1.3em',
-    marginRight: '10px',
-    paddingLeft: '2px',
-    alignItems: 'center',
-  },
-  contentContainerProperties: {
-    marginTop: '4px',
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    height: '100%',
-    overflow: 'scroll',
-  },
-  contentContainerNotes: {
-    marginTop: '4px',
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    height: '100%',
-    overflow: 'auto',
-  },
-  rightGroup: {
-    width: '160px',
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-  },
-  leftGroup: {
-    width: '100px',
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'flex-start',
-    alignItems: 'center',
-  },
-  container: {
-    background: '#7EC43B',
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-  },
-  iconContainerClose: {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    width: '14px',
-    height: '14px',
-  },
-}))
+  );
+};
