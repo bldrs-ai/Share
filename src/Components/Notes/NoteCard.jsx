e9a71e9dbbcb59cf9915a9d04e20accc0121b48d
rondoor124 <rondoor124@gmail.com>
2022-12-20 12:23:07 -0300

    update NoteCard styles to sx

--- src/Components/Notes/NoteCard.jsx
+++ src/Components/Notes/NoteCard.jsx
@@ -1,6 +1,6 @@
 import React, {useState, useEffect, useContext} from 'react'
 import ReactMarkdown from 'react-markdown'
-import {makeStyles} from '@mui/styles'
+import {Box, useTheme} from '@mui/material'
 import {ColorModeContext} from '../../Context/ColorMode'
 import useStore from '../../store/useStore'
 import {assertDefined} from '../../utils/assert'
@@ -50,6 +50,7 @@ export default function NoteCard({
 }) {
   assertDefined(body, id, index)
   const [expandText, setExpandText] = useState(false)
+  // eslint-disable-next-line no-unused-vars
   const [expandImage, setExpandImage] = useState(expandedImage)
   const selectedNoteId = useStore((state) => state.selectedNoteId)
   const cameraControls = useStore((state) => state.cameraControls)
@@ -59,7 +60,7 @@ export default function NoteCard({
   const selected = selectedNoteId === id
   const bodyWidthChars = 80
   const textOverflow = body.length > bodyWidthChars
-  const theme = useContext(ColorModeContext)
+  const colorTheme = useContext(ColorModeContext)
   const embeddedCameraParams = findUrls(body)
       .filter((url) => {
         if (url.indexOf('#') === -1) {
@@ -124,19 +125,22 @@ export default function NoteCard({
   }
 
 
-  const classes = useStyles({
-    isDay: theme.isDay(),
-    expandText: expandText,
-    select: selected,
-    expandImage: expandImage,
-    isComment: isComment,
-  })
-
-
   return (
-    <div className={classes.container}>
-      <div
-        className={classes.selectionContainer}
+    <Box sx={{
+      'marginBottom': '1em',
+      'backgroundColor': colorTheme.isDay() ? 'white' : '#383838',
+      'borderRadius': '5px',
+      'width': '100%',
+      '@media (max-width: 900px)': {
+        width: '350px',
+      },
+    }}
+    >
+      <Box
+        sx={{
+          cursor: isComment ? null : 'pointer',
+
+        }}
         role='button'
         tabIndex={0}
         onClick={() => isComment ? null : selectCard()}
@@ -152,18 +156,33 @@ export default function NoteCard({
           selected={selected}
           onClickSelect={selectCard}
         />
-      </div>
-      <div className={classes.body}>
+      </Box>
+      <Box sx={{
+        'height': 'auto',
+        'margin': '5px',
+        'paddingLeft': '5px',
+        'fontSize': '1em',
+        'lineHeight': '1.3em',
+        // Restore link styling for notes and comments
+        '& a': {
+          color: colorTheme.isDay() ? 'black' : 'lightGrey',
+          textDecoration: 'underline',
+        },
+        '& img': {
+          width: '100%',
+        },
+      }}
+      >
         <ReactMarkdown>{body}</ReactMarkdown>
-      </div>
+      </Box>
       {textOverflow &&
-         <ShowMore
-           expandText={expandText}
-           onClick={(event) => {
-             event.preventDefault()
-             setExpandText(!expandText)
-           }}
-         />
+        <ShowMore
+          expandText={expandText}
+          onClick={(event) => {
+            event.preventDefault()
+            setExpandText(!expandText)
+          }}
+        />
       }
       {embeddedCameraParams || numberOfComments > 0 ?
         <CardActions
@@ -175,52 +194,108 @@ export default function NoteCard({
           onClickShare={shareIssue}
         /> : null
       }
-    </div>
+    </Box>
   )
 }
 
 
 const CardTitle = ({avatarUrl, title, username, selected, isComment, date, onClickSelect}) => {
-  const classes = useStyles({isComment: isComment})
   const dateParts = date.split('T')
   return (
-    <div className={classes.titleContainer}>
-      <div className={classes.title}>
-        {
-          isComment ? null : <div >{title}</div>
-        }
-      </div>
-      <div className={classes.titleRightContainer}>
-        <div className={classes.metaDataContainer} style={{marginRight: '10px'}}>
-          <div className={classes.username}>{username}</div>
-          <div className={classes.username}>{dateParts[0]} {dateParts[1]}</div>
-        </div>
+    <Box sx={{
+      display: 'flex',
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      padding: '0.5em',
+      background: isComment ? '#F0F0F0' : '#C8E8C7',
+    }}
+    >
+      <Box sx={{
+        color: 'black',
+        width: '230px',
+      }}
+      >
+        {isComment ? null : <Box >{title}</Box>}
+      </Box>
+      <Box sx={{
+        width: '200px',
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+        marginRight: '6px',
+      }}
+      >
+        <Box sx={{
+          marginRight: '10px',
+          paddingRight: '10px',
+          paddingLeft: '10px',
+          borderRadius: '5px',
+          opacity: .5,
+        }}
+        >
+          <Box sx={{
+            fontSize: '10px',
+            color: 'black',
+          }}
+          >{username}</Box>
+          <Box sx={{
+            fontSize: '10px',
+            color: 'black',
+          }}
+          >{dateParts[0]} {dateParts[1]}</Box>
+        </Box>
         {!isRunningLocally() ?
           <img alt={'avatarImage'}
-            className={classes.avatarIcon}
+            sx={{
+              width: 24,
+              height: 24,
+              borderRadius: '50%',
+              backgroundColor: 'lightGrey',
+              display: 'flex',
+              flexDirection: 'row',
+              justifyContent: 'center',
+              alignItems: 'center',
+              fontWeight: 'bold',
+              border: '1px solid lightGrey',
+            }}
             src={avatarUrl}
           /> :
-          <div
-            className={classes.avatarPlaceholder}
+          <Box
+            sx={{
+              width: 24,
+              height: 24,
+              background: 'green',
+              borderRadius: '50%',
+            }}
           />
         }
-      </div>
-    </div>
+      </Box>
+    </Box>
   )
 }
 
 
 const ShowMore = ({onClick, expandText}) => {
-  const classes = useStyles()
+  const theme = useTheme()
+
+
   return (
-    <div className={classes.showMore}
-      onClick={onClick}
-      role='button'
-      tabIndex={0}
-      onKeyPress={onClick}
+    <Box sx={{
+      display: 'none',
+      cursor: 'pointer',
+      margin: '5px 5px 15px 10px',
+      fontSize: '10px',
+      color: theme.palette.highlight.main,
+    }}
+    onClick={onClick}
+    role='button'
+    tabIndex={0}
+    onKeyPress={onClick}
     >
       {expandText ? 'show less' : 'show more'}
-    </div>
+    </Box>
   )
 }
 
@@ -234,176 +309,94 @@ const CardActions = ({
   selected}) => {
   const [shareIssue, setShareIssue] = useState(false)
   const hasCameras = embeddedCameras.length > 0
-  const classes = useStyles({embeddedCameras: hasCameras})
   return (
-    <div className={classes.actions}>
-      <div className={classes.actionsLeftGroup}>
+    <Box sx={{
+      display: 'flex',
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      padding: '0px 5px 10px 5px',
+      fontSize: '10px',
+    }}
+    >
+      <Box sx={{
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+        fontSize: '10px',
+      }}
+      >
         {hasCameras &&
-         <TooltipIconButton
-           disabled={hasCameras}
-           title='Show the camera view'
-           size='small'
-           placement='bottom'
-           onClick={onClickCamera}
-           icon={<div className={classes.iconContainer}><CameraIcon/></div>}
-         />}
+          <TooltipIconButton
+            disabled={hasCameras}
+            title='Show the camera view'
+            size='small'
+            placement='bottom'
+            onClick={onClickCamera}
+            icon={
+              <Box sx={{
+                width: '20px',
+                height: '20px',
+                marginBottom: '2px',
+              }}
+              >
+                <CameraIcon />
+              </Box>
+            }
+          />}
         {selected &&
-         <TooltipIconButton
-           title='Share'
-           size='small'
-           placement='bottom'
-           onClick={() => {
-             onClickShare()
-             setShareIssue(!shareIssue)
-           }}
-           icon={<div className={classes.iconContainer}><ShareIcon/></div>}
-         />
+          <TooltipIconButton
+            title='Share'
+            size='small'
+            placement='bottom'
+            onClick={() => {
+              onClickShare()
+              setShareIssue(!shareIssue)
+            }}
+            icon={
+              <Box sx={{
+                width: '20px',
+                height: '20px',
+                marginBottom: '2px',
+              }}
+              >
+                <ShareIcon />
+              </Box>
+            }
+          />
         }
-      </div>
-      <div className={classes.commentsIconContainer}
-        role='button'
-        tabIndex={0}
-        onClick={selectCard}
-        onKeyPress={selectCard}
+      </Box>
+      <Box sx={{
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginRight: '4px',
+      }}
+      role='button'
+      tabIndex={0}
+      onClick={selectCard}
+      onKeyPress={selectCard}
       >
         {numberOfComments > 0 &&
-          <div className={classes.commentsQuantity}>{numberOfComments}</div>
+          <Box sx={{
+            width: '16px',
+            height: '16px',
+            borderRadius: '50%',
+            backgroundColor: 'white',
+            border: '1px solid lightGrey',
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'center',
+            alignItems: 'center',
+            fontWeight: 'bold',
+            color: 'black',
+            cursor: 'pointer',
+          }}
+          >{numberOfComments}</Box>
         }
-      </div>
-    </div>
+      </Box>
+    </Box>
   )
 }
-
-
-const useStyles = makeStyles((theme) => ({
-  container: {
-    'marginBottom': '1em',
-    'backgroundColor': (props) => props.isDay ? 'white' : '#383838',
-    'borderRadius': '5px',
-    'width': '100%',
-    '@media (max-width: 900px)': {
-      width: '350px',
-    },
-  },
-  titleContainer: {
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    padding: '0.5em',
-    background: (props) => props.isComment ? '#F0F0F0' : '#C8E8C7',
-  },
-  titleRightContainer: {
-    width: '200px',
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-    marginRight: '6px',
-  },
-  title: {
-    color: 'black',
-    width: '230px',
-  },
-  metaDataContainer: {
-    marginRight: '12px',
-    paddingRight: '10px',
-    paddingLeft: '10px',
-    borderRadius: '5px',
-    opacity: .5,
-  },
-  selectionContainer: {
-    cursor: (props) => props.isComment ? null : 'pointer',
-  },
-  body: {
-    'height': 'auto',
-    'margin': '5px',
-    'paddingLeft': '5px',
-    'fontSize': '1em',
-    'lineHeight': '1.3em',
-    // Restore link styling for notes and comments
-    '& a': {
-      color: (props) => props.isDay ? 'black' : 'lightGrey',
-      textDecoration: 'underline',
-    },
-    '& img': {
-      width: '100%',
-    },
-  },
-  showMore: {
-    display: 'none',
-    cursor: 'pointer',
-    margin: '5px 5px 15px 10px',
-    fontSize: '10px',
-    color: theme.palette.highlight.main,
-  },
-  actions: {
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    padding: '0px 5px 10px 5px',
-    fontSize: '10px',
-  },
-  actionsLeftGroup: {
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'flex-start',
-    alignItems: 'center',
-    fontSize: '10px',
-  },
-  commentsIconContainer: {
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginRight: '4px',
-  },
-  avatarIcon: {
-    width: 24,
-    height: 24,
-    borderRadius: '50%',
-    backgroundColor: 'lightGrey',
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-    fontWeight: 'bold',
-    border: '1px solid lightGrey',
-  },
-  avatarPlaceholder: {
-    width: 24,
-    height: 24,
-    background: 'green',
-    borderRadius: '50%',
-  },
-  commentsQuantity: {
-    width: 16,
-    height: 16,
-    borderRadius: '50%',
-    backgroundColor: 'white',
-    border: '1px solid lightGrey',
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-    fontWeight: 'bold',
-    color: 'black',
-    cursor: 'pointer',
-  },
-  select: {
-    borderRadius: '6px',
-    cursor: 'pointer',
-    marginRight: '2px',
-  },
-  username: {
-    fontSize: '10px',
-    color: 'black',
-  },
-  iconContainer: {
-    width: '20px',
-    height: '20px',
-    marginBottom: '2px',
-  },
-}),
-)
