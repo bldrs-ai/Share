0793a6d13ea4ed9539d51315344ed88b35e4b8b5
OlegMoshkovich <oleg.mosh@gmail.com>
2022-12-20 16:05:46 +0100

    revert

--- src/Components/NavPanel.jsx
+++ src/Components/NavPanel.jsx
@@ -77,7 +77,7 @@ const useStyles = makeStyles((theme) => ({
     'opacity': .8,
     'justifyContent': 'space-around',
     'alignItems': 'center',
-    'maxHeight': '450px',
+    'maxHeight': '100%',
     '@media (max-width: 900px)': {
       maxHeight: '150px',
       width: '300px',
