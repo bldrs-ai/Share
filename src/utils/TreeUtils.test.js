974a1ac8507482009151373b55fc3a2d3afc3690
Omachonu Ogali <oo@bldrs.ai>
2022-12-22 11:20:36 -0500

    Wrap tests in a describe block to eliminate confusion

--- src/utils/TreeUtils.test.js
+++ src/utils/TreeUtils.test.js
@@ -89,36 +89,38 @@ export function makeTestTree() {
 }
 
 
-test('Test setupLookupAndParentLinks', () => {
-  const tree = makeTestTree()
-  const eltsById = {}
-  setupLookupAndParentLinks(tree, eltsById)
-  const a = tree
-  const b = tree.children[0]
-  const c = tree.children[0].children[0]
-  expect(b).toEqual(c.parent)
-  expect(a).toEqual(b.parent)
-  expect(undefined).toEqual(a.parent)
-  expect(a).toEqual(eltsById[0])
-  expect(b).toEqual(eltsById[1])
-  expect(c).toEqual(eltsById[2])
-})
+describe('TreeUtils', () => {
+  test('Test setupLookupAndParentLinks', () => {
+    const tree = makeTestTree()
+    const eltsById = {}
+    setupLookupAndParentLinks(tree, eltsById)
+    const a = tree
+    const b = tree.children[0]
+    const c = tree.children[0].children[0]
+    expect(b).toEqual(c.parent)
+    expect(a).toEqual(b.parent)
+    expect(undefined).toEqual(a.parent)
+    expect(a).toEqual(eltsById[0])
+    expect(b).toEqual(eltsById[1])
+    expect(c).toEqual(eltsById[2])
+  })
 
 
-test('Test computeElementPathIds', () => {
-  const tree = makeTestTree()
-  const a = tree
-  const b = tree.children[0]
-  const c = tree.children[0].children[0]
-  const getIdCb = (elt) => elt.name
-  expect(computeElementPathIds(a, getIdCb)).toEqual(['a'])
-  expect(computeElementPathIds(b, getIdCb)).toEqual(['b'])
-  expect(computeElementPathIds(c, getIdCb)).toEqual(['c'])
-  const eltsById = {}
-  setupLookupAndParentLinks(tree, eltsById)
-  expect(computeElementPathIds(a, getIdCb)).toEqual(['a'])
-  expect(computeElementPathIds(b, getIdCb)).toEqual(['a', 'b'])
-  expect(computeElementPathIds(c, getIdCb)).toEqual(['a', 'b', 'c'])
+  test('Test computeElementPathIds', () => {
+    const tree = makeTestTree()
+    const a = tree
+    const b = tree.children[0]
+    const c = tree.children[0].children[0]
+    const getIdCb = (elt) => elt.Name
+    expect(computeElementPathIds(a, getIdCb)).toEqual(['aRootElement'])
+    expect(computeElementPathIds(b, getIdCb)).toEqual(['b'])
+    expect(computeElementPathIds(c, getIdCb)).toEqual(['c'])
+    const eltsById = {}
+    setupLookupAndParentLinks(tree, eltsById)
+    expect(computeElementPathIds(a, getIdCb)).toEqual(['aRootElement'])
+    expect(computeElementPathIds(b, getIdCb)).toEqual(['aRootElement', 'b'])
+    expect(computeElementPathIds(c, getIdCb)).toEqual(['aRootElement', 'b', 'c'])
+  })
 })
 
 export const createFakeProject = ({expressId, name, longName}) => {
