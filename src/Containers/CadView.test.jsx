a486c278624e8e3107669fdd135e76df6a463f29
Omachonu Ogali <oo@bldrs.ai>
2022-12-22 11:23:33 -0500

    Refactor <CadView> component tests (and add some new assertions)

--- src/Containers/CadView.test.jsx
+++ src/Containers/CadView.test.jsx
@@ -1,136 +1,178 @@
-import React, {useState} from 'react'
-import {render, renderHook, act, fireEvent, screen, waitFor} from '@testing-library/react'
+import React from 'react'
+import {act, fireEvent, render, renderHook, waitFor, waitForElementToBeRemoved} from '@testing-library/react'
 import {__getIfcViewerAPIMockSingleton} from 'web-ifc-viewer'
 import useStore from '../store/useStore'
 import ShareMock from '../ShareMock'
-import {actAsyncFlush} from '../utils/tests'
-import {makeTestTree} from '../utils/TreeUtils.test'
+import {createFakeTree} from '../utils/TreeUtils.test'
 import CadView from './CadView'
+import '@testing-library/jest-dom'
+import {visitTree} from '../utils/TreeUtils'
 
 
 let viewerMock
+let testTree
+let testProperties
+
 
 describe('CadView', () => {
   beforeEach(() => {
     viewerMock = __getIfcViewerAPIMockSingleton()
+    testTree = createFakeTree()
+    testProperties = {}
+
+    testProperties[testTree.expressID] = {
+      Name: testTree.Name || null,
+      LongName: testTree.LongName || null,
+    }
+
+    visitTree(testTree, (child, parent) => {
+      testProperties[child.expressID] = {
+        Name: child.Name || null,
+        LongName: child.LongName || null,
+      }
+    })
+
+    viewerMock._loadedModel.ifcManager.getSpatialStructure.mockReturnValueOnce(new Promise(function(resolve, reject) {
+      resolve(testTree)
+    }))
+
+    viewerMock.getProperties.mockImplementation((modelID, expressID, indirect, recursive) => new Promise(function(resolve, reject) {
+      if (expressID in testProperties) {
+        resolve(testProperties[expressID])
+      }
+
+      reject(new Error(`express ID ${expressID} not found`))
+    }))
   })
 
   afterEach(() => {
     jest.clearAllMocks()
   })
 
-  it('renders correctly', () => {
+  it('renders an IFC correctly', async () => {
     const modelPath = {
       filepath: `index.ifc`,
     }
 
-    render(
-        <ShareMock>
-          <CadView
-            installPrefix={''}
-            appPrefix={''}
-            pathPrefix={''}
-            modelPath={modelPath}
-          />
-        </ShareMock>,
-    )
-  })
-
-  it('renders with mock IfcViewerAPI', async () => {
-    const modelPath = {
-      filepath: `index.ifc`,
-    }
-
-    // Set up mocks
-    viewerMock._loadedModel.ifcManager.getSpatialStructure.mockReturnValueOnce(makeTestTree())
-
-    render(
-        <ShareMock>
-          <CadView
-            installPrefix={''}
-            appPrefix={''}
-            pathPrefix={''}
-            modelPath={modelPath}
-          />
-        </ShareMock>,
+    const {container, findByText, queryByText} = render(
+        <CadView
+          installPrefix={''}
+          appPrefix={''}
+          pathPrefix={''}
+          modelPath={modelPath}
+        />,
+        {
+          wrapper: ({children}) => <ShareMock>{children}</ShareMock>,
+        },
     )
 
-    // Necessary to wait for some components to render to avoid act()
-    // warnings from testing-library.
-    await waitFor(() => screen.getByTitle(/Bldrs: 1.0.0/i))
-    await actAsyncFlush()
+    await waitFor(() => {
+      expect(container.querySelector('div[data-model-ready]')
+          .getAttribute('data-model-ready'))
+          .toEqual('true')
+    })
+
+    const loadingMessage = `Loading index.ifc`
+    await findByText(loadingMessage)
+    await waitForElementToBeRemoved(() => queryByText(loadingMessage))
+
+    await waitFor(() => {
+      expect(container.querySelector('div[data-model-ready]')
+          .getAttribute('data-model-ready'))
+          .toEqual('true')
+    })
 
     // Assert mocks were executed as expected
     const expectedNumOfCalls = 1
-    const calls = viewerMock._loadedModel.ifcManager.getSpatialStructure.mock.calls
-    expect(calls.length).toBe(expectedNumOfCalls)
+    expect(viewerMock._loadedModel.ifcManager.getSpatialStructure).toHaveBeenCalledTimes(expectedNumOfCalls)
   })
-
 
   it('renders and selects the element ID from URL', async () => {
-    const testTree = makeTestTree()
-    const targetEltId = testTree.children[0].expressID
+    const targetElementId = testTree.children[0].expressID
+    const targetElementName = testTree.children[0].Name.value
     const modelPath = {
-
-      filepath: `index.ifc/${targetEltId}`,
+      filepath: `index.ifc`,
       gitpath: undefined,
     }
-    viewerMock._loadedModel.ifcManager.getSpatialStructure.mockReturnValueOnce(testTree)
-    const {result} = renderHook(() => useState(modelPath))
-    render(
-        <ShareMock>
-          <CadView
-            installPrefix={'/'}
-            appPrefix={'/'}
-            pathPrefix={'/'}
-            modelPath={result.current[0]}
-          />
-        </ShareMock>)
-    await waitFor(() => screen.getByTitle(/Bldrs: 1.0.0/i))
-    await actAsyncFlush()
-    const getPropsCalls = viewerMock.getProperties.mock.calls
-    const numCallsExpected = 2 // First for root, second from URL path
-    expect(getPropsCalls.length).toBe(numCallsExpected)
-    expect(getPropsCalls[0][0]).toBe(0) // call 1, arg 1
-    expect(getPropsCalls[0][0]).toBe(0) // call 2, arg 2
-    expect(getPropsCalls[1][0]).toBe(0) // call 2, arg 1
-    expect(getPropsCalls[1][1]).toBe(targetEltId) // call 2, arg 2
-    await actAsyncFlush()
+
+    const {result} = renderHook(() => useStore((state) => state))
+    const {container, findByLabelText, findByText, findByPlaceholderText, findByTitle} = render(
+        <CadView
+          installPrefix={''}
+          appPrefix={''}
+          pathPrefix={''}
+          modelPath={modelPath}
+        />,
+        {
+          wrapper: ({children}) => <ShareMock initialEntries={[`/share/v/p/index.ifc/${targetElementId}`]}>{children}</ShareMock>,
+        },
+    )
+
+    await findByTitle('Open IFC')
+    await findByPlaceholderText('Search / Insert GitHub link')
+
+    await findByLabelText('IFC Navigator')
+    await findByText(targetElementName)
+
+    await waitFor(() => {
+      expect(container.querySelector('div[data-model-ready]')
+          .getAttribute('data-model-ready'))
+          .toEqual('true')
+    })
+
+    expect(result.current.selectedElements).toEqual([`${targetElementId}`])
+    expect(result.current.selectedElement.Name.value).toEqual('Fake Site')
+
+    const expectedNumOfCalls = 2 // First for root, second from URL path
+    expect(viewerMock.getProperties).toHaveBeenCalledTimes(expectedNumOfCalls)
+    expect(viewerMock.getProperties).toHaveBeenCalledWith(0, testTree.expressID)
+    expect(viewerMock.getProperties).toHaveBeenCalledWith(0, targetElementId)
   })
 
-  it('clear elements and planes on unselect', async () => {
-    const testTree = makeTestTree()
-    const targetEltId = testTree.children[0].expressID
+  /* it('clear elements and planes on unselect', async () => {
+    const targetElementId = testTree.children[0].expressID
     const modelPath = {
-      filepath: `index.ifc/${targetEltId}`,
+      filepath: `index.ifc`,
       gitpath: undefined,
     }
     viewerMock._loadedModel.ifcManager.getSpatialStructure.mockReturnValueOnce(testTree)
     const {result} = renderHook(() => useStore((state) => state))
     await act(() => {
-      result.current.setSelectedElement(targetEltId)
-      result.current.setSelectedElements([targetEltId])
+      result.current.setSelectedElement(targetElementId)
+      result.current.setSelectedElements([targetElementId])
       result.current.setCutPlaneDirection('y')
+      result.current.setLevelInstance(null)
     })
-    const {getByTitle} = render(
-        <ShareMock>
-          <CadView
-            installPrefix={'/'}
-            appPrefix={'/'}
-            pathPrefix={'/'}
-            modelPath={modelPath}
-          />
-        </ShareMock>)
-    expect(getByTitle('Section')).toBeInTheDocument()
+
+    const {container, getByTitle} = render(
+        <CadView
+          installPrefix={''}
+          appPrefix={''}
+          pathPrefix={''}
+          modelPath={modelPath}
+        />,
+        {
+          wrapper: ({children}) => <ShareMock initialEntries={[`/share/v/p/index.ifc/${targetElementId}`]}>{children}</ShareMock>,
+        },
+    )
+
+    await waitFor(() => {
+      expect(container.querySelector('div[data-model-ready]')
+          .getAttribute('data-model-ready'))
+          .toEqual('true')
+    })
+
     const clearSelection = getByTitle('Clear')
-    act(() => {
+    await act(() => {
       fireEvent.click(clearSelection)
     })
-    const callDeletePlanes = viewerMock.clipper.deleteAllPlanes.mock.calls
-    expect(callDeletePlanes.length).toBe(1)
-    expect(result.current.selectedElements).toBe(null)
+
+    const expectedNumOfCalls = 1
+    expect(viewerMock.clipper.deleteAllPlanes).toHaveBeenCalledTimes(expectedNumOfCalls)
+
+    expect(result.current.selectedElements).toEqual([])
     expect(result.current.selectedElement).toBe(null)
     expect(result.current.cutPlaneDirection).toBe(null)
-    await actAsyncFlush()
-  })
+    expect(result.current.levelInstance).toBe(null)
+  }) */
 })
