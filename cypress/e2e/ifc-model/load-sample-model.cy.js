fb2cfcc431e81562a6c3f639cfb7cb6909934248
rondoor124 <rondoor124@gmail.com>
2022-12-21 21:54:40 -0300

    increase timeout

--- cypress/e2e/ifc-model/load-sample-model.cy.js
+++ cypress/e2e/ifc-model/load-sample-model.cy.js
@@ -6,28 +6,28 @@ describe('sample models', () => {
       cy.get('#viewer-container').get('canvas').should('be.visible')
 
       // Wait up to 15 seconds for IFC to finish loading
-      cy.get('[data-model-ready="true"]', {timeout: 20000}).should('exist')
+      cy.get('[data-model-ready="true"]', {timeout: 300000}).should('exist')
     })
 
     it('should display tooltip when hovering', () => {
-      cy.findByRole('button', {name: 'Open IFC', timeout: 20000}).realHover()
-      cy.findByRole('tooltip', {timeout: 20000}).contains('Open IFC')
+      cy.findByRole('button', {name: 'Open IFC', timeout: 300000}).realHover()
+      cy.findByRole('tooltip', {timeout: 300000}).contains('Open IFC')
     })
 
     it('should display the sample models dialog', () => {
-      cy.findByRole('button', {name: 'Open IFC', timeout: 20000}).realClick()
-      cy.findByRole('dialog', {timeout: 20000}).contains('Sample Projects')
+      cy.findByRole('button', {name: 'Open IFC', timeout: 300000}).realClick()
+      cy.findByRole('dialog', {timeout: 300000}).contains('Sample Projects')
     })
 
     it('should load the Momentum model when selected', () => {
-      cy.findByRole('button', {name: 'Open IFC', timeout: 20000}).realClick()
+      cy.findByRole('button', {name: 'Open IFC', timeout: 300000}).realClick()
       cy.findByLabelText('Sample Projects').realClick()
-      cy.findByRole('listbox', {timeout: 20000}).within(() => {
-        cy.findByRole('option', {name: 'Momentum', timeout: 20000}).realClick()
+      cy.findByRole('listbox', {timeout: 300000}).within(() => {
+        cy.findByRole('option', {name: 'Momentum', timeout: 300000}).realClick()
       })
-      cy.findByRole('listbox', {timeout: 20000}).should('not.exist')
-      cy.findByRole('tree', {label: 'IFC Navigator', timeout: 20000})
-      cy.findByText('Momentum / KNIK v3', {timeout: 20000})
+      cy.findByRole('listbox', {timeout: 300000}).should('not.exist')
+      cy.findByRole('tree', {label: 'IFC Navigator', timeout: 300000})
+      cy.findByText('Momentum / KNIK v3', {timeout: 300000})
     })
   })
 })
