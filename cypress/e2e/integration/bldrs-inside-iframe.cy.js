089bbfc7b42388493453cba9cd45364f1e907163
Abdelrahman Zien <a.osamamail@gmail.com>
2023-01-07 18:38:24 +0200

    fix linting issues

--- cypress/e2e/integration/bldrs-inside-iframe.cy.js
+++ cypress/e2e/integration/bldrs-inside-iframe.cy.js
@@ -3,7 +3,7 @@ const path = require('path')
 /**
  * Black-box integration tests for Bldrs running in an iframe.
  * Bldrs emits messages and receives messages via Matrix Widgets API.
- * 
+ *
  * The setup includes a standalone web page with a Bldrs iframe and wired
  * up message handling. This scenario comes as close as possible to a real-
  * world integration scenario. An important difference is that the cypress
@@ -13,7 +13,7 @@ const path = require('path')
 describe('bldrs inside iframe', () => {
   const SYSTEM_UNDER_TEST = '/cypress/static/bldrs-inside-iframe.html'
   const KEYCODE_ESC = 27
-
+  const REQUEST_SUCCESS_CODE = 200
   const REMOTE_IFC_URL = '**/Momentum.ifc'
   const REMOTE_IFC_FIXTURE = 'Momentum.ifc'
 
@@ -24,7 +24,7 @@ describe('bldrs inside iframe', () => {
     const fixtures = ['bldrs-inside-iframe.html', 'bldrs-inside-iframe.js']
     const targetDirectory = 'docs/cypress/static/'
     for (const fixture of fixtures) {
-      cy.fixture(fixture, null).then(content => {
+      cy.fixture(fixture, null).then((content) => {
         const outPath = path.join(targetDirectory, fixture)
         cy.writeFile(outPath, content)
       })
@@ -35,7 +35,7 @@ describe('bldrs inside iframe', () => {
     cy.clearCookies()
     cy.visit(SYSTEM_UNDER_TEST)
     cy.get('iframe').iframe().as('iframe')
-    cy.get('@iframe').trigger('keydown', { keyCode: KEYCODE_ESC })
+    cy.get('@iframe').trigger('keydown', {keyCode: KEYCODE_ESC})
   })
 
   it('should emit ready-messsage when page load completes', () => {
@@ -52,15 +52,15 @@ describe('bldrs inside iframe', () => {
 
     cy.get('#txtSendMessageType').clear().type('ai.bldrs-share.LoadModel')
     const msg = {
-      githubIfcPath: model
+      githubIfcPath: model,
     }
 
-    cy.intercept('GET', REMOTE_IFC_URL, { fixture: REMOTE_IFC_FIXTURE }).as('loadModel')
+    cy.intercept('GET', REMOTE_IFC_URL, {fixture: REMOTE_IFC_FIXTURE}).as('loadModel')
 
     cy.get('#txtSendMessagePayload').clear()
-      .type(JSON.stringify(msg), { parseSpecialCharSequences: false })
+        .type(JSON.stringify(msg), {parseSpecialCharSequences: false})
     cy.get('#btnSendMessage').click()
-    cy.wait('@loadModel').its('response.statusCode').should('eq', 200)
+    cy.wait('@loadModel').its('response.statusCode').should('eq', REQUEST_SUCCESS_CODE)
     // cy.get('@iframe').find('[data-ifc-model="1"]').should('exist')
     cy.get('@iframe').contains('span', modelRootNodeName).should('exist')
     // cy.get('#messagesCount').contains('2') //Second loaded message received
@@ -69,52 +69,49 @@ describe('bldrs inside iframe', () => {
   it('should select element when SelectElements-message emitted', () => {
     cy.get('#lastMessageReceivedAction').contains(/ModelLoaded/i)
     const globalId = '02uD5Qe8H3mek2PYnMWHk1'
-    const expectedExpressId = '621'
     // cy.get('@iframe').find('[data-ifc-model="1"]').should('exist')
     cy.get('#txtSendMessageType').clear().type('ai.bldrs-share.SelectElements')
     const msg = {
       globalIds: [globalId],
     }
-    cy.get('#txtSendMessagePayload').clear().type(JSON.stringify(msg), { parseSpecialCharSequences: false })
+    cy.get('#txtSendMessagePayload').clear().type(JSON.stringify(msg), {parseSpecialCharSequences: false})
     cy.get('#btnSendMessage').click()
-    cy.get('@iframe').findByRole('button', { name: /Properties/ }).click()
+    cy.get('@iframe').findByRole('button', {name: /Properties/}).click()
     cy.get('@iframe').contains('span', /621/).should('exist')
   })
 
   it('should emit SelectionChanged-message when element was selected through the menu and when cleared', () => {
     const targetElementId = '3vMqyUfHj3tgritpIZS4iG'
-    
-    cy.get('#messagesCount').contains(/1/)
     cy.get('#lastMessageReceivedAction').contains(/ModelLoaded/i)
     cy.get('@iframe').findByText(/bldrs/i).click()
     cy.get('@iframe').findByText(/build/i).click()
     cy.get('@iframe').findByText(/every/i).click()
     cy.get('@iframe').findByText(/thing/i).click()
     cy.get('@iframe').findAllByText(/together/i).first().click()
-    
+
     cy.get('#txtLastMsg').should(($txtLastMsg) => {
-      let msg = JSON.parse($txtLastMsg.val())
-      assert.equal(msg.api, "fromWidget")
-      assert.equal(msg.widgetId, "bldrs-share")
+      const msg = JSON.parse($txtLastMsg.val())
+      assert.equal(msg.api, 'fromWidget')
+      assert.equal(msg.widgetId, 'bldrs-share')
       assert.exists(msg.requestId)
       assert.exists(msg.data)
       assert.equal(msg.action, 'ai.bldrs-share.SelectionChanged')
-      assert.equal(msg.data["current"][0],targetElementId)
+      assert.equal(msg.data['current'][0], targetElementId)
     })
 
     cy.get('@iframe').findAllByText(/together/i).last().click()
     cy.get('#lastMessageReceivedAction').contains(/SelectionChanged/i)
 
-    cy.get('@iframe').findByRole('button', { name: /Clear/ }).click()
+    cy.get('@iframe').findByRole('button', {name: /Clear/}).click()
 
     cy.get('#txtLastMsg').should(($txtLastMsg) => {
-      var msg = JSON.parse($txtLastMsg.val())
-      assert.equal(msg.api, "fromWidget")
-      assert.equal(msg.widgetId, "bldrs-share")
+      const msg = JSON.parse($txtLastMsg.val())
+      assert.equal(msg.api, 'fromWidget')
+      assert.equal(msg.widgetId, 'bldrs-share')
       assert.exists(msg.requestId)
       assert.exists(msg.data)
       assert.equal(msg.action, 'ai.bldrs-share.SelectionChanged')
-      assert.equal(msg.data["current"].length, 0)
+      assert.equal(msg.data['current'].length, 0)
     })
   })
 
@@ -128,9 +125,9 @@ describe('bldrs inside iframe', () => {
     cy.get('#btnSendMessage').click()
 
     cy.findByRole('tree', {label: 'IFC Navigator'}).should('not.exist')
-     cy.get('@iframe').findByRole('button', {name: /Notes/}).should('not.exist')
-     cy.get('@iframe').findByRole('button', {name: /Properties/}).should('not.exist')
-     cy.get('@iframe').findByRole('button', {name: /Section/}).should('not.exist')
-     cy.get('@iframe').findByRole('button', {name: /Clear/}).should('not.exist')
+    cy.get('@iframe').findByRole('button', {name: /Notes/}).should('not.exist')
+    cy.get('@iframe').findByRole('button', {name: /Properties/}).should('not.exist')
+    cy.get('@iframe').findByRole('button', {name: /Section/}).should('not.exist')
+    cy.get('@iframe').findByRole('button', {name: /Clear/}).should('not.exist')
   })
 })
