2294f1f9a3b527521966bda91c19a7e2113f7bcf
Ron <rondoor124@gmail.com>
2023-01-13 12:04:04 -0800

    remove unused component in `CadView.test.jsx`

--- src/Containers/CadView.test.jsx
+++ src/Containers/CadView.test.jsx
@@ -1,6 +1,5 @@
 import React, {useState} from 'react'
 import {render, renderHook, act, fireEvent, screen, waitFor} from '@testing-library/react'
-// import user from '@testing-library/user-event'
 import {__getIfcViewerAPIMockSingleton} from 'web-ifc-viewer'
 import useStore from '../store/useStore'
 import ShareMock from '../ShareMock'
