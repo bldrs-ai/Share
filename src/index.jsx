fe1333b7e639c135621adc1580e70ed460b40a5c
Omachonu Ogali <oo@bldrs.ai>
2023-01-18 10:32:55 -0500

    Add Sentry to React Router implementation

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,35 @@
 import React from 'react'
 import {createRoot} from 'react-dom/client'
-import {BrowserRouter} from 'react-router-dom'
+import {
+  BrowserRouter,
+  Routes,
+  useLocation,
+  useNavigationType,
+  createRoutesFromChildren,
+  matchRoutes,
+} from 'react-router-dom'
 import BaseRoutes from './BaseRoutes'
 import {FlagsProvider} from 'react-feature-flags'
 import {flags} from './FeatureFlags'
+import * as Sentry from '@sentry/react'
+import {BrowserTracing} from '@sentry/tracing'
 
+
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  integrations: [
+    new BrowserTracing({
+      routingInstrumentation: Sentry.reactRouterV6Instrumentation(
+          React.useEffect,
+          useLocation,
+          useNavigationType,
+          createRoutesFromChildren,
+          matchRoutes,
+      ),
+    }),
+  ],
+  tracesSampleRate: 1.0,
+})
 
 if (process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test') {
   const {worker} = require('./__mocks__/browser')
