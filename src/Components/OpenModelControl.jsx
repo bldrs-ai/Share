f95e5ba9200aaa1ecb42e2bba3550286689c25de
Omachonu Ogali <oo@bldrs.ai>
2023-02-06 13:14:52 -0500

    Fix whitespace

--- src/Components/OpenModelControl.jsx
+++ src/Components/OpenModelControl.jsx
@@ -39,7 +39,7 @@ export default function OpenModelControl({fileOpen}) {
           icon={<OpenIcon/>}
           placement={'top'}
           selected={isDialogDisplayed}
-	  dataTestId='open-ifc'
+          dataTestId='open-ifc'
         />
       </Paper>
       {isDialogDisplayed &&
