18efd058179c785ddf45ad65cb75be681ac813c8
OlegMoshkovich <oleg.mosh@gmail.com>
2023-02-19 20:58:53 +0100

    position loader

--- src/Components/AuthNav.jsx
+++ src/Components/AuthNav.jsx
@@ -1,17 +1,25 @@
 import React from 'react'
 import {useAuth0} from '@auth0/auth0-react'
-import {CircularProgress} from '@material-ui/core'
 import LoginButton from './LoginButton'
 import UserProfile from './UserProfile'
+import Loader from './Loader'
+import {TooltipIconButton} from './Buttons'
 
 
 const AuthNav = () => {
   const {isLoading, isAuthenticated} = useAuth0()
 
   if (isLoading) {
-    return <CircularProgress/>
+    return (
+      <TooltipIconButton
+        title={'Log in with GitHub'}
+        icon={<Loader/>}
+        onClick={() => {
+          return undefined
+        }}
+      />
+    )
   }
-
   return isAuthenticated ?
     <UserProfile/> :
     <LoginButton/>
