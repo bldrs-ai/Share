286e5a4eebb4a6eb32b213198378487a653fa1cb
Ron <rondoor124@gmail.com>
2023-02-23 23:12:10 -0800

    complete unit tests for sprite svg

--- src/utils/svg.test.js
+++ src/utils/svg.test.js
@@ -1,6 +1,6 @@
 import {deletePropertyRecursive, deleteStringValueMatchRecursive} from './objects'
 import {UUID_REGEX} from './strings'
-import {getSvgElFromStr, getSvgGroupFromObj, getSvgSpriteFromEl} from './svg'
+import {getSvgGroupFromObj, getSpriteFromSvgCanvas} from './svg'
 
 
 const {SVGLoader} = jest.requireActual('three/examples/jsm/loaders/SVGLoader')
@@ -18,11 +18,9 @@ describe('svg', () => {
     expect(svgGroup).toMatchSnapshot()
   })
 
-  it('getSvgSpriteFromEl snapshot of PlaceMarkSvg matches', () => {
-    const fs = require('fs')
-    const svgStr = fs.readFileSync('src/assets/icons/PlaceMark.svg', 'utf8')
-    const svgEl = getSvgElFromStr(svgStr)
-    const svgSprite = getSvgSpriteFromEl({svgEl})
+  it('getSpriteFromSvgCanvas snapshot of PlaceMarkSvg matches', () => {
+    const svgCanvas = document.createElement('canvas')
+    const svgSprite = getSpriteFromSvgCanvas({svgCanvas})
     deletePropertyRecursive(svgSprite, 'uuid')
     deleteStringValueMatchRecursive(svgSprite, UUID_REGEX)
     expect(svgSprite).toMatchSnapshot()
