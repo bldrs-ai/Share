c3dbb3fafb89236e94e0eff5435c202a9755e253
Ron <rondoor124@gmail.com>
2023-02-23 20:51:36 -0800

    update unit tests

--- src/utils/svg.test.js
+++ src/utils/svg.test.js
@@ -1,6 +1,6 @@
 import {deletePropertyRecursive, deleteStringValueMatchRecursive} from './objects'
 import {UUID_REGEX} from './strings'
-import {getSvgGroupFromObj, getSvgSpriteFromEl} from './svg'
+import {getSvgElFromStr, getSvgGroupFromObj, getSvgSpriteFromEl} from './svg'
 
 
 const {SVGLoader} = jest.requireActual('three/examples/jsm/loaders/SVGLoader')
@@ -11,8 +11,8 @@ describe('svg', () => {
     const fs = require('fs')
     const svgBuf = fs.readFileSync('src/assets/icons/PlaceMark.svg', 'utf8')
     const svgLoader = new SVGLoader()
-    const svgData = svgLoader.parse(svgBuf)
-    const svgGroup = getSvgGroupFromObj({svgData})
+    const svgObj = svgLoader.parse(svgBuf)
+    const svgGroup = getSvgGroupFromObj({svgObj})
     deletePropertyRecursive(svgGroup, 'uuid')
     deleteStringValueMatchRecursive(svgGroup, UUID_REGEX)
     expect(svgGroup).toMatchSnapshot()
@@ -21,7 +21,8 @@ describe('svg', () => {
   it('getSvgSpriteFromEl snapshot of PlaceMarkSvg matches', () => {
     const fs = require('fs')
     const svgStr = fs.readFileSync('src/assets/icons/PlaceMark.svg', 'utf8')
-    const svgSprite = getSvgSpriteFromEl({svgStr})
+    const svgEl = getSvgElFromStr(svgStr)
+    const svgSprite = getSvgSpriteFromEl({svgEl})
     deletePropertyRecursive(svgSprite, 'uuid')
     deleteStringValueMatchRecursive(svgSprite, UUID_REGEX)
     expect(svgSprite).toMatchSnapshot()
