977228dfd948435d88bcde9794431803b3365146
OlegMoshkovich <oleg.mosh@gmail.com>
2023-03-01 15:48:37 +0100

    change logo angle

--- src/Components/OpenModelControl.jsx
+++ src/Components/OpenModelControl.jsx
@@ -118,7 +118,7 @@ function ModelFileSelector({setIsDialogDisplayed}) {
       4: '/share/v/gh/Swiss-Property-AG/Seestrasse-Public/main/SEESTRASSE.ifc#c:119.61,50.37,73.68,16.18,11.25,5.74',
       // eslint-disable-next-line max-len
       5: '/share/v/gh/sujal23ks/BCF/main/packages/fileimport-service/ifc/ifcs/171210AISC_Sculpture_brep.ifc/120010/120020/120023/4998/2867#c:-163.46,16.12,223.99,12.03,-28.04,-15.28',
-      6: '/share/v/gh/OlegMoshkovich/Logo/main/IFC_STUDY.ifc',
+      6: '/share/v/gh/OlegMoshkovich/Logo/main/IFC_STUDY.ifc#c:71.223,-22.129,-55.609,7.344,-10.301,0.934',
     }
     window.removeEventListener('beforeunload', handleBeforeUnload)
     navigate({pathname: modelPath[e.target.value]})
