920ebeadbbd0847dd2d04ad03ebb3b65ea842d57
Omachonu Ogali <oo@bldrs.ai>
2023-03-07 11:23:07 -0500

    Refactor unit test to search by title rather than test ID

--- src/Components/Buttons.test.jsx
+++ src/Components/Buttons.test.jsx
@@ -9,17 +9,15 @@ describe('<TooltipIconButton />', () => {
   test('should render successfully', async () => {
     const testId = 'test-button'
     const rendered = render(
-        <MockComponent>
-          <TooltipIconButton
-            dataTestId={testId}
-            title={'Hello. Is it me you\'re looking for?'}
-            // eslint-disable-next-line no-empty-function
-            onClick={() => {}}
-            icon={<QuestionIcon/>}
-          />
-        </MockComponent>)
+        <TooltipIconButton
+          dataTestId={testId}
+          title={'Hello Button'}
+          // eslint-disable-next-line no-empty-function
+          onClick={() => {}}
+          icon={<QuestionIcon/>}
+        />, {wrapper: MockComponent})
 
-    const button = rendered.getByTestId(testId)
+    const button = rendered.getByTitle('Hello Button')
     fireEvent.mouseOver(button)
 
     const tooltip = await rendered.findByRole('tooltip')
