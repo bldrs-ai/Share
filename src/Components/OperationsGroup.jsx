ba2cf524ce454159f67e6fcef9d80b5f11d2c4b8
OlegMoshkovich <oleg.mosh@gmail.com>
2023-03-22 14:29:39 +0100

    test

--- src/Components/OperationsGroup.jsx
+++ src/Components/OperationsGroup.jsx
@@ -74,6 +74,7 @@ export default function OperationsGroup({deselectItems}) {
   // disappear. This keeps them visible.
   const bgOpacity = 0.2
   const bgColor = hexToRgba(assertDefined(theme.palette.scene.background), bgOpacity)
+  console.log('here')
   return (
     <Box
       sx={{
