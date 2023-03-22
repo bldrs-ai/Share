96b072476fb6b170236510ce2c17b0f383e6d96d
Ron <rondoor124@gmail.com>
2023-03-22 15:29:57 -0700

    simplify dev mode

--- src/utils/common.js
+++ src/utils/common.js
@@ -1,10 +1,6 @@
-import debug from './debug'
-
-
 /**
  * @return {boolean}
  */
 export function isDevMode() {
-  debug().log('common#isDevMode: process.env.NODE_ENV: ', process.env.NODE_ENV)
-  return process.env.NODE_ENV === 'development'
+  return false
 }
