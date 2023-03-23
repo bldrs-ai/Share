722122816e4b8d454ee073f2b88defc6122061a5
OlegMoshkovich <oleg.mosh@gmail.com>
2023-03-23 11:20:38 +0100

    create breadcrumnbs component

---
+++ src/Components/FileBreadcrumbs.jsx
@@ -0,0 +1,92 @@
+import React from 'react'
+import Box from '@mui/material/Box'
+import Tooltip from '@mui/material/Tooltip'
+import useTheme from '@mui/styles/useTheme'
+
+
+/**
+ * @param {object} modelPath
+ * @param {number} searchAndNavWidthPx
+ * @param {number} searchAndNavMaxWidthPx
+ * @return {object} React component
+ */
+export default function FileBreadCrumbs({modelPath, searchAndNavWidthPx, searchAndNavMaxWidthPx}) {
+  const theme = useTheme()
+  return (
+    <Box sx={{
+      'display': 'flex',
+      'flexDirection': 'row',
+      'borderRadius': '5px',
+      'width': '275px',
+      'marginTop': '14px',
+      'padding': '6px 0px 6px 14px',
+      'background': theme.palette.primary.background,
+      'color': theme.palette.primary.contrastText,
+      'textOverflow': 'ellipsis',
+      'overflow': 'hidden',
+      'whiteSpace': 'nowrap',
+      'opacity': .8,
+      '& a': {
+        ...theme.typography.tree,
+        color: theme.palette.primary.contrastText,
+        opacity: .4,
+      },
+      '@media (max-width: 900px)': {
+        width: `${searchAndNavWidthPx}px`,
+        maxWidth: `${searchAndNavMaxWidthPx}px`,
+      },
+    }}
+    >
+      <Tooltip title={modelPath.org} placement={'bottom'}>
+        <Box
+          sx={{
+            textOverflow: 'ellipsis',
+            overflow: 'hidden',
+            whiteSpace: 'nowrap',
+          }}
+        >
+          <a
+            href={`https://github.com/${modelPath.org}`}
+            target='_new'
+          >
+            {modelPath.org}
+          </a>
+        </Box>
+      </Tooltip>
+      <Tooltip title={modelPath.repo} placement={'bottom'}>
+        <Box
+          sx={{
+            maxWidth: '60px',
+            textOverflow: 'ellipsis',
+            overflow: 'hidden',
+            whiteSpace: 'nowrap',
+          }}
+        >
+          <a
+            href={`https://github.com/${modelPath.org}/${modelPath.repo}`}
+            target='_new'
+          >
+            &gt; {modelPath.repo}
+          </a>
+        </Box>
+      </Tooltip>
+      <Tooltip title={modelPath.filepath} placement={'bottom'}>
+        <Box
+          sx={{
+            maxWidth: '60px',
+            textOverflow: 'ellipsis',
+            overflow: 'hidden',
+            whiteSpace: 'nowrap',
+          }}
+        >
+          <a
+            href={`https://github.com/${modelPath.org}/${modelPath.repo}/blob/${modelPath.branch}${modelPath.filepath}`}
+            target='_new'
+          >
+            {modelPath.filepath}
+          </a>
+        </Box>
+      </Tooltip>
+    </Box>
+  )
+}
--- src/Containers/CadView.jsx
+++ src/Containers/CadView.jsx
@@ -2,11 +2,11 @@ import React, {useEffect, useState} from 'react'
 import {Color, MeshLambertMaterial} from 'three'
 import {useNavigate, useSearchParams, useLocation} from 'react-router-dom'
 import Box from '@mui/material/Box'
-import Tooltip from '@mui/material/Tooltip'
 import useTheme from '@mui/styles/useTheme'
 import {navToDefault} from '../Share'
 import Alert from '../Components/Alert'
 import BranchesControl from '../Components/BranchesControl'
+import FileBreadCrumbs from '../Components/FileBreadCrumbs'
 import {useWindowDimensions} from '../Components/Hooks'
 import Logo from '../Components/Logo'
 import NavPanel from '../Components/NavPanel'
@@ -610,81 +610,11 @@ export default function CadView({
           }
           {
             modelPath.repo !== undefined &&
-            <Box sx={{
-              'display': 'flex',
-              'flexDirection': 'row',
-              'borderRadius': '5px',
-              'width': '275px',
-              'marginTop': '14px',
-              'padding': '6px 0px 6px 14px',
-              'background': theme.palette.primary.background,
-              'color': theme.palette.primary.contrastText,
-              'textOverflow': 'ellipsis',
-              'overflow': 'hidden',
-              'whiteSpace': 'nowrap',
-              'opacity': .8,
-              '& a': {
-                ...theme.typography.tree,
-                color: theme.palette.primary.contrastText,
-                opacity: .4,
-              },
-              '@media (max-width: 900px)': {
-                width: `${searchAndNavWidthPx}px`,
-                maxWidth: `${searchAndNavMaxWidthPx}px`,
-              },
-            }}
-            >
-              <Tooltip title={modelPath.org} placement={'bottom'}>
-                <Box
-                  sx={{
-                    textOverflow: 'ellipsis',
-                    overflow: 'hidden',
-                    whiteSpace: 'nowrap',
-                  }}
-                >
-                  <a
-                    href={`https://github.com/${modelPath.org}`}
-                    target='_new'
-                  >
-                    {modelPath.org}
-                  </a>
-                </Box>
-              </Tooltip>
-              <Tooltip title={modelPath.repo} placement={'bottom'}>
-                <Box
-                  sx={{
-                    maxWidth: '60px',
-                    textOverflow: 'ellipsis',
-                    overflow: 'hidden',
-                    whiteSpace: 'nowrap',
-                  }}
-                >
-                  <a
-                    href={`https://github.com/${modelPath.org}/${modelPath.repo}`}
-                    target='_new'
-                  >
-                    /{modelPath.repo}
-                  </a>
-                </Box>
-              </Tooltip>
-              <Tooltip title={modelPath.filepath} placement={'bottom'}>
-                <Box
-                  sx={{
-                    maxWidth: '60px',
-                    textOverflow: 'ellipsis',
-                    overflow: 'hidden',
-                    whiteSpace: 'nowrap',
-                  }}
-                >
-                  <a
-                    href={`https://github.com/${modelPath.org}/${modelPath.repo}/blob/${modelPath.branch}${modelPath.filepath}`}
-                    target='_new'
-                  >
-                    {modelPath.filepath}
-                  </a>
-                </Box>
-              </Tooltip>
-            </Box>
+            <FileBreadCrumbs
+              modelPath={modelPath}
+              searchAndNavWidthPx={searchAndNavWidthPx}
+              searchAndNavMaxWidthPx={searchAndNavMaxWidthPx}
+            />
           }
 
           {isNavPanelOpen &&
