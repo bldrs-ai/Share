0d632a1506983a957497c5bef8f3a4028cef59f4
Ron <rondoor124@gmail.com>
2023-03-27 15:56:51 -0500

    update unit tests

--- src/utils/objects.test.js
+++ src/utils/objects.test.js
@@ -67,39 +67,14 @@ describe('objects util', () => {
     })
   })
 
-  it('filterObject', () => {
-    const testObj = deepCloneObject(templateObj)
+  it('filterObject removes a single name/value pair', () => {
+    const testObj = {a: 1, b: 2, c: 3}
     const filteredObject = filterObject(testObj, (val, key) => {
-      // eslint-disable-next-line no-magic-numbers
-      if (val === 3 && key === 'uuid') {
+      if (val === 1 && key === 'a') {
         return false
       }
       return true
     })
-    expect(filteredObject).toStrictEqual({
-      a: 1,
-      b: 2,
-      arr: [
-        {
-          foo: 'a',
-          bar: 'b',
-          uuid: 'c',
-        },
-      ],
-      foo: {
-        geometries: [
-          {
-            uuid: 'ADD77535-D1B6-49A9-915B-41343B08BF83',
-            type: 'ShapeGeometry',
-            shapes: [],
-            curveSegments: 12,
-          },
-        ],
-        uuids: [
-          'ADD77535-D1B6-49A9-915B-41343B08BF83',
-          'FDD77535-D1B6-49A9-915B-41343B08BF83',
-        ],
-      },
-    })
+    expect(filteredObject).toStrictEqual({b: 2, c: 3})
   })
 })
