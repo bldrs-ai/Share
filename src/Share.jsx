8f2b908e5e1e0554f0afc680ac1e46a4eb78d5b5
Omachonu Ogali <oo@bldrs.ai>
2023-03-27 12:14:09 -0400

    Update severity levels for particular debug calls

--- src/Share.jsx
+++ src/Share.jsx
@@ -68,7 +68,7 @@ export default function Share({installPrefix, appPrefix, pathPrefix}) {
       debug().log('Setting default repo pablo-mayrgundter/Share')
       setRepository('pablo-mayrgundter', 'Share')
     } else {
-      debug().log('No repository set for project!, ', pathPrefix)
+      debug().warn('No repository set for project!, ', pathPrefix)
     }
   }, [appPrefix, installPrefix, modelPath, pathPrefix, setRepository, urlParams, setModelPath])
 
