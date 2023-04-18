2e90301fd53796d85ef920e0bce5a3dfdbe9023c
Ron <rondoor124@gmail.com>
2023-04-18 08:04:16 -0400

    Use feature flag in placemark (#709)
    * remove debug
    
    * remove the hash logic used to enable place mark
    
    * disable dev mode
    
    * check placemark feature continuously
    
    * add debug
    
    * update feature flag checking logic
    
    * add unit test

--- src/utils/common.js
+++ src/utils/common.js
@@ -1,6 +1,36 @@
+import {assertDefined} from './assert'
+import debug from './debug'
+
+
 /**
  * @return {boolean}
  */
 export function isDevMode() {
+  return false
+}
+
+
+/**
+ * @param {string} name Feature name
+ * @return {boolean}
+ */
+export function existInFeature(name) {
+  assertDefined(name)
+  name = name.toLocaleLowerCase()
+  const initialParameters = new URLSearchParams(window.location.search)
+  const enabledFeatures = initialParameters.get('feature')
+  if (!enabledFeatures) {
+    return false
+  }
+  const enabledFeatureArr = enabledFeatures.split(',')
+  debug().log('common#existInFeature: enabledFeatureArr: ', enabledFeatureArr)
+
+  for (let i = 0; i < enabledFeatureArr.length; i++) {
+    if (enabledFeatureArr[i].toLocaleLowerCase() === name) {
+      debug().log('common#existInFeature: ', true, name)
+      return true
+    }
+  }
+
   return false
 }
