b65c5d71346f5e90c936334bc4362a90ad72c8ce
Omachonu Ogali <113154043+oo-bldrs@users.noreply.github.com>
2023-05-04 12:57:41 -0400

    Route unauthenicated download requests through Git Redirector (#717)

--- src/Containers/CadView.jsx
+++ src/Containers/CadView.jsx
@@ -764,7 +764,23 @@ export const getFinalURL = async (url, accessToken) => {
   switch (u.host.toLowerCase()) {
     case 'github.com':
       if (!accessToken) {
-        u.host = process.env.RAW_GIT_PROXY_URL || 'raw.githubusercontent.com'
+        const proxyURL = new URL(process.env.RAW_GIT_PROXY_URL || 'https://raw.githubusercontent.com')
+
+        // Replace the protocol, host, and hostname in the target
+        u.protocol = proxyURL.protocol
+        u.host = proxyURL.host
+        u.hostname = proxyURL.hostname
+
+        // If the port is specified, replace it in the target URL
+        if (proxyURL.port) {
+          u.port = proxyURL.port
+        }
+
+        // If there's a path, *and* it's not just the root, then prepend it to the target URL
+        if (proxyURL.pathname && proxyURL.pathname !== '/') {
+          u.pathname = proxyURL.pathname + u.pathname
+        }
+
         return u.toString()
       }
 
