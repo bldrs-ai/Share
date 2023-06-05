79285db78d44f44faadef6294b6973acab4f281b
OlegMoshkovich <oleg.mosh@gmail.com>
2023-06-05 11:48:49 +0200

    test open icon

--- src/Components/OpenModelControl.jsx
+++ src/Components/OpenModelControl.jsx
@@ -14,6 +14,7 @@ import {handleBeforeUnload} from '../utils/event'
 import {getOrganizations, getRepositories, getFiles, getUserRepositories} from '../utils/GitHub'
 import {RectangularButton} from '../Components/Buttons'
 import OpenIcon from '../assets/icons/Open.svg'
+import OpenNewIcon from '../assets/icons/OpenNew.svg'
 import UploadIcon from '../assets/icons/Upload.svg'
 
 
@@ -52,7 +53,7 @@ export default function OpenModelControl({fileOpen, modelPath, isLocalModel}) {
       <TooltipIconButton
         title={'Projects'}
         onClick={() => setIsDialogDisplayed(true)}
-        icon={<OpenIcon/>}
+        icon={<OpenNewIcon/>}
         placement={'bottom'}
         selected={isDialogDisplayed}
         dataTestId='open-ifc'
