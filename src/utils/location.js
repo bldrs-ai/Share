2a8e27564cd1efaa645c62e0fadb865c451368db
Rafael Toledano Illán <r.toledano.illan@vyzn.tech>
2023-07-18 12:36:38 +0200

    VYZN-942: Replaces pushState for replaceState

--- src/utils/location.js
+++ src/utils/location.js
@@ -474,7 +474,7 @@ export function removeHashParams(location, name, paramKeys = []) {
 
   location.hash = newParamsEncoded
   if (location.hash === '') {
-    history.pushState(
+    history.replaceState(
         '', document.title, window.location.pathname + window.location.search)
   }
 }
