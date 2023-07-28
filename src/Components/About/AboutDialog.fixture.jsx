fa710a7dda424c24d604f49ac040030ad0670a99
Pablo Mayrgundter <pablo.mayrgundter@gmail.com>
2023-07-28 17:07:26 +0200

    Fix style setup in AboutDialog.fixture

--- src/Components/About/AboutDialog.fixture.jsx
+++ src/Components/About/AboutDialog.fixture.jsx
@@ -1,21 +1,27 @@
 import React from 'react'
 import {HelmetProvider} from 'react-helmet-async'
+import CssBaseline from '@mui/material/CssBaseline'
 import {ThemeProvider} from '@mui/material/styles'
 import useShareTheme from '../../theme/Theme'
+import Styles from '../../Styles'
 import {AboutDialog} from './AboutControl'
 
 
 /** @return {React.Component} */
 export default function Example() {
+  const theme = useShareTheme()
   return (
     <HelmetProvider>
-      <ThemeProvider theme={useShareTheme()}>
-        <AboutDialog
-          isDialogDisplayed={true}
-          // eslint-disable-next-line no-empty-function
-          setIsDialogDisplayed={() => {}}
-        />
-      </ThemeProvider>
+      <CssBaseline enableColorScheme>
+        <ThemeProvider theme={theme}>
+          <Styles theme={theme}/>
+          <AboutDialog
+            isDialogDisplayed={true}
+            // eslint-disable-next-line no-empty-function
+            setIsDialogDisplayed={() => {}}
+          />
+        </ThemeProvider>
+      </CssBaseline>
     </HelmetProvider>
   )
 }
