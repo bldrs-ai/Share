3ed06383ec2b34cf514087ee4eaf871d192776bc
OlegMoshkovich <oleg.mosh@gmail.com>
2023-09-13 14:50:24 +0200

    add auto complete

---
+++ src/Components/InputAutocomplete.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import Autocomplete from '@mui/material/Autocomplete'
+import TextField from '@mui/material/TextField'
+import Stack from '@mui/material/Stack'
+import {assertDefined} from '../utils/assert'
+
+/**
+ * Input with autocomplete feature.
+ *
+ * @property {Array<object>} elements suggested elements used to autocomple input,the object is in a shape of {title:'suggestion'}
+ * @property {string} placeholder Input placeholder
+ * @property {string} size MUI size of the input component
+ * @return {React.Component}
+ */
+export default function InputAutocomplete({elements, placeholder, size = 'small'}) {
+  assertDefined(elements, placeholder)
+  return (
+    <Stack spacing={3} sx={{minWidth: '280px'}}>
+      <Autocomplete
+        multiple
+        options={elements}
+        getOptionLabel={(option) => option.title}
+        filterSelectedOptions
+        size={size}
+        renderInput={(params) => {
+          return (
+            <TextField
+              {...params}
+              placeholder={placeholder}
+              size={size}
+            />
+          )
+        }
+        }
+      />
+    </Stack>
+  )
+}
