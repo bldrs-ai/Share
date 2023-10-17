727735079400f4c247d5bb4e116adb902b3fb611
OlegMoshkovich <oleg.mosh@gmail.com>
2023-10-18 00:16:03 +0200

    move the misc group up

--- src/Containers/CadView.jsx
+++ src/Containers/CadView.jsx
@@ -655,7 +655,7 @@ export default function CadView({
             'bottom': '1em',
             'width': '100%',
             '@media (max-width: 900px)': {
-              bottom: '6em',
+              bottom: '8em',
             },
           }}
         >
