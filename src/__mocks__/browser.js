5e99e7b7084a3dc20e08cdd5d07b3a23ab5cfdd0
Pablo Mayrgundter <pablo.mayrgundter@gmail.com>
2023-10-25 12:11:26 -0500

    Remove global Google Analytics and mock in dev mode.

--- src/__mocks__/browser.js
+++ src/__mocks__/browser.js
@@ -1,6 +1,6 @@
 import {setupWorker} from 'msw'
-import {handlers} from './api-handlers'
+import {initHandlers} from './api-handlers'
 
 
 // This configures a Service Worker with the given request handlers.
-export const worker = setupWorker(...handlers)
+export const worker = setupWorker(...initHandlers())
