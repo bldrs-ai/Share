408912624a6dcb38a65790db9d3b3ec084492d36
OlegMoshkovich <oleg.mosh@gmail.com>
2023-11-07 17:16:47 +0100

    add the test

---
+++ src/Components/HelpControl.test.jsx
@@ -0,0 +1,28 @@
+
+import React from 'react'
+import {render, fireEvent} from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+import HelpControl from './HelpControl'
+import ShareMock from '../ShareMock'
+
+
+describe('<HelpDialog />', () => {
+  it('renders the first page of the HelpDialog', () => {
+    const {getByTitle, getByText} = render(<ShareMock><HelpControl/></ShareMock>)
+    const button = getByTitle('Help')
+    fireEvent.click(button)
+    const text = getByText('Study the project using standard sections')
+    expect(text).toBeInTheDocument()
+  })
+
+  it('navigates to the next page when the next button is clicked', () => {
+    const {getByTitle, getByText, getByTestId, debug} = render(<ShareMock><HelpControl/></ShareMock>)
+    const button = getByTitle('Help')
+    fireEvent.click(button)
+    const nextPageButton = getByTestId('Next')
+    fireEvent.click(nextPageButton)
+    debug()
+    const text = getByText('Isolate selected element')
+    expect(text).toBeInTheDocument()
+  })
+})
