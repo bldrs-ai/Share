6b777a43178f3d8253e5a013eb0908807dae703c
OlegMoshkovich <oleg.mosh@gmail.com>
2023-11-16 19:02:10 +0100

    adjust the version history panel tests

--- src/Components/VersionHistoryPanel.test.jsx
+++ src/Components/VersionHistoryPanel.test.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ShareMock from '../ShareMock'
 import {render} from '@testing-library/react'
+// import {getCommitsForBranch} from '../utils/GitHub'
 // import VersionHistoryPanel from './VersionHistoryPanel'
 
 
@@ -14,9 +15,32 @@ jest.mock('../utils/GitHub', () => ({
 }))
 
 describe('VersionsHistoryPanel', () => {
-  it('Version history is rendered', () => {
+  it('fetches commits on mount', () => {
     render(
         <ShareMock/>,
+        // <ShareMock>
+        //   <VersionHistoryPanel branch="main"/>
+        // </ShareMock>,
     )
+
+    // await waitFor(() => {
+    //   // expect(getCommitsForBranch).toHaveBeenCalled()
+    // })
   })
+
+  // it('navigates to main on button click', () => {
+  //   const navigate = useNavigate()
+
+  //   const {getByLabelText} = render(
+  //       <ShareMock>
+  //         <VersionHistoryPanel branch="main"/>
+  //       </ShareMock>,
+  //   )
+
+  //   fireEvent.click(getByLabelText('navigate_to_tip'))
+
+  //   expect(navigate).toHaveBeenCalledWith({
+  //     pathname: expect.any(String), // Replace with expected pathname
+  //   })
+  // })
 })
