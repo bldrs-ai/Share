dbc70c8da4bd511750d8363b09f536e79ffe3ffe
OlegMoshkovich <oleg.mosh@gmail.com>
2023-11-20 16:58:01 +0100

    add imagine dialog to operations group

--- src/Components/ImagineControl.jsx
+++ src/Components/ImagineControl.jsx
@@ -1,17 +1,17 @@
 import React, {createRef, useEffect, useState} from 'react'
-import Link from '@mui/material/Link'
+import TextField from '@mui/material/TextField'
 import Typography from '@mui/material/Typography'
+import Link from '@mui/material/Link'
 import Stack from '@mui/material/Stack'
 import useStore from '../store/useStore'
 import Dialog from './Dialog'
 import {
   addCameraUrlParams,
-  removeCameraUrlParams,
 } from './CameraControl'
 import {ControlButton} from './Buttons'
 import CopyIcon from '../assets/icons/Copy.svg'
 import {Helmet} from 'react-helmet-async'
-import AiIcon from '../assets/icons/AI.svg'
+import AutoFixHighIcon from '@mui/icons-material/AutoFixHigh'
 
 
 /**
@@ -28,8 +28,8 @@ export default function ShareControl() {
 
   return (
     <ControlButton
-      title='Ai imagine rendering'
-      icon={<AiIcon className='icon-share'/>}
+      title='Imagine AI rendering'
+      icon={<AutoFixHighIcon className='icon-share'/>}
       isDialogDisplayed={openedDialog}
       setIsDialogDisplayed={setIsDialogDisplayed}
       dialog={
@@ -53,7 +53,6 @@ export default function ShareControl() {
  * @return {React.Component} The react component
  */
 function ShareDialog({isDialogDisplayed, setIsDialogDisplayed}) {
-  const [isCameraInUrl] = useState(true)
   const cameraControls = useStore((state) => state.cameraControls)
   const viewer = useStore((state) => state.viewer)
   const model = useStore((state) => state.model)
@@ -62,11 +61,7 @@ function ShareDialog({isDialogDisplayed, setIsDialogDisplayed}) {
 
   useEffect(() => {
     if (viewer) {
-      if (isCameraInUrl) {
-        addCameraUrlParams(cameraControls)
-      } else {
-        removeCameraUrlParams()
-      }
+      addCameraUrlParams(cameraControls)
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [viewer, model])
@@ -76,40 +71,39 @@ function ShareDialog({isDialogDisplayed, setIsDialogDisplayed}) {
     setIsDialogDisplayed(false)
   }
 
-
-  const onCopy = (event) => {
-    navigator.clipboard.writeText(location)
+  const onCopy = async (event) => {
+    await navigator.clipboard.writeText(window.location.href)
+    window.open('https://discord.com/channels/853953158560743424/1126526910495740005', '_blank')
     urlTextFieldRef.current.select()
     closeDialog()
-    window.open('https://discord.com/channels/853953158560743424/1126526910495740005', '_blank').focus()
   }
+
 
   return (
     <Dialog
-      icon={<AiIcon className='icon-share'/>}
-      headerText='A.I. agent'
+      icon={<AutoFixHighIcon className='icon-share'/>}
+      headerText='AI rendering'
       isDialogDisplayed={isDialogDisplayed}
       setIsDialogDisplayed={closeDialog}
-      actionTitle='Copy link and go discord'
+      actionTitle='Copy Link and go to Discord'
       actionIcon={<CopyIcon className='icon-share'/>}
       actionCb={onCopy}
       content={
-        <Stack spacing={1} sx={{width: '230px', height: '120px'}}>
+        <Stack
+          alignItems='center'
+          justifyContent='flex-start'
+          spacing={1}
+          sx={{
+            width: '300px',
+          }}
+        >
           <Helmet>
-            <title>Imagine AI rendering</title>
+            <title>AI rendering</title>
           </Helmet>
           <Stack
             spacing={1}
-            sx={{
-              lineHeight: '1.4em',
-              textAlign: 'left',
-            }}
           >
-            <Typography variant={'body1'}
-              sx={{
-                lineHeight: '1.4em',
-              }}
-            >
+            <Typography variant={'body1'}>
             At the moment our AI agent lives on  {' '}
               <Link
                 underline="always"
@@ -123,14 +117,23 @@ function ShareDialog({isDialogDisplayed, setIsDialogDisplayed}) {
             discrord.
               </Link>
             </Typography>
-            <Typography variant={'body1'}
-              sx={{
-                lineHeight: '1.4em',
-              }}
-            >
-              To use it is please go to discord channel type /imagine and follow the intstructions.
+            <Typography variant={'body1'}>
+              To use it is please go to discord channel, type /imagine and follow the intstructions.
             </Typography>
           </Stack>
+          <TextField
+            sx={{
+              width: '266px',
+            }}
+            value={String(window.location)}
+            inputRef={urlTextFieldRef}
+            variant='outlined'
+            size='small'
+            rows={1}
+            InputProps={{
+              readOnly: true,
+            }}
+          />
         </Stack>
       }
     />)
