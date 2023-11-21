d9e34ee0350483e5098371c9941e9d0adcfb147e
OlegMoshkovich <oleg.mosh@gmail.com>
2023-11-21 16:09:44 +0100

    add the version history panel test

--- src/Components/VersionHistoryPanel.test.jsx
+++ src/Components/VersionHistoryPanel.test.jsx
@@ -1,27 +1,50 @@
 import React from 'react'
 import ShareMock from '../ShareMock'
-import {render, renderHook, act} from '@testing-library/react'
+import {render, renderHook, act, waitFor} from '@testing-library/react'
 import VersionHistoryPanel from './VersionHistoryPanel'
+import {getCommitsForBranch} from '../utils/GitHub'
 import {
   MOCK_MODEL_PATH_GIT,
   MOCK_REPOSITORY,
+  MOCK_COMMITS,
 } from '../utils/GitHub'
 import useStore from '../store/useStore'
 
 
+jest.mock('../utils/GitHub', () => ({
+  getCommitsForBranch: jest.fn(() => Promise.resolve([
+    MOCK_COMMITS,
+  ])),
+}))
+
 describe('VersionsHistoryPanel', () => {
+  it('fetches commits on mount', async () => {
+    const {result} = renderHook(() => useStore((state) => state))
+    await act(() => {
+      result.current.setModelPath(MOCK_MODEL_PATH_GIT)
+      result.current.setRepository(MOCK_REPOSITORY)
+    })
+    getCommitsForBranch.mockResolvedValueOnce(MOCK_COMMITS)
+    render(
+        <ShareMock>
+          <VersionHistoryPanel branch="main"/>
+        </ShareMock>,
+    )
+    await waitFor(() => {
+      expect(getCommitsForBranch).toHaveBeenCalled()
+    })
+  })
   it('renders the panel', async () => {
     const {result} = renderHook(() => useStore((state) => state))
     await act(() => {
       result.current.setModelPath(MOCK_MODEL_PATH_GIT)
       result.current.setRepository(MOCK_REPOSITORY)
     })
-    const {debug, getByText} = render(
+    const {getByText} = render(
         <ShareMock>
           <VersionHistoryPanel branch="main"/>
         </ShareMock>,
     )
-    debug()
     const dialogTitle = getByText('Version History')
     expect(dialogTitle).toBeInTheDocument()
   })
