5de984c604d57937c1a3d81b6d3946a0812c1b73
OlegMoshkovich <oleg.mosh@gmail.com>
2023-11-27 15:59:00 +0100

    fix one of the timeline tests

--- src/Components/Versions/VersionsTimeline.test.jsx
+++ src/Components/Versions/VersionsTimeline.test.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {render, screen, fireEvent} from '@testing-library/react'
+import {render, fireEvent} from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 import VersionsTimeline from './VersionsTimeline'
 
@@ -7,33 +7,30 @@ import VersionsTimeline from './VersionsTimeline'
 describe('CustomTimeline', () => {
   const mockCommitData = [
     {
-      authorName: 'John Doe',
+      authorName: 'User1',
       commitDate: '2023-10-13',
       commitMessage: 'Create initial structure',
     },
     {
-      authorName: 'Jane Smith',
+      authorName: 'User2',
       commitDate: '2023-10-14',
       commitMessage: 'Add new feature',
     },
   ]
 
-  test('renders correctly', () => {
-    const items = screen.getAllByText(/Create initial structure|Add new feature/)
-    render(<VersionsTimeline commitData={mockCommitData}/>)
-    // eslint-disable-next-line no-magic-numbers
-    expect(items).toHaveLength(2)
-  })
-
-  it('displays the correct number of timeline items', () => {
-    const vt = <VersionsTimeline commitData={mockCommitData}/>
-    render(vt)
-  })
-
-  it.only('updates the active timeline item on click', () => {
+  it.only('displays the correct number of timeline items', () => {
     const commitNavigateCb = jest.fn()
     const {getByText} = render(<VersionsTimeline commitData={mockCommitData} commitNavigateCb={commitNavigateCb}/>)
-    const firstItem = getByText('John Doe')
+    const firstItem = getByText('User1')
+    const secondItem = getByText('User2')
+    expect(firstItem).toBeInTheDocument()
+    expect(secondItem).toBeInTheDocument()
+  })
+
+  it('updates the active timeline item on click', () => {
+    const commitNavigateCb = jest.fn()
+    const {getByText} = render(<VersionsTimeline commitData={mockCommitData} commitNavigateCb={commitNavigateCb}/>)
+    const firstItem = getByText('User1')
     fireEvent.click(firstItem)
     expect(commitNavigateCb).toHaveBeenCalledTimes(1)
   })
