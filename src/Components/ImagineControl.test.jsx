6a1c5d47cff8d4e65db2f6f168b2c8ffe1baa7cc
Pablo Mayrgundter <pablo.mayrgundter@gmail.com>
2023-11-30 20:45:18 +0100

    Rework text for Bldr Bot. Remove debug from unit tests.

--- src/Components/ImagineControl.test.jsx
+++ src/Components/ImagineControl.test.jsx
@@ -6,22 +6,22 @@ import {MockComponent} from '../__mocks__/MockComponent'
 
 describe('ImagineControl', () => {
   it('renders the dialog in the document', () => {
-    const {getByTitle, debug} = render(<ImagineControl/>, {
+    const {getByTitle} = render(<ImagineControl/>, {
       wrapper: MockComponent,
     })
-    debug()
+
     const component = getByTitle('Bldr Bot Rendering')
     expect(component).toBeInTheDocument()
   })
 
   it('updates the title when the dialog is open', async () => {
-    const {getByTitle, debug} = render(<ImagineControl/>, {
+    const {getByTitle} = render(<ImagineControl/>, {
       wrapper: MockComponent,
     })
 
     const button = getByTitle('Bldr Bot Rendering')
     fireEvent.click(button)
-    debug()
+
     await(waitFor(() => expect(document.title).toBe('BLDR Bot')))
   })
 })
