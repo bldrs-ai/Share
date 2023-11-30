1a5995be50535b7b7e6816ef0d6716b68c661225
Pablo Mayrgundter <pablo.mayrgundter@gmail.com>
2023-11-30 16:16:55 -0600

    Rework Bot guide text (#876)
    * Rework text for Bldr Bot
    
    * Rework text for Bldr Bot. Remove debug from unit tests.
    
    * Remove more debug from unit tests.

--- src/Components/LoginMenu.test.jsx
+++ src/Components/LoginMenu.test.jsx
@@ -8,50 +8,50 @@ import ShareMock from '../ShareMock'
 describe('LoginMenu', () => {
   it('renders the login button when not logged in', async () => {
     mockedUseAuth0.mockReturnValue(mockedUserLoggedOut)
-    const {debug, findByTitle, findByText} = render(<ShareMock><LoginMenu/></ShareMock>)
+    const {findByTitle, findByText} = render(<ShareMock><LoginMenu/></ShareMock>)
     const usersMenu = await findByTitle('Users menu')
     fireEvent.click(usersMenu)
-    debug()
+
     const LoginWithGithub = await findByText('Log in with Github')
     expect(LoginWithGithub).toBeInTheDocument()
   })
 
   it('renders the user avatar when logged in', async () => {
     mockedUseAuth0.mockReturnValue(mockedUserLoggedIn)
-    const {debug, findByTitle, findByText} = render(<ShareMock><LoginMenu/></ShareMock>)
+    const {findByTitle, findByText} = render(<ShareMock><LoginMenu/></ShareMock>)
     const usersMenu = await findByTitle('Users menu')
     fireEvent.click(usersMenu)
-    debug()
+
     const LoginWithGithub = await findByText('Log out')
     expect(LoginWithGithub).toBeInTheDocument()
   })
 
   it('renders the theme selection', async () => {
     mockedUseAuth0.mockReturnValue(mockedUserLoggedIn)
-    const {debug, findByTitle, findByText} = render(<ShareMock><LoginMenu/></ShareMock>)
+    const {findByTitle, findByText} = render(<ShareMock><LoginMenu/></ShareMock>)
     const usersMenu = await findByTitle('Users menu')
     fireEvent.click(usersMenu)
-    debug()
+
     const dayThemeButton = await findByText('Day theme')
     expect(dayThemeButton).toBeInTheDocument()
   })
 
   it('renders the night theme when selected', async () => {
     mockedUseAuth0.mockReturnValue(mockedUserLoggedIn)
-    const {debug, findByTitle, findByText} = render(<ShareMock><LoginMenu/></ShareMock>)
+    const {findByTitle, findByText} = render(<ShareMock><LoginMenu/></ShareMock>)
     const usersMenu = await findByTitle('Users menu')
     fireEvent.click(usersMenu)
     const dayThemeButton = await findByText('Day theme')
     fireEvent.click(dayThemeButton)
-    debug()
+
     const nighThemeButton = await findByText('Night theme')
     expect(nighThemeButton).toBeInTheDocument()
   })
 
   it('renders users avatar when logged in', async () => {
     mockedUseAuth0.mockReturnValue(mockedUserLoggedIn)
-    const {debug, findByAltText} = render(<ShareMock><LoginMenu/></ShareMock>)
-    debug()
+    const {findByAltText} = render(<ShareMock><LoginMenu/></ShareMock>)
+
     const avatarImage = await findByAltText('Unit Testing')
     expect(avatarImage).toBeInTheDocument()
   })
