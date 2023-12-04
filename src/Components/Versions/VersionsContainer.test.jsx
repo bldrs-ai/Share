d0565d95836ba5e6b4c3064000402c0e65caefb2
OlegMoshkovich <oleg.mosh@gmail.com>
2023-12-04 11:50:07 +0100

    adjust the version container test and subrtract branched control from the cadview

--- src/Components/Versions/VersionsContainer.test.jsx
+++ src/Components/Versions/VersionsContainer.test.jsx
@@ -1,8 +1,7 @@
 import React from 'react'
 import ShareMock from '../../ShareMock'
-import {render, renderHook, act, waitFor} from '@testing-library/react'
+import {render, renderHook, act} from '@testing-library/react'
 import VersionsContainer from './VersionsContainer'
-import {getCommitsForBranch} from '../../utils/GitHub'
 import {
   MOCK_MODEL_PATH_GIT,
   MOCK_REPOSITORY,
@@ -18,22 +17,6 @@ jest.mock('../../utils/GitHub', () => ({
 }))
 
 describe('VersionsContainer', () => {
-  it('fetches commits on mount', async () => {
-    const {result} = renderHook(() => useStore((state) => state))
-    await act(() => {
-      result.current.setModelPath(MOCK_MODEL_PATH_GIT)
-      result.current.setRepository(MOCK_REPOSITORY)
-    })
-    getCommitsForBranch.mockResolvedValueOnce(MOCK_COMMITS)
-    render(
-        <ShareMock>
-          <VersionsContainer branch="main"/>
-        </ShareMock>,
-    )
-    await waitFor(() => {
-      expect(getCommitsForBranch).toHaveBeenCalled()
-    })
-  })
   it('renders the panel', async () => {
     const {result} = renderHook(() => useStore((state) => state))
     await act(() => {
