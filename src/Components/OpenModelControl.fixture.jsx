7af5e96192c51b3fd4f872a01badcae3fd4ffe79
Pablo Mayrgundter <pablo.mayrgundter@gmail.com>
2024-01-30 21:11:52 +0100

    Save Dialog.jsx

--- src/Components/OpenModelControl.fixture.jsx
+++ src/Components/OpenModelControl.fixture.jsx
@@ -3,9 +3,11 @@ import {ThemeCtx} from '../theme/Theme.fixture'
 import OpenModelControl from './OpenModelControl'
 
 
+// For unit test
 export const OpenModelControlFixture = () => <ThemeCtx><OpenModelControl/></ThemeCtx>
 
 
+// For cosmos
 export default {
   OpenModelControl: <OpenModelControlFixture/>,
 }
