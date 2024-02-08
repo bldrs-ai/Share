dce0b9f876f736361fcab8d9a4506e21c05cbce3
Pablo Mayrgundter <pablo.mayrgundter@gmail.com>
2024-02-08 20:03:56 +0100

    In build.js, use esbuild defines for MSW disable detection, to support simpler NODE_ENV=production setting.

--- tools/esbuild/build.js
+++ tools/esbuild/build.js
@@ -8,7 +8,9 @@ esbuild
     .build(config)
     .then((result) => {
       // Remove development resources from non-development builds
-      if (process.env.DISABLE_MOCK_SERVICE_WORKER === 'true') {
+      if (config.define['process.env.DISABLE_MOCK_SERVICE_WORKER'] === 'true') {
+        // eslint-disable-next-line no-console
+        console.log('Removing MSW from build')
         fs.unlink(join(config.outdir, 'mockServiceWorker.js'), (err) => {
           // eslint-disable-next-line no-console
           console.log(err)
