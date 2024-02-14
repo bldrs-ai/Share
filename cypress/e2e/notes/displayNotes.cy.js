de04a9212caf51aec3ba38c208e0a1f87b96571e
OlegMoshkovich <oleg.mosh@gmail.com>
2024-02-14 13:02:07 +0100

    add notes tests

---
+++ cypress/e2e/notes/displayNotes.cy.js
@@ -0,0 +1,72 @@
+describe('notes', () => {
+  context('notes interactions in the side drawer', () => {
+    beforeEach(() => {
+      cy.clearCookies()
+      cy.visit('/')
+      cy.get('.MuiDialogActions-root > .MuiButtonBase-root').click()
+      cy.get('[data-testid="Notes"]').click()
+    })
+    it('should display Notes navbar title', () => {
+      cy.get('.css-13e0tv1 > .css-hb3iqx > .css-95g4uk > .MuiTypography-root').contains('NOTES')
+    })
+    it('should navigate to create notes when add a note button is clicked', () => {
+      cy.get('[data-testid="Add a note"]').click()
+      cy.get('.css-13e0tv1 > .css-hb3iqx > .css-95g4uk > .MuiTypography-root').contains('ADD A NOTE')
+    })
+    it('should navigate back to the notes list', () => {
+      cy.get('[data-testid="Add a note"]').click()
+      cy.get('.css-13e0tv1 > .css-hb3iqx > .css-95g4uk > .MuiTypography-root').contains('ADD A NOTE')
+      cy.get('[data-testid="Back to the list"]').click()
+      cy.get('.MuiList-root')
+    })
+  })
+  context('note card elements', () => {
+    beforeEach(() => {
+      cy.clearCookies()
+      cy.visit('/')
+      cy.get('.MuiDialogActions-root > .MuiButtonBase-root').click()
+      cy.get('[data-testid="Notes"]').click()
+    })
+    it('should display notes list', () => {
+      cy.get('.MuiList-root')
+    })
+    it('should display note body', () => {
+      cy.get('.MuiList-root')
+      cy.get(':nth-child(1) > [data-testid="selectionContainer"] > .MuiCardContent-root > p').contains('Test Issue body')
+    })
+    it('a note should contain github link button', () => {
+      cy.get('.MuiList-root')
+      cy.get(':nth-child(1) > .css-24km69 > .css-1yae3jf > [data-testid="Open in Github"]')
+    })
+    it('a note should contain discussion button ', () => {
+      cy.get('.MuiList-root')
+      cy.get('[data-testid="Discussion"]')
+    })
+  })
+  context('note card interactions', () => {
+    beforeEach(() => {
+      cy.clearCookies()
+      cy.visit('/')
+      cy.get('.MuiDialogActions-root > .MuiButtonBase-root').click()
+      cy.get('[data-testid="Notes"]').click()
+      cy.get(':nth-child(2) > [data-testid="selectionContainer"] > .MuiCardContent-root > p').click()
+    })
+    it('navbar should change to a note view when a note is selected', () => {
+      cy.get('.css-13e0tv1 > .css-hb3iqx > .css-95g4uk > .MuiTypography-root').contains('NOTE')
+    })
+    it('url should be copied to a clipboard when the note is shared', () => {
+      cy.get('.css-1yae3jf > [data-testid="Share"]').click()
+      cy.get('.MuiSnackbarContent-message > div')
+    })
+    it('comment should be displayed when a note is selected', () => {
+      cy.get(':nth-child(2) > .MuiCardContent-root > p').contains('Test Comment 1')
+    })
+    it('notes should change when previous and next nav buttons are clicked', () => {
+      cy.get('[data-testid="Previous Note"]').click()
+      cy.get('.MuiList-root > :nth-child(1) > .MuiCardContent-root > p').contains('Test Issue body')
+      cy.get('[data-testid="Next Note"]').click()
+      cy.get('.MuiCardHeader-title').contains('Local issue - some text is here to test')
+    })
+  })
+})
+
