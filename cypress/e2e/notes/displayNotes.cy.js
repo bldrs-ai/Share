66d14951dbdbb50dbd23364108afd658894518eb
OlegMoshkovich <oleg.mosh@gmail.com>
2024-02-15 19:34:24 +0100

    add display notes test

--- cypress/e2e/notes/displayNotes.cy.js
+++ cypress/e2e/notes/displayNotes.cy.js
@@ -4,6 +4,7 @@ describe('notes', () => {
       cy.clearCookies()
       cy.visit('/')
       cy.get('.MuiIconButton-root').click()
+      cy.get('.MuiSnackbar-root > .MuiPaper-root').should('not.exist')
       cy.get('[data-testid="Notes"]').click()
     })
     it('should display Notes navbar title', () => {
@@ -20,41 +21,40 @@ describe('notes', () => {
       cy.get('.MuiList-root')
     })
   })
-  // context('note card elements', () => {
-  //   beforeEach(() => {
-  //     cy.clearCookies()
-  //     cy.visit('/')
-  //     cy.get('.MuiIconButton-root').click()
-  //     cy.get('[data-testid="Notes"]', {timeout: 10000}).should('be.visible')
-  //     cy.get('[data-testid="Notes"]').click({force: true})
-  //   })
-  //   it('should display notes list', () => {
-  //     cy.get('.MuiList-root')
-  //   })
-  //   it('should display note body', () => {
-  //     cy.get('.MuiList-root')
-  //     cy.get(':nth-child(1) > [data-testid="selectionContainer"] > .MuiCardContent-root > p').contains('Test Issue body')
-  //   })
-  //   it('a note should contain github link button', () => {
-  //     cy.get('.MuiList-root')
-  //     cy.get(':nth-child(1) > .css-24km69 > .css-1yae3jf > [data-testid="Open in Github"]')
-  //   })
-  //   it('a note should contain discussion button ', () => {
-  //     cy.get('.MuiList-root')
-  //     cy.get('[data-testid="Discussion"]')
-  //   })
-  // })
+  context('note card elements', () => {
+    beforeEach(() => {
+      cy.clearCookies()
+      cy.visit('/')
+      cy.get('.MuiIconButton-root').click()
+      cy.get('.MuiSnackbar-root > .MuiPaper-root').should('not.exist')
+      cy.get('[data-testid="Notes"]').click()
+    })
+    it('should display notes list', () => {
+      cy.get('.MuiList-root')
+    })
+    it('should display note body', () => {
+      cy.get('.MuiList-root')
+      cy.get(':nth-child(1) > [data-testid="selectionContainer"] > .MuiCardContent-root > p').contains('Test Issue body')
+    })
+    it('a note should contain github link button', () => {
+      cy.get('.MuiList-root')
+      cy.get(':nth-child(1) > .css-24km69 > .css-1yae3jf > [data-testid="Open in Github"]')
+    })
+    it('a note should contain discussion button ', () => {
+      cy.get('.MuiList-root')
+      cy.get('[data-testid="Discussion"]')
+    })
+  })
   // context('note card interactions', () => {
   //   beforeEach(() => {
   //     cy.clearCookies()
   //     cy.visit('/')
   //     cy.get('.MuiIconButton-root').click()
-  //     cy.get('[data-testid="Notes"]', {timeout: 10000}).should('be.visible')
-  //     cy.get('[data-testid="Notes"]').click({force: true})
+  //     cy.get('[data-testid="Notes"]').click()
   //     cy.get(':nth-child(2) > [data-testid="selectionContainer"] > .MuiCardContent-root > p').click()
   //   })
   //   it('navbar should change to a note view when a note is selected', () => {
-  //     cy.get('.css-13e0tv1 > .css-hb3iqx > .css-95g4uk > .MuiTypography-root').contains('NOTE')
+  //     cy.get('[data-testid="panelTitle"]').contains('NOTE')
   //   })
   //   it('url should be copied to a clipboard when the note is shared', () => {
   //     cy.get('.css-1yae3jf > [data-testid="Share"]').click()
