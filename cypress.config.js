0ce2984da3d00f0da3e4b2dbda3b3c1f0cd2aaa1
nickcastel50 <nickcastel50@gmail.com>
2024-02-23 18:26:02 -0500

    Remove env from cypress config

--- cypress.config.js
+++ cypress.config.js
@@ -10,12 +10,6 @@ module.exports = defineConfig({
     },
     screenshotOnRunFailure: false,
     video: false,
-    env: {
-      OAUTH2_CLIENT_ID: 'cypresstestaudience',
-      AUTH0_DOMAIN: null,
-      RAW_GIT_PROXY_URL: 'https://rawgit.bldrs.dev/r',
-      NODE_ENV: 'development'
-    },
     defaultCommandTimeout: 20000, // Set default command timeout to 20 seconds
   },
 })
