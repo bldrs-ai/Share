653bbdc74f62d4be61673ae9df4426803cff3a06
nickcastel50 <nickcastel50@gmail.com>
2024-03-18 23:44:39 -0400

    * refactored cypress tests, modularized auth0 + other utility functions for reuse

--- cypress/e2e/profile-100/login.cy.js
+++ cypress/e2e/profile-100/login.cy.js
@@ -2,7 +2,7 @@
 import {auth0Login, setPort, setupAuthenticationIntercepts, waitForModel} from '../../support/utils'
 
 
-describe('save model', () => {
+describe('Profile 100: Login', () => {
   context('when no model is loaded', () => {
     beforeEach(() => {
       cy.clearLocalStorage()
@@ -13,24 +13,15 @@ describe('save model', () => {
       setupAuthenticationIntercepts()
     })
 
-    it('should not find Save IFC button before login', () => {
-      cy.visit('/')
-      waitForModel()
-      cy.findByTestId('Save IFC', {timeout: 10000}).should('not.exist')
-      cy.screenshot()
-    })
-
-    it('should only find Save IFC button after login', () => {
+    it('Should Login', () => {
       cy.visit('/')
       // Now trigger the login process, which will use the mocked loginWithPopup
       cy.url().then((currentUrl) => {
         const url = new URL(currentUrl)
         setPort(url.port)
         waitForModel()
-        cy.findByTitle('Save IFC', {timeout: 5000}).should('not.exist')
         auth0Login()
-        cy.findByTitle('Save IFC', {timeout: 5000}).should('exist')
-
+        // take screenshot
         cy.screenshot()
       })
     })
