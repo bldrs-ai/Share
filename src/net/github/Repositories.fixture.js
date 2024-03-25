673c9d57e7fbb05a30e693b0fd3106e87a59733e
nickcastel50 <nickcastel50@gmail.com>
2024-03-24 23:51:33 -0400

    * add mock_user_repositories to fixture

--- src/net/github/Repositories.fixture.js
+++ src/net/github/Repositories.fixture.js
@@ -5,3 +5,32 @@ export const MOCK_REPOSITORY = {
   full_name: 'bldrs-ai/Share',
   private: true,
 }
+
+export const MOCK_USER_REPOSITORIES = {
+  data: [
+    {
+      id: 452316989,
+      node_id: 'R_kgDOGvXPPQ',
+      name: 'bizdev',
+      full_name: 'bldrs-ai/bizdev',
+      private: true,
+      owner: {
+        login: 'cypresstester',
+        id: 78882658,
+      },
+      fork: false,
+    },
+    {
+      id: 337912599,
+      node_id: 'MDEwOlJlcG9zaXRvcnkzMzc5MTI1OTk=',
+      name: 'buildrs.github.io',
+      full_name: 'bldrs-ai/buildrs.github.io',
+      private: true,
+      owner: {
+        login: 'bldrs-ai',
+        id: 78882658,
+      },
+      fork: false,
+    },
+  ],
+}
