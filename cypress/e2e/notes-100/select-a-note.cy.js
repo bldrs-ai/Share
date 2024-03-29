6b76d933009ee5a66990eec8edcbc3a0e01e70f3
OlegMoshkovich <oleg.mosh@gmail.com>
2024-03-29 11:38:07 +0100

    fix the error

--- cypress/e2e/notes-100/select-a-note.cy.js
+++ cypress/e2e/notes-100/select-a-note.cy.js
@@ -28,16 +28,17 @@ describe('access-notes-list', () => {
       // Close About
       cy.get('button[aria-label="action-button"]')
           .click()
+      // Click on Notes button
       cy.get('[data-testid="Notes"]')
           .click()
       cy.get('.MuiList-root')
     })
-    it('The title on the navbar changes to NOTE)', () => {
-      cy.get('[data-testid="panelTitle"]').contains('NOTE')
-    })
     it('The list of notes is updated to display only the selected note)', () => {
       cy.get(':nth-child(1) > .MuiPaper-root > [data-testid="card-body"] > .MuiCardContent-root').contains('Test Issue body').click()
       cy.get('.MuiCardHeader-title').contains('Local issue 2')
+    })
+    it('The title on the navbar changes to NOTE)', () => {
+      cy.get('[data-testid="panelTitle"]').contains('NOTE')
     })
     it('A list of comments attached to the note to be visible.)', () => {
       cy.get(':nth-child(1) > .MuiPaper-root > [data-testid="card-body"] > .MuiCardContent-root').contains('Test Issue body').click()
