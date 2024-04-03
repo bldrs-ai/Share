ede613621a0f09d46d7c4ac175035ac29f5b2650
OlegMoshkovich <oleg.mosh@gmail.com>
2024-04-03 12:33:39 +0200

    add select a note test

---
+++ cypress/e2e/notes-100/select-a-note.cy.js
@@ -0,0 +1,34 @@
+import {waitForModel, homepageSetup} from '../../support/utils'
+
+
+describe('select-a-note', () => {
+  context('Open index.ifc and notes', () => {
+    beforeEach(() => {
+      homepageSetup()
+    })
+    it('The list of notes is updated to display only the selected note', () => {
+      cy.setCookie('isFirstTime', '1')
+      cy.visit('/')
+      waitForModel()
+      cy.get('[data-testid="Notes"]').click()
+      cy.get(':nth-child(1) > .MuiPaper-root > [data-testid="card-body"] > .MuiCardContent-root').contains('Test Issue body').click()
+      cy.get('.MuiCardHeader-title').contains('Local issue 2')
+    })
+    it('A list of comments attached to the note to be visible', () => {
+      cy.setCookie('isFirstTime', '1')
+      cy.visit('/')
+      waitForModel()
+      cy.get('[data-testid="Notes"]').click()
+      cy.get(':nth-child(1) > .MuiPaper-root > [data-testid="card-body"] > .MuiCardContent-root').contains('Test Issue body').click()
+      cy.get(':nth-child(2) > .MuiPaper-root > .MuiCardContent-root > p').contains('Test Comment 1')
+    })
+    it('The title on the navbar changes to NOTE', () => {
+      cy.setCookie('isFirstTime', '1')
+      cy.visit('/')
+      waitForModel()
+      cy.get('[data-testid="Notes"]').click()
+      cy.get(':nth-child(1) > .MuiPaper-root > [data-testid="card-body"] > .MuiCardContent-root').contains('Test Issue body').click()
+      cy.get('.css-1oum0wi > .css-8lgfcg > :nth-child(1) > .css-95g4uk > [data-testid="panelTitle"]').should('have.text', 'NOTE')
+    })
+  })
+})
