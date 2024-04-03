8ef204e21c1b88486f37580031663575a7cbcf87
OlegMoshkovich <oleg.mosh@gmail.com>
2024-04-03 11:55:08 +0200

    Notes 100 - Access Notes list #1054 (#1086)
    * add access notes list
    
    * add panel title
    
    * remove the comment

---
+++ cypress/e2e/notes-100/access-notes-list.cy.js
@@ -0,0 +1,24 @@
+import {waitForModel, homepageSetup} from '../../support/utils'
+
+
+describe('access-notes-list', () => {
+  context('Open model by following a link to a project on Share (e.g. our index.ifc)', () => {
+    beforeEach(() => {
+      homepageSetup()
+    })
+    it('A list of notes to be visible)', () => {
+      cy.setCookie('isFirstTime', '1')
+      cy.visit('/')
+      waitForModel()
+      cy.get('[data-testid="Notes"]').click()
+      cy.get('.MuiList-root')
+    })
+    it('should display Notes navbar title', () => {
+      cy.setCookie('isFirstTime', '1')
+      cy.visit('/')
+      waitForModel()
+      cy.get('[data-testid="Notes"]').click()
+      cy.get('[data-testid="panelTitle"]').contains('NOTES')
+    })
+  })
+})
