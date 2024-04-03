04e5341e12df8f617f1175bde5c94569b0bc8329
OlegMoshkovich <oleg.mosh@gmail.com>
2024-04-03 13:27:11 +0200

    add test for the github link

---
+++ cypress/e2e/notes-100/note-contains-link-to-github-issue.cy.js
@@ -0,0 +1,26 @@
+import {waitForModel, homepageSetup} from '../../support/utils'
+
+
+describe('select-a-note', () => {
+  context('Open index.ifc and notes', () => {
+    beforeEach(() => {
+      homepageSetup()
+    })
+    it('Github link is visible on a note card in the list', () => {
+      cy.setCookie('isFirstTime', '1')
+      cy.visit('/')
+      waitForModel()
+      cy.get('[data-testid="Notes"]').click()
+      cy.get('[data-testid="Open in Github"]')
+    })
+    it('Github link is visible on a note card when the note selected', () => {
+      cy.setCookie('isFirstTime', '1')
+      cy.visit('/')
+      waitForModel()
+      cy.get('[data-testid="Notes"]').click()
+      cy.get(':nth-child(1) > .MuiPaper-root > [data-testid="card-body"] > .MuiCardContent-root').contains('Test Issue body').click()
+      cy.get('.MuiCardHeader-title').contains('Local issue 2')
+      cy.get('[data-testid="Open in Github"]')
+    })
+  })
+})
