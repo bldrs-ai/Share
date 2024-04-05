db5365e3317fde220b10a17072953214b25e32f7
Pablo Mayrgundter <pablo.mayrgundter@gmail.com>
2024-04-05 17:06:41 -0400

    percy: convert a view-100 test case to percy screenshot (#1065)
    * percy: convert a view-100 test case to percy screenshot, with preserveDrawingBuffer enabled to fix screen capture
    
    * percy: introduce THREE_PDB_IS_ENABLED build flag to enable WebGLRenderer.preserveDrawingBuffer for cypress
    
    * gha: percy: try running `yarn cy` inside percy
    
    * gha: percy: try running `yarn cy` inside percy 2
    
    * gha: percy: try running `yarn cy` inside percy 3
    
    * gha: percy: try running `yarn cy` inside percy 4
    
    * gha: percy: try running `yarn cy` inside percy 5, wire percy token
    
    * gha: percy: try running `yarn cy` inside percy 6, dbl check percy token
    
    * gha: percy: try running `yarn cy` inside percy 7, set environment name in yaml
    
    * view-100: remove saved screens, use percy instead
    
    ---------
    
    Signed-off-by: Pablo Mayrgundter <pablo.mayrgundter@gmail.com>

--- tools/esbuild/vars.prod.js
+++ tools/esbuild/vars.prod.js
@@ -2,18 +2,28 @@ export default {
   // Auth
   AUTH0_DOMAIN: 'bldrs.us.auth0.com',
   OAUTH2_CLIENT_ID: null,
-  OAUTH2_REDIRECT_URI: null, // TODO(pablo): using null in prod to return to window.location.origin
+  // TODO(pablo): using null in prod to return to window.location.origin
+  OAUTH2_REDIRECT_URI: null,
+
   // GitHub
-  GITHUB_API_TOKEN: null, // TODO(pablo): maybe remove? not using anymore
+  // TODO(pablo): maybe remove? not using anymore
+  GITHUB_API_TOKEN: null,
   GITHUB_BASE_URL: 'https://git.bldrs.dev/p/gh',
   RAW_GIT_PROXY_URL: 'https://rawgit.bldrs.dev/r',
+
   // Monitoring
   SENTRY_DSN: null,
   SENTRY_ENVIRONMENT: null,
+
   // Share
   OPFS_IS_ENABLED: true,
   THEME_IS_ENABLED: true,
   USE_WEBIFC_SHIM: true,
+
   // Testing
   MSW_IS_ENABLED: false,
+  // Off for performance in prod
+  // https://threejs.org/docs/#api/en/renderers/WebGLRenderer.preserveDrawingBuffer
+  // https://stackoverflow.com/a/40098594
+  THREE_PDB_IS_ENABLED: false,
 }
