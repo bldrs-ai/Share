123ffd2195c7ed44758cf257545d5d04c088b156
OlegMoshkovich <oleg.mosh@gmail.com>
2024-04-05 15:24:05 +0200

    refactor tests

--- cypress/e2e/notes-100/select-a-note.cy.js
+++ cypress/e2e/notes-100/select-a-note.cy.js
@@ -1,4 +1,4 @@
-import {waitForModel, homepageSetup} from '../../support/utils'
+import {waitForModel, homepageSetup, setCookingAndVisitHome} from '../../support/utils'
 
 
 describe('select-a-note', () => {
@@ -7,32 +7,28 @@ describe('select-a-note', () => {
       homepageSetup()
     })
     it('The list of notes is updated to display only the selected note', () => {
-      cy.setCookie('isFirstTime', '1')
-      cy.visit('/')
+      setCookingAndVisitHome()
       waitForModel()
       cy.get('[data-testid="Notes"]').click()
       cy.get(':nth-child(1) > .MuiPaper-root > [data-testid="card-body"] > .MuiCardContent-root').contains('Test Issue body').click()
       cy.get('.MuiCardHeader-title').contains('Local issue 2')
     })
     it('A list of comments attached to the note to be visible', () => {
-      cy.setCookie('isFirstTime', '1')
-      cy.visit('/')
+      setCookingAndVisitHome()
       waitForModel()
       cy.get('[data-testid="Notes"]').click()
       cy.get(':nth-child(1) > .MuiPaper-root > [data-testid="card-body"] > .MuiCardContent-root').contains('Test Issue body').click()
       cy.get(':nth-child(2) > .MuiPaper-root > .MuiCardContent-root > p').contains('Test Comment 1')
     })
     it('The title on the navbar changes to NOTE', () => {
-      cy.setCookie('isFirstTime', '1')
-      cy.visit('/')
+      setCookingAndVisitHome()
       waitForModel()
       cy.get('[data-testid="Notes"]').click()
       cy.get(':nth-child(1) > .MuiPaper-root > [data-testid="card-body"] > .MuiCardContent-root').contains('Test Issue body').click()
       cy.get('.css-1oum0wi > .css-8lgfcg > :nth-child(1) > .css-95g4uk > [data-testid="panelTitle"]').should('have.text', 'NOTE')
     })
     it('Back to the list button to be visible on the navbar', () => {
-      cy.setCookie('isFirstTime', '1')
-      cy.visit('/')
+      setCookingAndVisitHome()
       waitForModel()
       cy.get('[data-testid="Notes"]').click()
       cy.get(':nth-child(1) > .MuiPaper-root > [data-testid="card-body"] > .MuiCardContent-root').contains('Test Issue body').click()
