e165ae8ff9d4df7314e4522632415f31cf517984
OlegMoshkovich <oleg.mosh@gmail.com>
2024-04-15 14:26:30 +0200

    add the rest of the notes 100

--- cypress/e2e/notes-100/access-shared-note.cy.js
+++ cypress/e2e/notes-100/access-shared-note.cy.js
@@ -1,16 +1,45 @@
 import {waitForModel, homepageSetup} from '../../support/utils'
 
-
-describe('select-a-note', () => {
-  context('Open the link with a shared note', () => {
+// From https://github.com/bldrs-ai/Share/issues/1072
+describe('Notes 100 - Access shared note', () => {
+  context('Access shared link containing the path to the general notes', () => {
     beforeEach(() => {
       homepageSetup()
-    })
-    it('Visit notes permalink, side drawer shall be opened', () => {
       cy.setCookie('isFirstTime', '1')
       cy.visit('/share/v/p/index.ifc#c:-133.022,131.828,161.85,-38.078,22.64,-2.314;i:')
       waitForModel()
+    })
+    it('Correct project to be loaded into the viewport and side drawer to be open', () => {
+      cy.get('.css-1q8dpte').should('exist')
+    })
+    it('Panel title to contain NOTES string', () => {
+      cy.get('[data-testid="panelTitle"]').contains('NOTE')
+    })
+    it('Notes list to be visible', () => {
       cy.get('[data-testid="panelTitle"]').contains('NOTES')
     })
+  })
+  context('Access the shared link containing the path to a note index', () => {
+    beforeEach(() => {
+      homepageSetup()
+      cy.setCookie('isFirstTime', '1')
+      cy.visit('http://localhost:8080/share/v/p/index.ifc#c:-26.91,28.84,112.47,-22,16.21,-3.48;i:2')
+      waitForModel()
+    })
+      it('Correct project to be loaded into the viewport and side drawer to be open', () => {
+        cy.get('.css-1q8dpte').should('exist')
+      })
+      it('Panel title to contain NOTE string', () => {
+        cy.get('[data-testid="panelTitle"]').contains('NOTE')
+      })
+      it('Panel nav to contain back button', () => {
+        cy.get('[data-testid="Back to the list"]')
+      })
+      it('Shared note to be visible', () => {
+        cy.get('.MuiList-root > :nth-child(1) > .MuiCardContent-root').contains('Test Issue body')
+      })
+      it('Notes comments to be visible', () => {
+        cy.get(':nth-child(2) > .MuiPaper-root > .MuiCardContent-root').contains('Test Comment 1')
+      })
   })
 })
