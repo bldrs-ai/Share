72135d3d30c1cf49acb222a4b288460d5816881b
OlegMoshkovich <oleg.mosh@gmail.com>
2024-04-15 14:53:21 +0200

    adjust the tests

--- cypress/e2e/notes-100/access-shared-note.cy.js
+++ cypress/e2e/notes-100/access-shared-note.cy.js
@@ -9,9 +9,6 @@ describe('Notes 100 - Access shared note', () => {
       cy.visit('/share/v/p/index.ifc#c:-133.022,131.828,161.85,-38.078,22.64,-2.314;i:')
       waitForModel()
     })
-    it('Correct project to be loaded into the viewport and side drawer to be open', () => {
-      cy.get('.css-1q8dpte').should('exist')
-    })
     it('Panel title to contain NOTES string', () => {
       cy.get('[data-testid="panelTitle"]').contains('NOTE')
     })
@@ -26,14 +23,11 @@ describe('Notes 100 - Access shared note', () => {
       cy.visit('http://localhost:8080/share/v/p/index.ifc#c:-26.91,28.84,112.47,-22,16.21,-3.48;i:2')
       waitForModel()
     })
-      it('Correct project to be loaded into the viewport and side drawer to be open', () => {
-        cy.get('.css-1q8dpte').should('exist')
-      })
       it('Panel title to contain NOTE string', () => {
         cy.get('[data-testid="panelTitle"]').contains('NOTE')
       })
       it('Panel nav to contain back button', () => {
-        cy.get('[data-testid="Back to the list"]')
+        cy.get('[data-testid="Back to the list"]').should('exist')
       })
       it('Shared note to be visible', () => {
         cy.get('.MuiList-root > :nth-child(1) > .MuiCardContent-root').contains('Test Issue body')
