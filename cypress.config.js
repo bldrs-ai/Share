833fe55482f02ae091139333a8c3ab5f9110c559
Pablo Mayrgundter <pablo.mayrgundter@gmail.com>
2024-04-19 15:54:14 +0200

    Tune cypress pageLoadTimeout to 15s to fit GHA response

--- cypress.config.js
+++ cypress.config.js
@@ -7,6 +7,6 @@ module.exports = defineConfig({
     fileServerFolder: 'docs/',
     screenshotOnRunFailure: false,
     video: false,
-    pageLoadTimeout: 2500,
+    pageLoadTimeout: 15000,
   },
 })
