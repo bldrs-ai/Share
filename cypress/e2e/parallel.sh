f722bef6a46f742e02e75be4385b49c7479cf893
Pablo Mayrgundter <pablo.mayrgundter@gmail.com>
2024-04-19 14:42:59 -0400

    Introduce cy-parallel pkg script (#1133)
    * Introduce cy-parallel pkg script.  Also tune cypress pageLoadTimeout from 60s to 2.5s to speedup broken tests.
    
    * Tune cypress pageLoadTimeout to 15s to fit GHA response
    
    * Remove AppStore.  Test pageLoadTimeout 2500 on GHA
    
    * Tune cypress pageLoadTimeout to 15s to fit GHA response, again
    
    * Delete cypress/e2e/create-100/imagine2.cy.js

---
+++ cypress/e2e/parallel.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+yarn install
+yarn cy-build
+LOGS_DIR=cypress/e2e/logs
+mkdir -p $LOGS_DIR
+
+run_cy_spec() {
+    local epic=$1
+    local specs=$2
+    local log="$LOGS_DIR/$epic.log"
+    (
+        yarn cy-spec $specs > $log 2> $log.err
+        echo -n "$epic "
+        tail -4 $log | egrep '(passed|failed)'
+    )&
+}
+
+echo "Running cypress specs in parallel..."
+
+# Misc
+run_cy_spec misc cypress/e2e/appStore,cypress/e2e/hide-feat,cypress/e2e/home,cypress/e2e/ifc-model,cypress/e2e/integration
+
+# Then conventional
+for EPIC in create profile notes versions view ; do
+    SPECS="cypress/e2e/$EPIC-100"
+    run_cy_spec $EPIC "$SPECS"
+done
+
+wait
+echo "All specs finished.  See $LOGS_DIR for output from each spec"
