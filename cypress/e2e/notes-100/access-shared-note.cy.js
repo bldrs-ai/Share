6057d506feb834b5a81b2c285bc9547fe41d5d75
OlegMoshkovich <oleg.mosh@gmail.com>
2024-04-23 08:13:38 +0200

    addressed comments

--- cypress/e2e/notes-100/access-shared-note.cy.js
+++ cypress/e2e/notes-100/access-shared-note.cy.js
@@ -1,26 +1,31 @@
-import {waitForModel, homepageSetup} from '../../support/utils'
+import {waitForModel, homepageSetup, setIsReturningUser} from '../../support/utils'
 
-// From https://github.com/bldrs-ai/Share/issues/1072
+/** {@link https://github.com/bldrs-ai/Share/issues/1072} */
 describe('Notes 100 - Access shared note', () => {
   context('Access shared link containing the path to the general notes', () => {
     beforeEach(() => {
       homepageSetup()
-      cy.setCookie('isFirstTime', '1')
+      setIsReturningUser()
+    })
+    beforeEach(() => {
       cy.visit('/share/v/p/index.ifc#c:-133.022,131.828,161.85,-38.078,22.64,-2.314;i:')
       waitForModel()
     })
-    it('Panel title to contain NOTES string', () => {
+    it('Correct project to be loaded into the viewport and side drawer to be open - Screen', () => {
+      // Panel title to contain 'NOTES' string
       cy.get('[data-testid="panelTitle"]').contains('NOTES')
-    })
-    it('Notes list to be visible', () => {
+      // List of notes to be visible
       cy.get('.MuiList-root').should('exist')
-    })
+      cy.percySnapshot()
+      })
   })
   context('Access the shared link containing the path to a note index', () => {
     beforeEach(() => {
       homepageSetup()
-      cy.setCookie('isFirstTime', '1')
-      cy.visit('http://localhost:8080/share/v/p/index.ifc#c:-26.91,28.84,112.47,-22,16.21,-3.48;i:2')
+      setIsReturningUser()
+    })
+    beforeEach(() => {
+      cy.visit('/share/v/p/index.ifc#c:-26.91,28.84,112.47,-22,16.21,-3.48;i:2')
       waitForModel()
     })
       it('Panel title to contain NOTE string', () => {
