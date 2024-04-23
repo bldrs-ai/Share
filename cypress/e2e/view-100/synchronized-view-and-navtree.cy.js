f24bbe4cbf02f8278a9bcbd19330c843009c564f
Pablo Mayrgundter <pablo.mayrgundter@gmail.com>
2024-04-23 10:57:26 -0400

    view-100/synchronized-view-and-navtree.cy.js (#1141)
    * view-100/synchronized-view-and-navtree.cy.js
    
    * wait for correct bounce

---
+++ cypress/e2e/view-100/synchronized-view-and-navtree.cy.js
@@ -0,0 +1,53 @@
+import '@percy/cypress'
+import {
+  homepageSetup,
+  setIsReturningUser,
+  visitHomepageWaitForModel,
+} from '../../support/utils'
+import {
+  waitForModelReady,
+} from '../../support/models'
+
+
+/** {@link https://github.com/bldrs-ai/Share/issues/1046} */
+describe('View 100: Synchronized View and NavTree', () => {
+  beforeEach(() => {
+    homepageSetup()
+    setIsReturningUser()
+  })
+
+  context('User visits homepage, Open NavTree > select item', () => {
+    beforeEach(() => {
+      visitHomepageWaitForModel()
+      cy.get('[data-testid="control-button-navigation"]').click()
+      cy.findByText('Bldrs').should('be.visible').click()
+      cy.findByText('Build').should('be.visible').click()
+      cy.findByText('Every').should('be.visible').click()
+      cy.findByText('Thing').should('be.visible').click()
+      cy.findAllByText('Together').then((elt) => elt[0]).should('be.visible').click()
+      cy.findAllByText('Together').then((elt) => elt[1]).should('be.visible').click()
+      cy.findAllByText('Together').then((elt) => elt[2]).should('be.visible').click()
+      cy.findAllByText('Together').then((elt) => elt[3]).should('be.visible').click()
+      cy.findAllByText('Together').then((elt) => elt[4]).should('be.visible').click()
+      cy.findAllByText('Together').then((elt) => elt[5]).should('be.visible').click()
+      cy.findAllByText('Together').then((elt) => elt[6]).should('be.visible').click()
+      cy.findAllByText('Together').then((elt) => elt[7]).should('be.visible').click()
+    })
+
+    it('Item highlighted in tree and scene - Screen', () => {
+      cy.percySnapshot()
+    })
+  })
+
+  context.only('Visits permalink to selected element', () => {
+    beforeEach(() => {
+      // TODO(pablo): can't figure out how to get intercept for deep path, but this works
+      cy.visit('/share/v/p/index.ifc/621')
+      waitForModelReady('bounceEltSelect')
+    })
+
+    it('Item highlighted in tree and scene - Screen', () => {
+      cy.percySnapshot()
+    })
+  })
+})
