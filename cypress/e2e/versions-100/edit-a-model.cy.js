102447627990fdc42e535c1a166078d193f65d0f
nickcastel50 <nickcastel50@gmail.com>
2024-04-30 23:51:26 -0400

    * wip added cypress tests: open 100: open model on GH via open UI and versions 100: edit-a-model

---
+++ cypress/e2e/versions-100/edit-a-model.cy.js
@@ -0,0 +1,62 @@
+import '@percy/cypress'
+import {
+  auth0Login,
+  homepageSetup,
+  returningUserVisitsHomepageWaitForModel,
+} from '../../support/utils'
+import {
+    setupVirtualPathIntercept,
+    waitForModelReady,
+  } from '../../support/models'
+
+
+/** {@link https://github.com/bldrs-ai/Share/issues/1160}*/
+describe('Versions 100: Edit a specific version', () => {
+  beforeEach(homepageSetup)
+  context('Returning user visits homepage', () => {
+    beforeEach(returningUserVisitsHomepageWaitForModel)
+
+    context('User login', () => {
+      beforeEach(auth0Login)
+
+      const interceptTag = 'ghOpenModelLoad'
+      const overwriteVersionInterceptTag = 'ghNewModelLoad'
+
+      it('Opens a model from Github', () => {
+        // set up initial index.ifc load
+        setupVirtualPathIntercept(
+          '/share/v/gh/cypresstester/test-repo/main/window.ifc',
+          '/index.ifc',
+          interceptTag)
+
+        cy.get('[data-testid="control-button-open"]').click()
+        cy.findByLabelText('Organization', {timeout: 5000}).click()
+        cy.contains('@cypresstester').click()
+        cy.findByLabelText('Repository', {timeout: 5000}).eq(0).click()
+        cy.contains('test-repo').click()
+        cy.findByLabelText('File', {timeout: 5000}).eq(0).click()
+        cy.contains('window.ifc').click()
+        cy.contains('button', 'Load file').click()
+        waitForModelReady(interceptTag)
+      })
+
+      it('Overwrites model with a new model on GitHub', () => {
+        // set up initial momentum.ifc load
+        setupVirtualPathIntercept(
+          '/share/v/gh/cypresstester/test-repo/main/window.ifc',
+          '/Momentum.ifc',
+          overwriteVersionInterceptTag)
+        cy.findByTitle('Save', {timeout: 5000}).should('exist').click({force: true})
+        cy.findByLabelText('Organization', {timeout: 5000}).click()
+        cy.contains('@cypresstester').click()
+        cy.findByLabelText('Repository', {timeout: 5000}).eq(0).click()
+        cy.contains('test-repo').click()
+        cy.findByLabelText('Enter file name').click().type('window.ifc')
+        // cy.percySnapshot(`${percyLabelPrefix} form filled`)
+
+        cy.contains('button', 'Save model').click()
+        waitForModelReady(overwriteVersionInterceptTag)
+      })
+    })
+  })
+})
