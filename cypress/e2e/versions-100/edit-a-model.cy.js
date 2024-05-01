1f15e9687acad78ed1d8f445fd76e3710e47b21e
nickcastel50 <nickcastel50@gmail.com>
2024-05-01 15:00:22 -0400

    * updated edit-a-model test

--- cypress/e2e/versions-100/edit-a-model.cy.js
+++ cypress/e2e/versions-100/edit-a-model.cy.js
@@ -13,40 +13,20 @@ import {
 /** {@link https://github.com/bldrs-ai/Share/issues/1160}*/
 describe('Versions 100: Edit a specific version', () => {
   beforeEach(homepageSetup)
-  context('Returning user visits homepage', () => {
-    beforeEach(returningUserVisitsHomepageWaitForModel)
 
-    context('User login', () => {
-      beforeEach(auth0Login)
+  context('Returning user visits homepage, logs in', () => {
+    const overwriteVersionInterceptTag = 'ghNewModelLoad'
+    beforeEach(() => {
+      returningUserVisitsHomepageWaitForModel()
+      auth0Login()
+      setupVirtualPathIntercept(
+        '/share/v/gh/cypresstester/test-repo/main/window.ifc',
+        '/Momentum.ifc',
+        overwriteVersionInterceptTag)
+    })
 
-      const interceptTag = 'ghOpenModelLoad'
-      const overwriteVersionInterceptTag = 'ghNewModelLoad'
-
-      it('Opens a model from Github', () => {
-        // set up initial index.ifc load
-        setupVirtualPathIntercept(
-          '/share/v/gh/cypresstester/test-repo/main/window.ifc',
-          '/index.ifc',
-          interceptTag)
-
-        cy.get('[data-testid="control-button-open"]').click()
-        cy.findByLabelText('Organization', {timeout: 5000}).click()
-        cy.contains('@cypresstester').click()
-        cy.findByLabelText('Repository', {timeout: 5000}).eq(0).click()
-        cy.contains('test-repo').click()
-        cy.findByLabelText('File', {timeout: 5000}).eq(0).click()
-        cy.contains('window.ifc').click()
-        cy.contains('button', 'Load file').click()
-        waitForModelReady(interceptTag)
-        cy.percySnapshot()
-      })
-
-      it('Overwrites model with a new model on GitHub', () => {
-        // set up initial momentum.ifc load
-        setupVirtualPathIntercept(
-          '/share/v/gh/cypresstester/test-repo/main/window.ifc',
-          '/Momentum.ifc',
-          overwriteVersionInterceptTag)
+    // Assumes this flow's file exists cypress/e2e/open/100/open-model-from-gh-ui.cy.js
+    it('Save index.ifc to new name, that overwrites existing other file', () => {
         cy.findByTitle('Save', {timeout: 5000}).should('exist').click({force: true})
         cy.findByLabelText('Organization', {timeout: 5000}).click()
         cy.contains('@cypresstester').click()
@@ -56,7 +36,6 @@ describe('Versions 100: Edit a specific version', () => {
         cy.contains('button', 'Save model').click()
         waitForModelReady(overwriteVersionInterceptTag)
         cy.percySnapshot()
-      })
     })
   })
 })
