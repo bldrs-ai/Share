7ac185897ff4d303f14308216cc01756bc039c05
nickcastel50 <nickcastel50@gmail.com>
2024-05-01 15:22:50 -0400

    Update open-model-from-gh-ui.cy.js

--- cypress/e2e/open/100/open-model-from-gh-ui.cy.js
+++ cypress/e2e/open/100/open-model-from-gh-ui.cy.js
@@ -13,32 +13,30 @@ import {
 /** {@link https://github.com/bldrs-ai/Share/issues/1159}*/
 describe('Open 100: Open model from GH via UI', () => {
   beforeEach(homepageSetup)
-  context('Returning user visits homepage', () => {
-    beforeEach(returningUserVisitsHomepageWaitForModel)
+  context('Returning user visits homepage, logs in', () => {
+    beforeEach(() => {
+      returningUserVisitsHomepageWaitForModel()
+      auth0Login()
+      // set up initial index.ifc load
+      setupVirtualPathIntercept(
+        '/share/v/gh/cypresstester/test-repo/main/window.ifc',
+        '/index.ifc',
+        interceptTag)
+    })
 
-    context('User login', () => {
-      beforeEach(auth0Login)
+    const interceptTag = 'ghOpenModelLoad'
+    it('Opens a model from Github via the UI', () => {
+      cy.get('[data-testid="control-button-open"]').click()
+      cy.findByLabelText('Organization', {timeout: 5000}).click()
+      cy.contains('@cypresstester').click()
+      cy.findByLabelText('Repository', {timeout: 5000}).eq(0).click()
+      cy.contains('test-repo').click()
+      cy.findByLabelText('File', {timeout: 5000}).eq(0).click()
+      cy.contains('window.ifc').click()
+      cy.contains('button', 'Load file').click()
 
-      const interceptTag = 'ghOpenModelLoad'
-      it('Opens a model from Github via the UI', () => {
-        // set up initial index.ifc load
-        setupVirtualPathIntercept(
-          '/share/v/gh/cypresstester/test-repo/main/window.ifc',
-          '/index.ifc',
-          interceptTag)
-
-        cy.get('[data-testid="control-button-open"]').click()
-        cy.findByLabelText('Organization', {timeout: 5000}).click()
-        cy.contains('@cypresstester').click()
-        cy.findByLabelText('Repository', {timeout: 5000}).eq(0).click()
-        cy.contains('test-repo').click()
-        cy.findByLabelText('File', {timeout: 5000}).eq(0).click()
-        cy.contains('window.ifc').click()
-        cy.contains('button', 'Load file').click()
-
-        waitForModelReady(interceptTag)
-        cy.percySnapshot()
-      })
+      waitForModelReady(interceptTag)
+      cy.percySnapshot()
     })
   })
 })
