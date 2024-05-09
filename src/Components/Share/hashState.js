c1e9353f567cb7c70a95b7d2f115aba0559a9f79
Pablo Mayrgundter <pablo.mayrgundter@gmail.com>
2024-05-09 20:05:31 +0200

    Visiting Share permalink does not show Share dialog again

--- src/Components/Share/hashState.js
+++ src/Components/Share/hashState.js
@@ -1,11 +1,8 @@
-import {hasParams} from '../../utils/location'
-
-
 /** The prefix to use for the Share state token */
 export const HASH_PREFIX_SHARE = 'share'
 
 
 /** @return {boolean} */
 export function isVisibleInitially() {
-  return hasParams(HASH_PREFIX_SHARE)
+  return false
 }
