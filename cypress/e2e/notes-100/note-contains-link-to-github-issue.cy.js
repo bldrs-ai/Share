e110ea08d9ab3c916098094c23b56eeb90f8abad
OlegMoshkovich <oleg.mosh@gmail.com>
2024-05-13 13:18:17 +0200

    chnage the github test

--- cypress/e2e/notes-100/note-contains-link-to-github-issue.cy.js
+++ cypress/e2e/notes-100/note-contains-link-to-github-issue.cy.js
@@ -16,10 +16,13 @@ describe('Notes 100: Note contains link to GitHub', () => {
         cy.percySnapshot()
       })
       context('Open first note', () => {
-        beforeEach(() => cy.get('[data-testid="list-notes"] :nth-child(1) > [data-testid="note-body"]').first().click())
+        beforeEach(() => cy.get(`
+        [data-testid="list-notes"]
+        :nth-child(2) > [data-testid="note-card"] > [data-testid="note-body"]`).first().click())
         it('Github link is visible on a note card when the note selected - Screen', () => {
-          cy.get('.MuiCardHeader-title').contains('issueTitle_4')
+          cy.get('.MuiCardHeader-title').contains('issueTitle_3')
           cy.get('[data-testid="Open in Github"]')
+          cy.get('[data-testid="list-notes"] > :nth-child(4) > [data-testid="note-card"] p').contains('testComment_1')
           cy.percySnapshot()
         })
       })
