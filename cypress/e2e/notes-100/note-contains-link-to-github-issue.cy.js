f382ee7be5c3efe94a3ffcebfb741f4b7deff53c
OlegMoshkovich <oleg.mosh@gmail.com>
2024-05-13 14:53:35 +0200

    fix the test

--- cypress/e2e/notes-100/note-contains-link-to-github-issue.cy.js
+++ cypress/e2e/notes-100/note-contains-link-to-github-issue.cy.js
@@ -18,11 +18,13 @@ describe('Notes 100: Note contains link to GitHub', () => {
       context('Open first note', () => {
         beforeEach(() => cy.get(`
         [data-testid="list-notes"]
-        :nth-child(2) > [data-testid="note-card"] > [data-testid="note-body"]`).first().click())
+        :nth-child(1) > [data-testid="note-card"] > [data-testid="note-body"]`).first().click())
         it('Github link is visible on a note card when the note selected - Screen', () => {
-          cy.get('.MuiCardHeader-title').contains('issueTitle_3')
+          cy.get('.MuiCardHeader-title').contains('issueTitle_4')
           cy.get('[data-testid="Open in Github"]')
           cy.get('[data-testid="list-notes"] > :nth-child(4) > [data-testid="note-card"] p').contains('testComment_1')
+          // eslint-disable-next-line cypress/no-unnecessary-waiting, no-magic-numbers
+          cy.wait(1000)
           cy.percySnapshot()
         })
       })
