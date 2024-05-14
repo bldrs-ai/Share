2456403bee892681ed4c384053eb3a098d7cf5b3
Pablo Mayrgundter <pablo.mayrgundter@gmail.com>
2024-05-14 12:41:04 +0200

    CadView: fix resetState bugs

--- cypress/e2e/open/100/open-sample-model.cy.js
+++ cypress/e2e/open/100/open-sample-model.cy.js
@@ -12,32 +12,78 @@ import {
 
 /** {@link https://github.com/bldrs-ai/Share/issues/757} */
 describe('Open 100: Open Sample Model', () => {
-  beforeEach(homepageSetup)
-  context('Returning user visits homepage, select OpenModelControl > Sample Models', () => {
+  context('Returning user visits homepage', () => {
     beforeEach(() => {
+      homepageSetup()
       setIsReturningUser()
       visitHomepageWaitForModel()
-      cy.get('[data-testid="control-button-open"]').click()
-      cy.get('[data-testid="textfield-sample-projects"]').click()
     })
 
-    it('Sample project list appears, including Momentum etc. - Screen', () => {
-      cy.percySnapshot()
-    })
-
-    context('Choose one of the projects from the list', () => {
-      const interceptTag = 'ghModelLoad'
+    context('Select OpenModelControl > Sample Models', () => {
       beforeEach(() => {
+        cy.get('[data-testid="control-button-open"]').click()
+        cy.get('[data-testid="textfield-sample-projects"]').click()
+      })
+
+      it('Sample project list appears, including Momentum etc. - Screen', () => {
+        cy.percySnapshot()
+      })
+
+      context('Choose one of the projects from the list', () => {
+        const interceptTag = 'ghModelLoad'
+        beforeEach(() => {
+          setupVirtualPathIntercept(
+            '/share/v/gh/Swiss-Property-AG/Momentum-Public/main/Momentum.ifc',
+            '/Momentum.ifc',
+            interceptTag,
+          )
+          cy.findByText('Momentum').click()
+          waitForModelReady(interceptTag)
+        })
+
+        it('Project loads - Screen', () => {
+          cy.percySnapshot()
+        })
+      })
+    })
+
+    context('Open up all persistent controls', () => {
+      beforeEach(() => {
+        // Select element, opens nav
+        const interceptEltSelectTag = 'twoLevelSelect'
+        cy.intercept('GET', '/share/v/p/index.ifc/81/621', {fixture: '404.html'}).as(interceptEltSelectTag)
+        cy.visit('/share/v/p/index.ifc/81/621')
+        waitForModelReady(interceptEltSelectTag)
+
+        // Open properties
+        cy.get('[data-testid="control-button-properties"]').click()
+
+        // Open notes
+        cy.get('[data-testid="control-button-notes"]').click()
+
+        // Open search
+        cy.get('[data-testid="control-button-search"]').click()
+
+        // Add a cutplane
+        cy.get('[data-testid="control-button-cut-plane"]').click()
+        cy.get('[data-testid="menu-cut-plane"]').should('exist')
+        cy.get('[data-testid="menu-item-plan"]').click()
+
+        // Select a sample project
+        cy.get('[data-testid="control-button-open"]').click()
+        cy.get('[data-testid="textfield-sample-projects"]').click()
+
+        const interceptModelLoadTag = 'ghModelLoad'
         setupVirtualPathIntercept(
           '/share/v/gh/Swiss-Property-AG/Momentum-Public/main/Momentum.ifc',
           '/Momentum.ifc',
-          interceptTag,
+          interceptModelLoadTag,
         )
         cy.findByText('Momentum').click()
+        waitForModelReady(interceptModelLoadTag)
       })
 
-      it('Project loads - Screen', () => {
-        waitForModelReady(interceptTag)
+      it('Project loads, all controls reset - Screen', () => {
         cy.percySnapshot()
       })
     })
