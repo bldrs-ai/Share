702c20070a1ab5642601c4bb08ece703096a35ca
OlegMoshkovich <oleg.mosh@gmail.com>
2024-05-30 11:16:15 +0200

    add edit tests

---
+++ cypress/e2e/notes-100/comment-edit.cy.js
@@ -0,0 +1,35 @@
+import '@percy/cypress'
+import {
+  homepageSetup,
+  returningUserVisitsHomepageWaitForModel,
+  auth0Login,
+} from '../../support/utils'
+
+/** {@link https://github.com/bldrs-ai/Share/issues/1186} */
+describe('Notes 100: Comment edit', () => {
+  beforeEach(homepageSetup)
+  context('Returning user visits homepage', () => {
+    beforeEach(returningUserVisitsHomepageWaitForModel)
+    context('Open Notes > first note', () => {
+      beforeEach(() => {
+        cy.get('[data-testid="control-button-notes"]').click()
+        cy.get('[data-testid="list-notes"] :nth-child(1) > [data-testid="note-body"]').first().click()
+      })
+      it('Comment switches to edit mode', () => {
+        auth0Login()
+        cy.get(`[data-testid="list-notes"] > :nth-child(3)
+        > [data-testid="note-card"] > .MuiCardActions-root > .MuiBox-root > [data-testid="editComment"]`).click()
+        cy.get('[data-testid="Save"]')
+        cy.percySnapshot()
+      })
+      it('Comment display updated body', () => {
+        auth0Login()
+        cy.get(`[data-testid="list-notes"] > :nth-child(3)
+        > [data-testid="note-card"] > .MuiCardActions-root > .MuiBox-root > [data-testid="editComment"]`).click()
+        cy.get('[placeholder="Note body"]').click().type(' updated body')
+        cy.get('[data-testid="Save"]').click()
+        cy.percySnapshot()
+      })
+    })
+  })
+})
