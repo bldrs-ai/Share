a008456933da18d382254ed4c1d837237974c586
OlegMoshkovich <oleg.mosh@gmail.com>
2024-06-03 22:59:12 +0200

    change the function comment

--- src/Components/Tabs.jsx
+++ src/Components/Tabs.jsx
@@ -8,7 +8,7 @@ import {assertDefined} from '../utils/assert'
  * @property {Array<string>} tabLabels Names of each tab
  * @property {Function} actionCb callBack fired when the tabs is selected, returns currect tab number
  * @property {boolean} [isScrollable] Enable scrolling for many (> 5) tabs
- * @return {ReactElement.Component}
+ * @return {ReactElement}
  */
 export default function Tabs({tabLabels, currentTab, actionCb, isScrollable = false}) {
   assertDefined(tabLabels, actionCb)
