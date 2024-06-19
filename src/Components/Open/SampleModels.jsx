93dcb9aae458d5dc187b6c3c1acf974d15143b57
OlegMoshkovich <oleg.mosh@gmail.com>
2024-06-19 11:36:33 +0200

    Open 200 - Tabbed Open dialog (#1195)

---
+++ src/Components/Open/SampleModels.jsx
@@ -0,0 +1,75 @@
+import React, {ReactElement, useState} from 'react'
+import {Grid, Chip, Typography} from '@mui/material'
+import Bplaza from '../../assets/icons/Bplaza.svg'
+import Eisvogel from '../../assets/icons/Eisvogel.svg'
+import Gear from '../../assets/icons/Gear.svg'
+import Momentum from '../../assets/icons/Momentum.svg'
+import Placeholder from '../../assets/icons/Placeholder.svg'
+import Schependomlaan from '../../assets/icons/Schependomlaan.svg'
+import Seestrasse from '../../assets/icons/Seestrasse.svg'
+import Sheenstock from '../../assets/icons/Sheenstock.svg'
+
+
+/**
+ * @property {Function} setIsDialogDisplayed callback
+ * @property {Function} navigate Callback from OpenModelDialog to change page url
+ * @return {ReactElement}
+ */
+export default function SampleModels({navigate, setIsDialogDisplayed}) {
+  const [, setSelected] = useState('')
+  const iconsStyle = {height: '1.6em'}
+  const modelPath = {
+    Momentum: '/share/v/gh/Swiss-Property-AG/Momentum-Public/main/Momentum.ifc#c:-38.64,12.52,35.4,-5.29,0.94,0.86',
+    Schneestock: '/share/v/gh/Swiss-Property-AG/Schneestock-Public/main/ZGRAGGEN.ifc#c:80.66,11.66,-94.06,6.32,2.93,-8.72',
+    Eisvogel: '/share/v/gh/Swiss-Property-AG/Eisvogel-Public/main/EISVOGEL.ifc#c:107.36,8.46,156.67,3.52,2.03,16.71',
+    Seestrasse: '/share/v/gh/Swiss-Property-AG/Seestrasse-Public/main/SEESTRASSE.ifc#c:119.61,50.37,73.68,16.18,11.25,5.74',
+    Schependomlaan: '/share/v/gh/bldrs-ai/test-models/main/ifc/Schependomlaan.ifc#c:60.45,-4.32,60.59,1.17,5.93,-3.77',
+    Structural_detail: '/share/v/gh/bldrs-ai/test-models/main/ifc/openifcmodels/171210AISC_Sculpture_param.ifc',
+    Bldrs_plaza: '/share/v/gh/OlegMoshkovich/Bldrs_Plaza/main/IFC_STUDY.ifc#c:220.607,-9.595,191.198,12.582,27.007,-21.842',
+    Gear: '/share/v/gh/bldrs-ai/test-models/main/step/zoo.dev/a-gear.step',
+  }
+
+  const modelIcon = {
+    Momentum: <Momentum style={iconsStyle}/>,
+    Schneestock: <Sheenstock style={iconsStyle}/>,
+    Eisvogel: <Eisvogel style={iconsStyle}/>,
+    Seestrasse: <Seestrasse style={iconsStyle}/>,
+    Schependomlaan: <Schependomlaan style={iconsStyle}/>,
+    Structural_detail: <Placeholder style={iconsStyle}/>,
+    Bldrs_plaza: <Bplaza style={iconsStyle}/>,
+    Gear: <Gear style={iconsStyle}/>,
+  }
+
+  const handleSelect = (modelName, closeDialog) => {
+    setSelected(modelName)
+    navigate({pathname: modelPath[modelName]})
+    closeDialog()
+  }
+
+  return (
+    <Grid
+      container
+      spacing={2}
+      justifyContent="center"
+      alignItems="center"
+    >
+      {Object.keys(modelPath).map((model, i) => (
+        <Grid item xs={6} key={i}>
+          <Chip
+            label={
+              <>
+                {modelIcon[model]}
+                <Typography variant="caption" sx={{marginTop: '.5em'}}>{model}</Typography>
+              </>
+            }
+            variant="sampleModel"
+            data-testid='sample-model-chip'
+            onClick={() => handleSelect(model, () => setIsDialogDisplayed(false))}
+            color="primary"
+          />
+        </Grid>
+      ))}
+    </Grid>
+  )
+}
+
--- src/Components/Tabs.jsx
+++ src/Components/Tabs.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useState, ReactElement} from 'react'
 import MuiTabs from '@mui/material/Tabs'
 import Tab from '@mui/material/Tab'
 import {assertDefined} from '../utils/assert'
@@ -8,18 +8,18 @@ import {assertDefined} from '../utils/assert'
  * @property {Array<string>} tabLabels Names of each tab
  * @property {Function} actionCb callBack fired when the tabs is selected, returns currect tab number
  * @property {boolean} [isScrollable] Enable scrolling for many (> 5) tabs
- * @return {React.Component}
+ * @return {ReactElement}
  */
-export default function Tabs({tabLabels, actionCb, isScrollable = false}) {
+export default function Tabs({tabLabels, currentTab, actionCb, isScrollable = false}) {
   assertDefined(tabLabels, actionCb)
-  const [value, setValue] = useState(0)
+  const [value, setValue] = useState(currentTab)
   const handleChange = (event, newValue) => {
     setValue(newValue)
     actionCb(newValue)
   }
   return (
     <MuiTabs value={value} onChange={handleChange} centered variant={isScrollable ? 'scrollable' : 'fullWidth'}>
-      {tabLabels.map((tab) => <Tab key={tab} label={tab}/>)}
+      {tabLabels.map((tab) => <Tab key={tab} label={tab} data-testid={`tab-${tab.toLowerCase()}`}/>)}
     </MuiTabs>
   )
 }
