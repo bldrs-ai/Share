d2cad658aee57050b79a3bf12a25dab82707f4cd
OlegMoshkovich <oleg.mosh@gmail.com>
2024-07-24 14:05:15 +0200

    Disable nav tree activation on the element select (#1239)
    * disable nav tree activation on the element select
    
    * add unit test
    
    * clean up test
    
    * adde shouild not exist condition to the e2e test

---
+++ src/Components/NavTree/NavTreeControl.test.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import {act, render, renderHook, fireEvent} from '@testing-library/react'
+import ShareMock from '../../ShareMock'
+import useStore from '../../store/useStore'
+import NavTreeControl from './NavTreeControl'
+
+
+describe('NavTree', () => {
+  it('NavTree to remain close when an element is selected', async () => {
+    const testLabel = 'control-button-navigation'
+    const {result} = renderHook(() => useStore((state) => state))
+    await act(() => {
+      result.current.setSelectedElement({id: 123})
+    })
+    const {getByTestId} = render(
+        <ShareMock>
+          <NavTreeControl/>
+        </ShareMock>)
+    expect(getByTestId(testLabel)).toBeInTheDocument()
+    expect(result.current.isNavTreeVisible).toEqual(false)
+  })
+  it('NavTree to open when nav control button is pressed', async () => {
+    const testLabel = 'control-button-navigation'
+    const {result} = renderHook(() => useStore((state) => state))
+    await act(() => {
+      result.current.setSelectedElement({id: 123})
+    })
+    const {getByTestId} = render(
+        <ShareMock>
+          <NavTreeControl/>
+        </ShareMock>)
+    const controlButton = getByTestId(testLabel)
+    expect(getByTestId(testLabel)).toBeInTheDocument()
+    fireEvent.click(controlButton)
+    expect(result.current.isNavTreeVisible).toEqual(true)
+  })
+})
