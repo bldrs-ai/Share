e11b2dfee1eb9f3c80a330b1995dcf5d8bd33025
OlegMoshkovich <oleg.mosh@gmail.com>
2024-07-24 12:02:05 +0200

    add accee properties test

---
+++ cypress/e2e/view-100/access-element-properties.cy.js
@@ -0,0 +1,31 @@
+import '@percy/cypress'
+import {
+  homepageSetup,
+  setIsReturningUser,
+} from '../../support/utils'
+import {
+  waitForModelReady,
+} from '../../support/models'
+
+
+/** {@link https://github.com/bldrs-ai/Share/issues/1242} */
+describe('View 100: Access elements property', () => {
+  beforeEach(() => {
+    homepageSetup()
+    setIsReturningUser()
+  })
+
+  context('User visits permalink to selected element and clicks properties control', () => {
+    beforeEach(() => {
+      cy.intercept('GET', '/share/v/p/index.ifc/81/621', {fixture: '404.html'}).as('twoLevelSelect')
+      cy.visit('/share/v/p/index.ifc/81/621')
+      waitForModelReady('twoLevelSelect')
+      cy.get('[data-testid="control-button-properties"]').click()
+    })
+
+    it('Side drawer containing properties shall be visible', () => {
+      cy.get('[data-testid="control-button-properties"]').should('be.visible')
+      cy.percySnapshot()
+    })
+  })
+})
