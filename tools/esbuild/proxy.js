c5599e39de61bbfcae66e66f9aeb53eca1159e87
nickcastel50 <nickcastel50@gmail.com>
2024-10-11 18:00:36 -0400

    * update proxy to properly reference debug wasm if it exists + update package.json

--- tools/esbuild/proxy.js
+++ tools/esbuild/proxy.js
@@ -1,13 +1,20 @@
 import http from 'node:http'
+import https from 'node:https'
 
 /**
  * @param {string} proxiedHost The host to which traffic will be sent. E.g. localhost
  * @param {number} port The port to which traffic will be sent.  E.g. 8079
+ * @param {boolean} useHttps Whether to use HTTPS for the proxied server
  * @return {object} Proxy server
  * @see https://esbuild.github.io/api/#serve-proxy
  */
-export function createProxyServer(host, port) {
+export function createProxyServer(host, port, useHttps = false) {
+  const requestModule = useHttps ? https : http
+
   return http.createServer((req, res) => {
+    // Rewrite the URL if it matches the pattern for a .wasm file
+    req.url = rewriteUrl(req.url)
+
     const options = {
       hostname: host,
       port: port,
@@ -16,25 +23,40 @@ export function createProxyServer(host, port) {
       headers: req.headers,
     }
 
-    // Forward each incoming request to esbuild
-    const proxyReq = http.request(options, (proxyResponse) => {
-      // If esbuild cannot find the resource to serve, send our react-router bounce.
+    // Forward each incoming request to the proxied server
+    const proxyReq = requestModule.request(options, (proxyResponse) => {
+      // If proxied server cannot find the resource, send a custom not-found page
       if (proxyResponse.statusCode === HTTP_NOT_FOUND) {
         serveNotFound(res)
         return
       }
 
-      // Set the correct Content-Type for .js files
-      if (req.url.endsWith('.js')) {
-        res.setHeader('Content-Type', 'application/javascript')
+      // Set the correct Content-Type for specific file types
+      const contentType = getContentType(req.url)
+      if (contentType) {
+        res.setHeader('Content-Type', contentType)
       }
 
-      // Otherwise, forward the response from esbuild to the client
+      // Optionally set Cache-Control headers for static assets
+      if (isCacheable(req.url)) {
+        res.setHeader('Cache-Control', 'public, max-age=31536000')
+      }
+
+      // Forward the response from the proxied server to the client
       res.writeHead(proxyResponse.statusCode, proxyResponse.headers)
       proxyResponse.pipe(res, {end: true})
     })
 
-    // Forward the body of the request to esbuild
+    // Handle request errors
+    proxyReq.on('error', (err) => {
+      // eslint-disable-next-line no-console
+      console.error(`Proxy request error: ${err.message}`)
+      // eslint-disable-next-line no-magic-numbers
+      res.writeHead(500)
+      res.end('Internal Server Error')
+    })
+
+    // Forward the body of the request to the proxied server
     req.pipe(proxyReq, {end: true})
   })
 }
@@ -43,7 +65,7 @@ const HTTP_FOUND = 200
 const HTTP_NOT_FOUND = 404
 
 /** Serve a 200 bounce page for missing resources. */
-const serveNotFound = ((res) => {
+const serveNotFound = (res) => {
   res.writeHead(HTTP_FOUND, {'Content-Type': 'text/html'})
   res.end(`<!DOCTYPE html>
 <html>
@@ -51,23 +73,62 @@ const serveNotFound = ((res) => {
     <meta charset="utf-8">
     <title>BLDRS - Redirect</title>
     <script type="text/javascript">
-      // Single Page Apps for GitHub Pages
-      // MIT License
-      // https://github.com/rafgraph/spa-github-pages
-      // This page needs to be > 512 bytes to work for IE.  Currently 968.
       var pathSegmentsToKeep = window.location.pathname.startsWith('/Share') ? 1 : 0
-
       var l = window.location
       var u1 = l.protocol + '//' + l.hostname + (l.port ? ':' + l.port : '')
       var u2 = l.pathname.split('/').slice(0, 1 + pathSegmentsToKeep).join('/') + '/?/'
       var u3 = l.pathname.slice(1).split('/').slice(pathSegmentsToKeep).join('/').replace(/&/g, '~and~')
       var u4 = (l.search ? '&' + l.search.slice(1).replace(/&/g, '~and~') : '')
-      //console.log('Redirect URL parts: ', u1, u2, u3, u4)
       l.replace(u1 + u2 + u3 + u4 + l.hash)
     </script>
   </head>
   <body>
-    Resource not found.  Redirecting...
+    Resource not found. Redirecting...
   </body>
 </html>`)
-})
+}
+
+/**
+ * Rewrite the URL if it's a .wasm file.
+ *
+ * @param {string} url The original request URL
+ * @return {string} The rewritten URL
+ */
+function rewriteUrl(url) {
+  // If the URL is for a .wasm file and starts with '/share/v/p/', rewrite it
+  if (url.endsWith('.wasm') && url.startsWith('/share/v/p/')) {
+    return url.replace('/share/v/p/', '/')
+  }
+  return url
+}
+
+/**
+ * Get the Content-Type based on file extension.
+ *
+ * @param {string} url The request URL
+ * @return {string|null} The MIME type or null if not recognized
+ */
+function getContentType(url) {
+  if (url.endsWith('.js')) {
+    return 'application/javascript'
+  } else if (url.endsWith('.wasm')) {
+    return 'application/wasm'
+  } else if (url.endsWith('.json')) {
+    return 'application/json'
+  } else if (url.endsWith('.css')) {
+    return 'text/css'
+  } else if (url.endsWith('.html')) {
+    return 'text/html'
+  }
+  return null
+}
+
+/**
+ * Determine if a resource is cacheable.
+ *
+ * @param {string} url The request URL
+ * @return {boolean} True if the resource should be cached
+ */
+function isCacheable(url) {
+  return /\.(js|css|wasm|png|jpg|jpeg|gif|svg|ico)$/.test(url)
+}
