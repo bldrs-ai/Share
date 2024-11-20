f7e27bf925a33f737b6708d012d9c9b6aed2fb81
nickcastel50 <nickcastel50@gmail.com>
2024-11-20 18:17:57 -0500

    * fix test

--- cypress/e2e/placemarks-100/marker-visibility.cy.js
+++ cypress/e2e/placemarks-100/marker-visibility.cy.js
@@ -12,12 +12,21 @@ describe('Placemarks 100: Not visible when notes is not open', () => {
     })
 
     context('Open Notes and MarkerControl should exist', () => {
-      beforeEach(() => cy.get('[data-testid="control-button-notes"]').click())
-      it('MarkerControl should exist', () => {
+        let win
+      beforeEach(() => {
+        cy.get('[data-testid="control-button-notes"]').click()
+
         cy.get('[data-testid="list-notes"]')
         cy.get('[data-testid="panelTitle"]').contains('NOTES')
+
+        cy.window().then((window) => {
+            win = window
+        })
+        // eslint-disable-next-line cypress/no-unnecessary-waiting, no-magic-numbers
+        cy.wait(1500)
+    })
+      it('MarkerControl should exist', () => {
         // Access the scene objects
-        cy.window().then((win) => {
         const markers = win.markerScene.markerObjects
 
         // Assert that markers exist
@@ -25,11 +34,9 @@ describe('Placemarks 100: Not visible when notes is not open', () => {
 
         // Check visibility of markers
         markers.forEach((marker) => {
-        cy.log('test')
         // eslint-disable-next-line no-unused-expressions
           expect(marker.userData.id).to.exist
         })
-      })
       })
     })
   })
