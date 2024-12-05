bldrs-ai/Share

commit 77f8aeec402028aea2858cccc70f7a06a3fe7285

UI Refresh: Apps, LeftDrawer, themed Typography, reorg; Hot-reload esbuild and more (#1271)
* cypress: skip broken tests. tracked in issue #1269

* cypress: skip another test. tracked in issue #1269

* checkpoint

* apps: url state.  sidebar: title padding cleanup. controlbutton: tooltips fixed. notes: avatar valign top, dateline linebreaks.

* tests passing.

* tests passing.

* layout: working in landscape; checkpoint

* layout: left side aligned

* unit tests pass

* lint passes

* versions: move fetch logic to hook, more testing

* SideDrawer: mobile bottom drawer mostly working

* cypress passing

* cypress passinger

* cypress passingerer

* Delete api.min.js

---------

Signed-off-by: Pablo Mayrgundter <pablo.mayrgundter@gmail.com>

src/Components/CutPlane/CutPlaneMenu.jsx
@@ -1,15 +1,20 @@
-import React, {ReactElement, useState, useEffect} from 'react'
+import React, {ReactElement, useCallback, useEffect, useState, useRef} from 'react'
 import {useLocation} from 'react-router-dom'
 import {Vector3} from 'three'
 import Menu from '@mui/material/Menu'
 import MenuItem from '@mui/material/MenuItem'
+import SvgIcon from '@mui/material/SvgIcon'
 import Typography from '@mui/material/Typography'
 import useStore from '../../store/useStore'
 import debug from '../../utils/debug'
-import {addHashParams, getHashParams, getObjectParams, removeParams} from '../../utils/location'
-import {floatStrTrim, isNumeric} from '../../utils/strings'
+import {floatStrTrim} from '../../utils/strings'
 import {TooltipIconButton} from '../Buttons'
-import {HASH_PREFIX_CUT_PLANE} from './hashState'
+import {
+  addHashParams,
+  getHashParams,
+  getPlanesFromHash,
+  removeHashParams,
+} from './hashState'
 import CloseIcon from '@mui/icons-material/Close'
 import CropOutlinedIcon from '@mui/icons-material/CropOutlined'
 import ElevationIcon from '../../assets/icons/Elevation.svg'
@@ -43,27 +48,9 @@ export default function CutPlaneMenu() {
   debug().log('CutPlaneMenu: location: ', location)
   debug().log('CutPlaneMenu: cutPlanes: ', cutPlanes)
 
-  const handleClose = () => {
-    setAnchorEl(null)
-  }
+  const handleClose = () => setAnchorEl(null)
 
-  useEffect(() => {
-    const planeHash = getHashParams(location, HASH_PREFIX_CUT_PLANE)
-    debug().log('CutPlaneMenu#useEffect: planeHash: ', planeHash)
-    if (planeHash && model && viewer) {
-      const planes = getPlanes(planeHash)
-      debug().log('CutPlaneMenu#useEffect: planes: ', planes)
-      if (planes && planes.length) {
-        setIsCutPlaneActive(true)
-        planes.forEach((plane) => {
-          togglePlane(plane)
-        })
-      }
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [model])
-
-  const togglePlane = ({direction, offset = 0}) => {
+  const togglePlane = useCallback(({direction, offset = 0}) => {
     setLevelInstance(null)
     const modelCenter = new Vector3
     model?.geometry.boundingBox.getCenter(modelCenter)
@@ -74,8 +61,8 @@ export default function CutPlaneMenu() {
     debug().log('CutPlaneMenu#togglePlane: ifcPlanes: ', viewer.clipper.planes)
 
     if (cutPlanes.findIndex((cutPlane) => cutPlane.direction === direction) > -1) {
-      debug().log('CutPlaneMenu#togglePlane: found: ', true)
-      removeParams(HASH_PREFIX_CUT_PLANE, [direction])
+      debug().log('CutPlaneMenu#togglePlane: found, removing...')
+      removeHashParams(location, [direction])
       removeCutPlaneDirection(direction)
       viewer.clipper.deleteAllPlanes()
       const restCutPlanes = cutPlanes.filter((cutPlane) => cutPlane.direction !== direction)
@@ -87,13 +74,35 @@ export default function CutPlaneMenu() {
         setIsCutPlaneActive(false)
       }
     } else {
-      debug().log('CutPlaneMenu#togglePlane: found: ', false)
-      addHashParams(window.location, HASH_PREFIX_CUT_PLANE, {[direction]: offset}, true)
+      debug().log('CutPlaneMenu#togglePlane: not found, adding...')
+      addHashParams(location, {[direction]: offset})
       addCutPlaneDirection({direction, offset})
       viewer.clipper.createFromNormalAndCoplanarPoint(normal, modelCenterOffset)
       setIsCutPlaneActive(true)
     }
-  }
+  }, [addCutPlaneDirection, cutPlanes, location, model?.geometry.boundingBox, removeCutPlaneDirection,
+      setIsCutPlaneActive, setLevelInstance, viewer?.clipper])
+
+
+  const lastHashRef = useRef('')
+  useEffect(() => {
+    if (!(model && viewer)) {
+      return
+    }
+
+    const planeHash = getHashParams(location)
+    if (lastHashRef.current === planeHash) {
+      return
+    }
+    lastHashRef.current = planeHash
+    const planes = getPlanesFromHash(planeHash)
+    if (planes && planes.length) {
+      setIsCutPlaneActive(true)
+      planes.forEach((plane) => {
+        togglePlane(plane)
+      })
+    }
+  }, [lastHashRef, location, model, setIsCutPlaneActive, togglePlane, viewer])
 
   return (
     <>
@@ -104,7 +113,7 @@ export default function CutPlaneMenu() {
         selected={anchorEl !== null || !!cutPlanes.length || isCutPlaneActive}
         variant='control'
         placement='top'
-        buttonTestId='control-button-cut-plane'
+        dataTestId='control-button-cut-plane'
       />
       <Menu
         elevation={1}
@@ -121,7 +130,7 @@ export default function CutPlaneMenu() {
           selected={cutPlanes.findIndex((cutPlane) => cutPlane.direction === 'y') > -1}
           data-testid='menu-item-plan'
         >
-          <PlanIcon className='icon-share'/>
+          <SvgIcon><PlanIcon className='icon-share'/></SvgIcon>
           <Typography sx={{marginLeft: '10px'}} variant='overline'>Plan</Typography>
         </MenuItem>
         <MenuItem
@@ -129,7 +138,7 @@ export default function CutPlaneMenu() {
           selected={cutPlanes.findIndex((cutPlane) => cutPlane.direction === 'x') > -1}
           data-testid='menu-item-section'
         >
-          <SectionIcon className='icon-share'/>
+          <SvgIcon><SectionIcon className='icon-share'/></SvgIcon>
           <Typography sx={{marginLeft: '10px'}} variant='overline'>Section</Typography>
         </MenuItem>
         <MenuItem
@@ -137,7 +146,7 @@ export default function CutPlaneMenu() {
           selected={cutPlanes.findIndex((cutPlane) => cutPlane.direction === 'z') > -1}
           data-testid='menu-item-elevation'
         >
-          <ElevationIcon className='icon-share'/>
+          <SvgIcon><ElevationIcon className='icon-share'/></SvgIcon>
           <Typography sx={{marginLeft: '10px'}} variant='overline'>Elevation</Typography>
         </MenuItem>
         <MenuItem
@@ -159,15 +168,16 @@ export default function CutPlaneMenu() {
 /**
  * Called by this component and CadView for consistent reset
  *
+ * @param {object} location from react-router
  * @param {object} viewer
  * @param {Function} setCutPlaneDirections
  * @param {Function} setIsCutPlaneActive
  */
-export function resetState(viewer, setCutPlaneDirections, setIsCutPlaneActive) {
+export function resetState(location, viewer, setCutPlaneDirections, setIsCutPlaneActive) {
   // These aren't setup when CadView inits
   if (viewer && setCutPlaneDirections && setIsCutPlaneActive) {
     removePlanes(viewer)
-    removePlanesFromHashState()
+    removeHashParams(location)
     setCutPlaneDirections([])
     setIsCutPlaneActive(false)
   }
@@ -218,64 +228,6 @@ export function getPlanesOffset(viewer, ifcModel) {
     return planesOffset
   }
   return undefined
-}
-
-
-/**
- * Add plane normal and the offset to the hash state
- *
- * @param {object} viewer
- * @param {object} ifcModel
- */
-export function addPlanesToHashState(viewer, ifcModel) {
-  if (viewer.clipper.planes.length > 0) {
-    const planeInfo = getPlanesOffset(viewer, ifcModel)
-    debug().log('CutPlaneMenu#addPlaneLocationToUrl: planeInfo: ', planeInfo)
-    addHashParams(window.location, HASH_PREFIX_CUT_PLANE, planeInfo, true)
-  }
-}
-
-
-/**
- * Get offset info of x, y, z from plane hash string
- *
- * @param {string} planeHash
- * @return {Array}
- */
-export function getPlanes(planeHash) {
-  if (!planeHash) {
-    return []
-  }
-  const parts = planeHash.split(':')
-  if (parts[0] !== HASH_PREFIX_CUT_PLANE || !parts[1]) {
-    return []
-  }
-  const planeObjectParams = getObjectParams(planeHash)
-  debug().log('CutPlaneMenu#getPlanes: planeObjectParams: ', planeObjectParams)
-  const planes = []
-  Object.entries(planeObjectParams).forEach((entry) => {
-    const [key, value] = entry
-    const removableParamKeys = []
-    if (isNumeric(key)) {
-      removableParamKeys.push(key)
-    } else {
-      planes.push({
-        direction: key,
-        offset: floatStrTrim(value),
-      })
-    }
-    if (removableParamKeys.length) {
-      removeParams(HASH_PREFIX_CUT_PLANE, removableParamKeys)
-    }
-  })
-  debug().log('CutPlaneMenu#getPlanes: planes: ', planes)
-  return planes
-}
-
-
-/** Removes cut plane params from hash state */
-export function removePlanesFromHashState() {
-  removeParams(HASH_PREFIX_CUT_PLANE)
 }
 
 

commit 55a73fafead33a6f1da4a7f806ba490ac0f8ecb6

cutplanes: fix regression that broke Clear All in #1271

src/Components/CutPlane/CutPlaneMenu.jsx
@@ -152,7 +152,7 @@ export default function CutPlaneMenu() {
         <MenuItem
           onClick={() => {
             setAnchorEl(null)
-            resetState(viewer, setCutPlaneDirections, setIsCutPlaneActive)
+            resetState(location, viewer, setCutPlaneDirections, setIsCutPlaneActive)
           }}
           data-testid='menu-item-clear-all'
         >
@@ -175,7 +175,7 @@ export default function CutPlaneMenu() {
  */
 export function resetState(location, viewer, setCutPlaneDirections, setIsCutPlaneActive) {
   // These aren't setup when CadView inits
-  if (viewer && setCutPlaneDirections && setIsCutPlaneActive) {
+  if (location && viewer && setCutPlaneDirections && setIsCutPlaneActive) {
     removePlanes(viewer)
     removeHashParams(location)
     setCutPlaneDirections([])