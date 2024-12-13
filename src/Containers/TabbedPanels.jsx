4797252b8d7c2d158ac38c3ea2724b396ee250e9
nickcastel50 <nickcastel50@gmail.com>
2024-12-12 20:04:49 -0500

    * add jest tests for TabbedPanels

--- src/Containers/TabbedPanels.jsx
+++ src/Containers/TabbedPanels.jsx
@@ -67,6 +67,7 @@ export default function TabbedPanels({
   function a11yProps(index) {
     return {
       'id': `simple-tab-${index}`,
+      'data-testid': `simple-tab-${index}`,
       'aria-controls': `simple-tabpanel-${index}`,
     }
   }
