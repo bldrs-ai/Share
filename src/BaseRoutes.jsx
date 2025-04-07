96100c04e262213018fc64b84e5c53de2050f712
nickcastel50 <nickcastel50@gmail.com>
2025-04-07 15:40:57 -0400

    * add reauthentication dialog

--- src/BaseRoutes.jsx
+++ src/BaseRoutes.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, useState} from 'react'
 import {Outlet, Route, Routes, useLocation, useNavigate} from 'react-router-dom'
 import CssBaseline from '@mui/material/CssBaseline'
 import {ThemeProvider} from '@mui/material/styles'
@@ -18,6 +18,11 @@ import {navWith} from './utils/navigate'
 import {jwtDecode} from 'jwt-decode'
 import PopupAuth from './Components/Auth/PopupAuth'
 import PopupCallback from './Components/Auth/PopupCallback'
+import Dialog from '@mui/material/Dialog'
+import DialogTitle from '@mui/material/DialogTitle'
+import DialogContent from '@mui/material/DialogContent'
+import DialogActions from '@mui/material/DialogActions'
+import Button from '@mui/material/Button'
 
 
 const SentryRoutes = Sentry.withSentryReactRouterV6Routing(Routes)
@@ -41,19 +46,22 @@ export default function BaseRoutes({testElt = null}) {
   const location = useLocation()
   const navigate = useNavigate()
   const installPrefix = window.location.pathname.startsWith('/Share') ? '/Share' : ''
-  const basePath = `${installPrefix }/`
+  const basePath = `${installPrefix}/`
   const {isLoading, isAuthenticated, getAccessTokenSilently} = useAuth0()
   const setAccessToken = useStore((state) => state.setAccessToken)
   const appPrefix = `${basePath}share`
   const setAppPrefix = useStore((state) => state.setAppPrefix)
   const setIsOpfsAvailable = useStore((state) => state.setIsOpfsAvailable)
   const setAppMetadata = useStore((state) => state.setAppMetadata)
+  const theme = useShareTheme()
 
+  // State for reauthentication modal.
+  const [reauthModalOpen, setReauthModalOpen] = useState(false)
+  const [reauthScope, setReauthScope] = useState('')
 
   useEffect(() => {
     setAppPrefix(appPrefix)
   }, [setAppPrefix, appPrefix])
-
 
   useEffect(() => {
     const checkAvailability = async () => {
@@ -69,10 +77,8 @@ export default function BaseRoutes({testElt = null}) {
     checkAvailability()
   }, [setIsOpfsAvailable])
 
-
   useEffect(() => {
-    if (location.pathname === installPrefix ||
-        location.pathname === basePath) {
+    if (location.pathname === installPrefix || location.pathname === basePath) {
       const fwdPath = `${appPrefix}`
       debug().log('BaseRoutes#useEffect[], forwarding to: ', fwdPath)
       navWith(navigate, fwdPath)
@@ -88,72 +94,101 @@ export default function BaseRoutes({testElt = null}) {
         },
         cacheMode: 'off',
         useRefreshTokens: true,
-      }).then((token) => {
-        if (token !== '') {
-          // Decode the token to extract the app_metadata custom claim.
-          // cypress check
-          if (token.access_token && token.access_token === 'mock_access_token') {
-            initializeOctoKitAuthenticated()
-            setAccessToken(token)
-            return
-          }
-          const decodedToken = jwtDecode(token)
-          const appData = decodedToken['https://bldrs.ai/app_metadata']
-          if (appData) {
-            if (appData.subscriptionStatus === 'shareProPendingReauth') {
-              // reauth with updated repo scope
-              window.open('/popup-auth?scope=repo', 'authPopup', 'width=600,height=600')
-            } else if (appData.subscriptionStatus === 'freePendingReauth') {
-              // reauth with updated scope (default scope)
-              window.open('/popup-auth?scope=public_repo', 'authPopup', 'width=600,height=600')
-            } else {
-              setAppMetadata(appData)
+      })
+        .then((token) => {
+          if (token !== '') {
+            // Cypress check
+            if (token.access_token && token.access_token === 'mock_access_token') {
               initializeOctoKitAuthenticated()
               setAccessToken(token)
+              return
             }
-          }
-        } else {
-          initializeOctoKitUnauthenticated()
-          setAccessToken(token)
-        }
-      }).catch((err) => {
-        if (err.error !== 'login_required') {
-          throw err
-        }
-      })
-    }
-  }, [appPrefix, setAppPrefix, basePath, installPrefix, location, navigate,
-      isLoading, isAuthenticated, getAccessTokenSilently, setAccessToken,
-      setAppMetadata])
-
-  const theme = useShareTheme()
-  return (
-    <CssBaseline enableColorScheme>
-      <ThemeProvider theme={theme}>
-        <Styles theme={theme}/>
-        <SentryRoutes>
-          {usePageTracking()}
-          <Route path={basePath} element={<Outlet/>}>
-            <Route
-              path='share/*'
-              element={
-                testElt ||
-                  <ShareRoutes
-                    installPrefix={installPrefix}
-                    appPrefix={`${appPrefix}`}
-                  />
+            const decodedToken = jwtDecode(token)
+            const appData = decodedToken['https://bldrs.ai/app_metadata']
+            if (appData) {
+              if (appData.subscriptionStatus === 'shareProPendingReauth') {
+                // Instead of immediately calling window.open, show a modal dialog.
+                setReauthScope('repo')
+                setReauthModalOpen(true)
+              } else if (appData.subscriptionStatus === 'freePendingReauth') {
+                setReauthScope('public_repo')
+                setReauthModalOpen(true)
+              } else {
+                setAppMetadata(appData)
+                initializeOctoKitAuthenticated()
+                setAccessToken(token)
               }
-            />
-            <Route path='about' element={<About/>}/>
-            <Route
-              path='blog/*'
-              element={<BlogRoutes/>}
-            />
-          </Route>
-          <Route path='popup-auth' element={<PopupAuth/>}/>
-          <Route path='popup-callback' element={<PopupCallback/>}/>
-        </SentryRoutes>
-      </ThemeProvider>
-    </CssBaseline>
+            }
+          } else {
+            initializeOctoKitUnauthenticated()
+            setAccessToken(token)
+          }
+        })
+        .catch((err) => {
+          if (err.error !== 'login_required') {
+            throw err
+          }
+        })
+    }
+  }, [
+    appPrefix,
+    setAppPrefix,
+    basePath,
+    installPrefix,
+    location,
+    navigate,
+    isLoading,
+    isAuthenticated,
+    getAccessTokenSilently,
+    setAccessToken,
+    setAppMetadata,
+  ])
+
+  return (
+    <>
+      <CssBaseline enableColorScheme>
+        <ThemeProvider theme={theme}>
+          <Styles theme={theme}/>
+          <SentryRoutes>
+            {usePageTracking()}
+            <Route path={basePath} element={<Outlet/>}>
+              <Route
+                path='share/*'
+                element={
+                  testElt || (
+                    <ShareRoutes
+                      installPrefix={installPrefix}
+                      appPrefix={`${appPrefix}`}
+                    />
+                  )
+                }
+              />
+              <Route path='about' element={<About/>}/>
+              <Route path='blog/*' element={<BlogRoutes/>}/>
+            </Route>
+            <Route path='popup-auth' element={<PopupAuth/>}/>
+            <Route path='popup-callback' element={<PopupCallback/>}/>
+          </SentryRoutes>
+        </ThemeProvider>
+      </CssBaseline>
+
+      {/* Reauthentication Modal */}
+      <Dialog open={reauthModalOpen} onClose={() => setReauthModalOpen(false)}>
+        <DialogTitle>Reauthentication Required</DialogTitle>
+        <DialogContent>
+          Your session requires reauthentication with GitHub. Please click the button below to proceed.
+        </DialogContent>
+        <DialogActions>
+          <Button
+            onClick={() => {
+              window.open(`/popup-auth?scope=${reauthScope}`, 'authPopup', 'width=600,height=600')
+              setReauthModalOpen(false)
+            }}
+          >
+            Reauthenticate
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
   )
 }
