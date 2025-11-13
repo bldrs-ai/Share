1ef8aad961d2d413bd998e7b364272eb9d20389d
Pablo Mayrgundter <pablo.mayrgundter@gmail.com>
2025-11-13 13:19:50 +0100

    more tweaks for PW port allocation

--- tools/utils.js
+++ tools/utils.js
@@ -5,11 +5,16 @@ import {execFileSync} from 'node:child_process'
  * This is a crazy scheme to get a port number that is unique to the playwright process
  * and not already in use.
  *
+ * @param {number?} desiredPort - The port number to use if no ancestor process is found
  * @return {number} The port number
  */
-export function runGetPortPlease() {
+export function runGetPortPlease(desiredPort) {
+  const args = ['tools/get-port-please.js']
+  if (desiredPort) {
+    args.push(desiredPort)
+  }
   // Invoke helper to get port from stdout
-  const stdOut = execFileSync('node', ['tools/get-port-please.js'], {encoding: 'utf8'}).trim()
+  const stdOut = execFileSync('node', args, {encoding: 'utf8'}).trim()
   // extract port number from stdout
   // eslint-disable-next-line no-control-regex
   const clean = stdOut.replace(/\x1B\[[0-9;]*[A-Za-z]/g, '').trim() // remove ANSI escapes and newlines/spaces
